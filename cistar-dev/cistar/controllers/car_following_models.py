4d530a7f3227ceb8c9b791afaa44e8c9d60e237f
Nishant <nishantkheterpal@gmail.com>
2017-02-23 16:24:05 -0800

    added lane-checking to car following models. needs validation.

--- cistar-dev/cistar/controllers/car_following_models.py
+++ cistar-dev/cistar/controllers/car_following_models.py
@@ -69,13 +69,18 @@ def make_jank_cfm(k_d=1, k_v=1, s=1):
 
     return cfm
 
-def make_better_cfm(k_d=1, k_v=1, k_c = 1, d_des=1, v_des = 8):
+def make_better_cfm(k_d=1, k_v=1, k_c = 1, d_des=1, v_des = 8, acc_max = 15):
     # k_d = proportional gain
     # k_v = derivative gain
     # s = safe distance
 
     def cfm(carID, env):
-        leadID = env.get_leading_car(carID)
+        this_lane = env.vehicles[carID]['lane']
+
+        leadID = env.get_leading_car(carID, this_lane)
+        if not leadID: # no car ahead
+            return acc_max
+
         leadPos = env.get_x_by_id(leadID)
         leadVel = env.vehicles[leadID]['speed']
 
@@ -116,19 +121,24 @@ def make_jank_bcm(k_d=1, k_v=1, s=1):
 
     return bcm
 
-def make_better_bcm(k_d=1, k_v=1, k_c = 1, d_des=1, v_des = 8):
+def make_better_bcm(k_d=1, k_v=1, k_c = 1, d_des=1, v_des = 8, acc_max = 15):
     # k_d = proportional gain
     # k_v = derivative gain
 
     def bcm(carID, env):
-        leadID = env.get_leading_car(carID)
+        this_lane = env.vehicles[carID]['lane']
+
+        leadID = env.get_leading_car(carID, this_lane)
+        if not leadID: # no car ahead
+            return acc_max
+
         leadPos = env.get_x_by_id(leadID)
         leadVel = env.vehicles[leadID]['speed']
 
         thisPos = env.get_x_by_id(carID)
         thisVel = env.vehicles[carID]['speed']
 
-        trailID = env.get_trailing_car(carID)
+        trailID = env.get_trailing_car(carID, this_lane)
         trailPos = env.get_x_by_id(trailID)
         trailVel = env.vehicles[trailID]['speed']
 
@@ -154,7 +164,12 @@ def make_ovm(alpha = 1, beta = 1, h_st = 5, h_go = 15, v_max = 35, dt = .1, tau 
     accelQueue = collections.deque()
 
     def ovm(carID, env):
-        leadID = env.get_leading_car(carID)
+        this_lane = env.vehicles[carID]['lane']
+
+        leadID = env.get_leading_car(carID, this_lane)
+        if not leadID: # no car ahead
+            return acc_max
+
         leadPos = env.get_x_by_id(leadID)
         leadVel = env.vehicles[leadID]['speed']
         leadLength = env.vehicles[leadID]['length']
