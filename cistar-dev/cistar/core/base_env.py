aae224d9f2efb6f86ac8cfe905076a66546dae34
Leah Dickstein <dicksteinleah@gmail.com>
2017-02-26 15:12:10 -0800

    base_env documentation (#55)
    * base_env documentation
    
    * cleaned up a bit

--- cistar-dev/cistar/core/base_env.py
+++ cistar-dev/cistar/core/base_env.py
@@ -18,7 +18,7 @@ This file provides the interface for controlling a SUMO simulation. Using the en
 start sumo, provide a scenario to specify a configuration and controllers, perform simulation steps, and
 reset the simulation to an initial configuration.
 
-This class cannot be used as is, as you must extend it to implement an action applicator method, and
+This class cannot be used as is: you must extend it to implement an action applicator method, and
 properties to define the MDP if you choose to use it with RLLab.
 
 """
@@ -29,130 +29,103 @@ COLORS = [(255,0,0,0),(0, 255,0,0),(0, 0, 255,0), (255, 255,0,0),(0, 255,255,0),
 class SumoEnvironment(Env):
 
     def __init__(self,env_params, sumo_binary, sumo_params, scenario):
+        """[summary]
+        
+        [description]
+        
+        Arguments:
+            env_params {dictionary} -- [description]
+            sumo_binary {string} -- Either "sumo" or "sumo-gui"
+            sumo_params {dictionary} -- {"port": {integer} connection to SUMO,
+                            "timestep": {float} default=0.01s, SUMO default=1.0s}
+            scenario {Scenario} -- @see Scenario; abstraction of the SUMO XML files
+                                    which specify the vehicles placed on the net
+        
+        Raises:
+            ValueError -- Raised if a SUMO port not provided
         """
-        Initialize the Sumo Environment, by starting SUMO, setting up TraCI and initializing vehicles
-        Input
-        -----
-        env_params   : use this dictionary to pass in parameters relevant to the environment
-                     (i.e. target velocities, constants for step function)
-        sumo_binary : SUMO library to start
-        sumo_params : port, config file, out, error etc.
-
-
-        Outputs
-        -------
-        (observation, reward, done, info)
-        observation : agent's observation of the current environment
-        reward [Float] : amount of reward due to the previous action
-        done : a boolean, indicating whether the episode has ended
-        info : a dictionary containing other diagnostic information from the previous action
-        """
-
-        self.num_vehicles = scenario.num_vehicles
-        self.num_rl_vehicles = scenario.num_rl_vehicles
         self.env_params = env_params
-        self.type_params = scenario.type_params
         self.sumo_binary = sumo_binary
         self.scenario = scenario
-        self.initial_state = {}
+        
+        # Vehicles: Key = Vehicle ID, Value = Dictionary describing the vehicle
         self.vehicles = {}
+
+        # Represents number of steps taken
         self.timer = 0
+        # 0.01 = default time step for our research
+        self.time_step = sumo_params["time_step"] if "time_step" in sumo_params else 0.01
 
         if "port" not in sumo_params:
             raise ValueError("SUMO port not defined")
-        else:
-            self.port = sumo_params["port"]
 
-        self.cfg = scenario.cfg
+        logging.info(" Starting SUMO on port " + str(sumo_params["port"]))
+        logging.debug(" Cfg file " + str(self.scenario.cfg))
 
-        logging.info(" Starting SUMO on port " + str(self.port) + "!")
-        logging.debug(" Cfg file " + str(self.cfg))
+        # Opening the I/O thread to SUMO
+        subprocess.Popen([self.sumo_binary, "-c", self.scenario.cfg, "--remote-port",
+                                        str(sumo_params["port"]), "--step-length", str(self.time_step)], stdout=sys.stdout, stderr=sys.stderr)
 
-        self.time_step = 0.01  # Default time step
-        if "time_step" in sumo_params:
-            self.time_step = sumo_params["time_step"]
-
-        subprocess.Popen([self.sumo_binary, "-c", self.cfg, "--remote-port",
-                                        str(self.port), "--step-length", str(self.time_step)], stdout=sys.stdout, stderr=sys.stderr)
-
-        logging.debug(" Initializing TraCI on port " + str(self.port) + "!")
-        traci.init(self.port)
-
-        self.initialize_simulation()
+        logging.debug(" Initializing TraCI on port " + str(sumo_params["port"]) + "!")
+        traci.init(sumo_params["port"])
 
         self.ids = traci.vehicle.getIDList()
-        self.controlled_ids = [i for i in self.ids if not traci.vehicle.getTypeID(i) == "rl"]
-        self.rl_ids = [i for i in self.ids if traci.vehicle.getTypeID(i) == "rl"]
 
+        self.controlled_ids, self.rl_ids = [], []
+        for i in self.ids:
+            if traci.vehicle.getTypeID(i) == "rl":
+                self.rl_ids.append(i)
+            else:
+                self.controlled_ids.append(i)
 
-        for index, car_id in enumerate(self.ids):
-            vehicle = {}
-            vehicle["id"] = car_id
-            vehicle["type"] = traci.vehicle.getTypeID(car_id)
-            vehicle["edge"] = traci.vehicle.getRoadID(car_id)
-            vehicle["position"] = traci.vehicle.getLanePosition(car_id)
-            vehicle["lane"] = traci.vehicle.getLaneIndex(car_id)
-            vehicle["speed"] = traci.vehicle.getSpeed(car_id)
-            vehicle["length"] = traci.vehicle.getLength(car_id)
-            vehicle["max_speed"] = traci.vehicle.getMaxSpeed(car_id)
-            self.vehicles[car_id] = vehicle
-            traci.vehicle.setSpeedMode(car_id, 0)
+        # TODO: could possibly be handled in sumo experiment
+        # Initial state: Key = Vehicle ID, Entry = (type_id, route_id, lane_index, lane_pos, speed, pos) 
+        self.initial_state = {}
+        self.setup_initial_state()
 
-            logging.debug("Car with id " + car_id + " is on route " + str(traci.vehicle.getRouteID(car_id)))
-            logging.debug("Car with id " + car_id + " is on edge " + str(traci.vehicle.getLaneID(car_id)))
-            logging.debug("Car with id " + car_id + " has valid route: " + str(traci.vehicle.isRouteValid(car_id)))
-            logging.debug("Car with id " + car_id + " has speed: " + str(traci.vehicle.getSpeed(car_id)))
-            logging.debug("Car with id " + car_id + " has pos: " + str(traci.vehicle.getPosition(car_id)))
-            logging.debug("Car with id " + car_id + " has route: " + str(traci.vehicle.getRoute(car_id)))
-            logging.debug("Car with id " + car_id + " is at index: " + str(traci.vehicle.getRouteIndex(car_id)))
-
-        # could possibly be handled in sumo experiment
-        self.store_initial_state()
-
-    def initialize_simulation(self):
+    def setup_initial_state(self):
         """
-        Needs to generate, and place cars in the correct places,
-        If cars are placed on routes in CFG, or generated in flows, step until all cars are on the board
-
-        (this method can and should be overridden for different initializations )
+        Store initial state so that simulation can be reset at the end.
+        TODO: Make traci calls as bulk as possible
+        Initial state is a dictionary: key = vehicle IDs, value = state describing car
         """
-        while traci.vehicle.getIDCount() < self.num_vehicles:
-            traci.simulationStep()
-            for car_id in traci.vehicle.getIDList():
-                # IMPORTANT FOR FINE GRAIN CONTROL OF VEHICLE SPEED
-                traci.vehicle.setSpeedMode(car_id, 0)
-                traci.vehicle.setSpeed(car_id, 10)
-
-
-    def store_initial_state(self):
-        """
-        Store initial state so that simulation can be reset at the end
-        """
-
-        # Get initial state of each vehicle, and store in initial config object
         for veh_id in self.ids:
-            type_id = traci.vehicle.getTypeID(veh_id)
-            route_id = traci.vehicle.getRouteID(veh_id)
-            lane_index = traci.vehicle.getLaneIndex(veh_id)
-            lane_pos = traci.vehicle.getLanePosition(veh_id)
-            speed = traci.vehicle.getSpeed(veh_id)
-            pos = traci.vehicle.getPosition(veh_id)
-            self.initial_state[veh_id] = (type_id, route_id, lane_index, lane_pos, speed, pos)
+            vehicle = {}
+            vehicle["id"] = veh_id
+            vehicle["type"] = traci.vehicle.getTypeID(veh_id)
+            vehicle["edge"] = traci.vehicle.getRoadID(veh_id)
+            vehicle["position"] = traci.vehicle.getLanePosition(veh_id)
+            vehicle["lane"] = traci.vehicle.getLaneIndex(veh_id)
+            vehicle["speed"] = traci.vehicle.getSpeed(veh_id)
+            vehicle["length"] = traci.vehicle.getLength(veh_id)
+            vehicle["max_speed"] = traci.vehicle.getMaxSpeed(veh_id)
+            self.vehicles[veh_id] = vehicle
+            traci.vehicle.setSpeedMode(veh_id, 0)
 
-    def apply_action(self, car_id, action):
-        """
-        :param car_id:
-        :param action: as specified by a controller or RL lab, usually a scalar value
-        """
-        raise NotImplementedError
+            # Saving initial state
+            route_id = traci.vehicle.getRouteID(veh_id)
+            pos = traci.vehicle.getPosition(veh_id)
+            # TODO: Should we save some of the arguments as strings so we don't
+            # have to repeatedly call str() in reset()
+            self.initial_state[veh_id] = (vehicle["type"], route_id, vehicle["lane"], vehicle["position"], vehicle["speed"], pos)
+
+            logging.debug("Car with id " + veh_id + " is on route " + str(route_id))
+            logging.debug("Car with id " + veh_id + " is on edge " + str(traci.vehicle.getLaneID(veh_id)))
+            logging.debug("Car with id " + veh_id + " has valid route: " + str(traci.vehicle.isRouteValid(veh_id)))
+            logging.debug("Car with id " + veh_id + " has speed: " + str(vehicle["speed"]))
+            logging.debug("Car with id " + veh_id + " has absolute position: " + str(pos))
+            logging.debug("Car with id " + veh_id + " has route: " + str(traci.vehicle.getRoute(veh_id)))
+            logging.debug("Car with id " + veh_id + " is at route index: " + str(traci.vehicle.getRouteIndex(veh_id)))
 
     def step(self, rl_actions):
         """
-        Run one timestep of the environment's dynamics. When end of episode
+        Run one timestep of the environment's dynamics. "Self-driving cars" will
+        step forward based on rl_actions, provided by the RL algorithm. Other cars
+        will step forward based on their car following model. When end of episode
         is reached, reset() should be called to reset the environment's internal state.
         Input
         -----
-        action : an action provided by the environment
+        rl_actions : an action provided by the rl algorithm
         Outputs
         -------
         (observation, reward, done, info)
@@ -162,49 +135,40 @@ class SumoEnvironment(Env):
         info : a dictionary containing other diagnostic information from the previous action
         """
         logging.debug("================= performing step =================")
-        for car_id in self.controlled_ids:
-            car_type = traci.vehicle.getTypeID(car_id)
-            action = self.type_params[car_type][1](car_id, self)
-            self.apply_action(car_id, action=action)
-            logging.debug("Car with id " + car_id + " is on route " + str(traci.vehicle.getRouteID(car_id)))
+        for veh_id in self.controlled_ids:
+            car_type = traci.vehicle.getTypeID(veh_id)
+            action = self.scenario.type_params[car_type][1](veh_id, self)
+            self.apply_action(veh_id, action=action)
+            logging.debug("Car with id " + veh_id + " is on route " + str(traci.vehicle.getRouteID(veh_id)))
 
-        for index, car_id in enumerate(self.rl_ids):
+        for index, veh_id in enumerate(self.rl_ids):
             action = rl_actions[index]
-            self.apply_action(car_id, action=action)
+            self.apply_action(veh_id, action=action)
 
         self.timer += 1
-        # if it's been long enough
-        # try and change lanes
-        if self.timer == 80:
-            print(' ')
-            print(' ')
-            self.timer = 0
-            for car_id in self.controlled_ids:
-                car_type = traci.vehicle.getTypeID(car_id)
-                newlane = self.type_params[car_type][2](car_id, self)
-                traci.vehicle.changeLane(car_id, newlane, 10000)
+        # TODO: Turn 100 into a hyperparameter
+        # if it's been long enough try and change lanes
+        if self.timer % 100 == 0:
+            for veh_id in self.controlled_ids:
+                car_type = self.vehicles[veh_id]["type"]
+                newlane = self.scenario.type_params[car_type][2](veh_id, self)
+                traci.vehicle.changeLane(veh_id, newlane, 10000)
 
         traci.simulationStep()
 
-        for index, car_id in enumerate(self.ids):
-            self.vehicles[car_id]["type"] = traci.vehicle.getTypeID(car_id)
-            self.vehicles[car_id]["edge"] = traci.vehicle.getRoadID(car_id)
-            self.vehicles[car_id]["position"] = traci.vehicle.getLanePosition(car_id)
-            self.vehicles[car_id]["lane"] = traci.vehicle.getLaneIndex(car_id)
-            self.vehicles[car_id]["speed"] = traci.vehicle.getSpeed(car_id)
+        for veh_id in self.ids:
+            self.vehicles[veh_id]["type"] = traci.vehicle.getTypeID(veh_id)
+            self.vehicles[veh_id]["edge"] = traci.vehicle.getRoadID(veh_id)
+            self.vehicles[veh_id]["position"] = traci.vehicle.getLanePosition(veh_id)
+            self.vehicles[veh_id]["lane"] = traci.vehicle.getLaneIndex(veh_id)
+            self.vehicles[veh_id]["speed"] = traci.vehicle.getSpeed(veh_id)
 
+        # TODO: Can self._state be initialized, saved and updated so that we can
+        # exploit numpy speed
         self._state = self.getState()
         reward = self.compute_reward(self._state)
         next_observation = np.copy(self._state)
         return Step(observation=next_observation, reward=reward, done=False)
-
-    def getState(self):
-        """
-        returns the state of the simulation, can change based on RL input.
-        :return:
-        """
-        raise NotImplementedError
-
 
     def reset(self):
         """
@@ -214,28 +178,43 @@ class SumoEnvironment(Env):
         observation : the initial observation of the space. (Initial reward is assumed to be 0.)
         """
         color = random.sample(COLORS, 1)[0]
-        for car_id in self.ids:
-            type_id, route_id, lane_index, lane_pos, speed, pos = self.initial_state[car_id]
+        for veh_id in self.ids:
+            type_id, route_id, lane_index, lane_pos, speed, pos = self.initial_state[veh_id]
 
-            logging.debug("Moving car " + car_id + " from " + str(traci.vehicle.getPosition(car_id)) + " to " + str(pos))
-            traci.vehicle.remove(car_id)
-            traci.vehicle.addFull(car_id, route_id, typeID=str(type_id), departLane=str(lane_index),
+            logging.debug("Moving car " + veh_id + " from " + str(traci.vehicle.getPosition(veh_id)) + " to " + str(pos))
+            traci.vehicle.remove(veh_id)
+            traci.vehicle.addFull(veh_id, route_id, typeID=str(type_id), departLane=str(lane_index),
                                   departPos=str(lane_pos), departSpeed=str(speed))
-            traci.vehicle.setColor(car_id, color)
+            traci.vehicle.setColor(veh_id, color)
         traci.simulationStep()
 
-        for index, car_id in enumerate(self.ids):
-            traci.vehicle.setSpeedMode(car_id, 0)
-            self.vehicles[car_id]["type"] = traci.vehicle.getTypeID(car_id)
-            self.vehicles[car_id]["edge"] = traci.vehicle.getRoadID(car_id)
-            self.vehicles[car_id]["position"] = traci.vehicle.getLanePosition(car_id)
-            self.vehicles[car_id]["lane"] = traci.vehicle.getLaneIndex(car_id)
-            self.vehicles[car_id]["speed"] = traci.vehicle.getSpeed(car_id)
+        # TODO: Replace these traci calls with initial_state accesses
+        for veh_id in self.ids:
+            traci.vehicle.setSpeedMode(veh_id, 0)
+            self.vehicles[veh_id]["type"] = traci.vehicle.getTypeID(veh_id)
+            self.vehicles[veh_id]["edge"] = traci.vehicle.getRoadID(veh_id)
+            self.vehicles[veh_id]["position"] = traci.vehicle.getLanePosition(veh_id)
+            self.vehicles[veh_id]["lane"] = traci.vehicle.getLaneIndex(veh_id)
+            self.vehicles[veh_id]["speed"] = traci.vehicle.getSpeed(veh_id)
 
         self._state = np.array([traci.vehicle.getSpeed(vID) for vID in self.ids])
         observation = np.copy(self._state)
 
         return observation
+
+    def apply_action(self, veh_id, action):
+        """
+        :param veh_id: {string}
+        :param action: as specified by a controller or rllab, usually a scalar value
+        """
+        raise NotImplementedError
+
+    def getState(self):
+        """
+        returns the state of the simulation, dependent on the experiment/environment
+        :return:
+        """
+        raise NotImplementedError
 
     @property
     def action_space(self):
@@ -251,10 +230,19 @@ class SumoEnvironment(Env):
         raise NotImplementedError
 
     def compute_reward(self, state):
+        """Reward function for RL.
+        
+        Arguments:
+            state {Array-type} -- State of all the vehicles in the simulation
+        """
         raise NotImplementedError
 
     def render(self):
+        """Description of the state for when verbose mode is on."""
         raise NotImplementedError
 
     def terminate(self):
+        """Closes the TraCI I/O connection. Should be done at end of every experiment.
+        Must be in Environment because the environment opens the TraCI connection.
+        """
         traci.close()
