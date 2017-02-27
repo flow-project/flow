39f9c5af60dfe4ed6ece787a6f4ae86cd47e184f
Kanaad Parvate <kanaadp@users.noreply.github.com>
2017-02-26 16:51:56 -0800

    loop code review (#60)

--- cistar-dev/cistar/envs/loop.py
+++ cistar-dev/cistar/envs/loop.py
@@ -9,32 +9,50 @@ import numpy as np
 
 
 class LoopEnvironment(SumoEnvironment):
-
-    def get_last_x_by_id(self, id):
-        return self.scenario.get_x(self.last_step[id]["edge"], self.last_step[id]["position"])
+    """
+    Half-implementation for the environment for loop scenarios. Contains non-static methods pertaining to a loop
+     environment (for example, headway calculation leading-car, etc.)
+    """
 
     def get_x_by_id(self, id):
+        """
+        Returns the position of the vehicle with specified id
+        :param id: id of vehicle
+        :return:
+        """
         if self.vehicles[id]["edge"] == '':
             print("This vehicle teleported and its edge is now empty", id)
         return self.scenario.get_x(self.vehicles[id]["edge"], self.vehicles[id]["position"])
 
     def get_leading_car(self, car_id, lane = None):
+        """
+        Returns the id of the car in-front of the specified car in the specified lane.
+        :param car_id: target car
+        :param lane: target lane
+        :return: id of leading car in target lane
+        """
         target_pos = self.get_x_by_id(car_id)
 
-        frontdists = []
+        front_dists = []
         for i in self.ids:
             if i != car_id:
                 c = self.vehicles[i]
                 if lane is None or c['lane'] == lane:
-                    distto = (self.get_x_by_id(i) - target_pos) % self.scenario.length
-                    frontdists.append((c["id"], distto))
+                    dist_to = (self.get_x_by_id(i) - target_pos) % self.scenario.length
+                    front_dists.append((c["id"], dist_to))
 
-        if frontdists:
-            return min(frontdists, key=(lambda x:x[1]))[0]
+        if front_dists:
+            return min(front_dists, key=(lambda x:x[1]))[0]
         else:
             return None
 
     def get_trailing_car(self, car_id, lane = None):
+        """
+        Returns the id of the car behind the specified car in the specified lane.
+        :param car_id: target car
+        :param lane: target lane
+        :return: id of trailing car in target lane
+        """
         target_pos = self.get_x_by_id(car_id)
 
         backdists = []
@@ -51,6 +69,7 @@ class LoopEnvironment(SumoEnvironment):
             return None
 
     def get_cars(self, car_id, dxBack, dxForward, lane = None, dx = None):
+        #TODO: correctly implement this method, and add documentation
         this_pos = self.get_x_by_id(car_id) # position of the car checking neighbors
         front_limit = this_pos + dxForward
         rear_limit = this_pos - dxBack
--- cistar-dev/cistar/envs/loop_accel.py
+++ cistar-dev/cistar/envs/loop_accel.py
@@ -7,28 +7,53 @@ import traci
 import numpy as np
 
 
-
+"""
+Fully functional environment. Takes in an *acceleration* as an action. Reward function is negative norm of the
+difference between the velocities of each vehicle, and the target velocity. State function is a vector of the
+velocities for each vehicle.
+"""
 class SimpleAccelerationEnvironment(LoopEnvironment):
+
 
     @property
     def action_space(self):
-        return Box(low=-5, high=5, shape=(len(self.rl_ids), ))
+        """
+        Actions are a set of accelerations from 0 to 15m/s
+        :return:
+        """
+        #TODO: max and min are parameters
+        return Box(low=-5, high=5, shape=(self.scenario.num_rl_vehicles, ))
 
     @property
     def observation_space(self):
+        """
+        See parent class
+        An observation is an array the velocities for each vehicle
+        """
         return Box(low=-np.inf, high=np.inf, shape=(len(self.ids), ))
 
     def apply_action(self, car_id, action):
-        '''Action is an acceleration here. Gets locally linearized to find velocity.'''
+        """
+        See parent class (base_env)
+         Given an acceleration, set instantaneous velocity given that acceleration.
+        """
         thisSpeed = self.vehicles[car_id]['speed']
         nextVel = thisSpeed + action * self.time_step
         nextVel = max(0, nextVel)
         traci.vehicle.slowDown(car_id, nextVel, 1)
 
     def compute_reward(self, velocity):
+        """
+        See parent class
+        """
         return -np.linalg.norm(velocity - self.env_params["target_velocity"])
 
     def getState(self):
+        """
+       See parent class
+       The state is an array the velocities for each vehicle
+       :return: an array of vehicle speed for each vehicle
+       """
         return np.array([self.vehicles[vehicle]["speed"] for vehicle in self.vehicles])
 
     def render(self):
