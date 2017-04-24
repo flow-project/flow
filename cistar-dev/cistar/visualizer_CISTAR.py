14108c5660da09c8a7aa2ecef6ed9422e6549fcc
Leah Dickstein <dicksteinleah@gmail.com>
2017-04-24 11:29:06 -0700

    updated visualizer to handle multiple observation variables, still need to make small fix to handle AWS run experiments

--- cistar-dev/cistar/visualizer_CISTAR.py
+++ cistar-dev/cistar/visualizer_CISTAR.py
@@ -11,14 +11,12 @@ from matplotlib import pyplot as plt
 import plotly.offline as po
 import plotly.graph_objs as go
 
-filename = str(uuid.uuid4())
-
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument('file', type=str,
                         help='path to the snapshot file')
-    parser.add_argument('--max_path_length', type=int, default=400,
+    parser.add_argument('--max_path_length', type=int,
                         help='Max length of rollout')
     parser.add_argument('--speedup', type=float, default=1,
                         help='Speedup')
@@ -28,49 +26,63 @@ if __name__ == "__main__":
                         help='Prefix for all generated plots')
     args = parser.parse_args()
 
-    # currently can't load because env has no scenario
+    # Max Path Length should match max path length of original experiment
+    if not args.max_path_length:
+        raise ValueError("Max Path Length not provided, please match max path length of original experiment")
 
     data = joblib.load(args.file)
     policy = data['policy']
     env = data['env']
 
     # Hacky way to recreate scenario
-    tot_cars, auton_cars = 4, 4
+    tot_cars, auton_cars = 12, 12
+    num_itr = 1000
+    obs_vars = ["Velocity", "Fuel", "Distance"]
 
     # if env.orig_env.cfgfn[:17] == "/root/code/rllab/":
         # env.orig_env.cfgfn = env.orig_env.cfgfn[17:]
-    # Each entry should be a matrix shape length_rollout x num_rollouts
-    carvels = [np.zeros((args.max_path_length, args.num_rollouts)) for car in range(tot_cars)]
+
+    path = rollout(env, policy, max_path_length=args.max_path_length,
+                   animated=False, speedup=args.speedup)
+    obs = path['observations']
+    # print("Obs shape", obs.shape)
+    _, flat_obs = obs.shape
+    num_obs_var = flat_obs / tot_cars
+
+    all_obs = np.zeros((args.num_rollouts, args.max_path_length, flat_obs))
+    all_rewards = np.zeros((args.num_rollouts, args.max_path_length))
     for j in range(args.num_rollouts):
         path = rollout(env, policy, max_path_length=args.max_path_length,
                    animated=False, speedup=args.speedup)
         obs = path['observations'] # length of rollout x flattened observation
-
-        for car in range(tot_cars):
-            carvels[car][:,j] = obs[:,car]
-
+        all_obs[j] = obs
+        all_rewards[j] = path["rewards"]
         print("\n Done: {0} / {1}, {2}%".format(j+1, args.num_rollouts, (j+1) / args.num_rollouts))
-    print(obs.shape)
-    plt.figure()
-    for car in range(tot_cars):
-        # mean is horizontal, across num_rollouts
-        center = np.mean(carvels[car], axis=1)
-        # stdev = np.std(carvels[car], axis=1)
-        plt.plot(range(args.max_path_length), center, lw=2.0, label='Car {}'.format(car))
-        # plt.plot(range(args.max_path_length), center + stdev, lw=2.0, c='black', ls=':')
-        # plt.plot(range(args.max_path_length), center - stdev, lw=2.0, c='black', ls=':')
-    plt.ylabel("Velocity m/s", fontsize=15)
-    plt.xlabel("Rollout/Path Length", fontsize=15)
-    plt.title("Cars {0} / {1} Itr 1000".format(auton_cars, tot_cars), fontsize=16)
-    plt.legend(loc=0)
-    plt.savefig("visualizer/{0}_velocity.png".format(args.plotname), bbox="tight")
+    
+
+    # TODO: savefig doesn't like making new directories
+    for obs_var_idx in range(len(obs_vars)):
+        obs_var = obs_vars[obs_var_idx]
+        plt.figure()
+        for car in range(tot_cars):
+            # mean is horizontal, across num_rollouts
+            center = np.mean(all_obs[:, :, tot_cars*obs_var_idx + car], axis=0)
+            # stdev = np.std(carvels[car], axis=1)
+            plt.plot(range(args.max_path_length), center, lw=2.0, label='Car {}'.format(car))
+            # plt.plot(range(args.max_path_length), center + stdev, lw=2.0, c='black', ls=':')
+            # plt.plot(range(args.max_path_length), center - stdev, lw=2.0, c='black', ls=':')
+        plt.ylabel(obs_var, fontsize=15)
+        plt.xlabel("Rollout/Path Length", fontsize=15)
+        plt.title("Cars {0} / {1} Itr {2}".format(auton_cars, tot_cars, num_itr), fontsize=16)
+        plt.legend(loc=0)
+        plt.savefig("visualizer/{0}_{1}.png".format(args.plotname, obs_var), bbox="tight")
 
     plt.figure()
     # print(path["rewards"].shape)
-    plt.plot(range(args.max_path_length), path["rewards"], lw=2.0)
+    plt.plot(range(args.max_path_length), np.mean(all_rewards, axis=0), lw=2.0)
     plt.ylabel("Reward", fontsize=15)
     plt.xlabel("Rollout/Path Length", fontsize=15)
-    plt.title("Cars {0} / {1} Itr 1000".format(auton_cars, tot_cars), fontsize=16)
+    plt.title("Cars {0} / {1} Itr {2}".format(auton_cars, tot_cars, num_itr), fontsize=16)
     plt.savefig("visualizer/{0}_reward.png".format(args.plotname), bbox="tight")
     # print('Total reward: ', sum(path["rewards"]))
     env.terminate()
