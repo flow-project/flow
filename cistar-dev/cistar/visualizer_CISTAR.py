6c8ee3322e17f0578dc3499b6f1deb8d55772592
Leah Dickstein <dicksteinleah@gmail.com>
2017-04-28 11:39:24 -0700

    updated to get more information from pkl

--- cistar-dev/cistar/visualizer_CISTAR.py
+++ cistar-dev/cistar/visualizer_CISTAR.py
@@ -16,10 +16,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('file', type=str,
                         help='path to the snapshot file')
-    parser.add_argument('--max_path_length', type=int,
-                        help='Max length of rollout')
-    parser.add_argument('--speedup', type=float, default=1,
-                        help='Speedup')
+    # parser.add_argument('--max_path_length', type=int, default=1,
+    #                     help='Max length of rollout')
+    # parser.add_argument('--speedup', type=float, default=1,
+    #                     help='Speedup')
     parser.add_argument('--num_rollouts', type=int, default=100, 
                         help='Number of rollouts we will average over')
     parser.add_argument('--plotname', type=str, default="traffic_plot",
@@ -27,33 +27,31 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # Max Path Length should match max path length of original experiment
-    if not args.max_path_length:
-        raise ValueError("Max Path Length not provided, please match max path length of original experiment")
+    # if not max_path_length:
+        # raise ValueError("Max Path Length not provided, please match max path length of original experiment")
+
+    # Input
+    obs_vars = ["Velocity", "Fuel", "Distance"]
 
     data = joblib.load(args.file)
     policy = data['policy']
     env = data['env']
+    algo = data['algo']
 
-    # Hacky way to recreate scenario
-    tot_cars, auton_cars = 12, 12
-    num_itr = 1000
-    obs_vars = ["Velocity", "Fuel", "Distance"]
-
-    # if env.orig_env.cfgfn[:17] == "/root/code/rllab/":
-        # env.orig_env.cfgfn = env.orig_env.cfgfn[17:]
-
-    path = rollout(env, policy, max_path_length=args.max_path_length,
-                   animated=False, speedup=args.speedup)
-    obs = path['observations']
-    # print("Obs shape", obs.shape)
-    _, flat_obs = obs.shape
+    # Recreate experiment params
+    tot_cars = env._wrapped_env.scenario.num_vehicles
+    rl_cars = env._wrapped_env.scenario.num_rl_vehicles
+    num_itr = algo.n_itr
+    max_path_length = algo.max_path_length
+    flat_obs = env._wrapped_env.observation_space.flat_dim
     num_obs_var = flat_obs / tot_cars
 
-    all_obs = np.zeros((args.num_rollouts, args.max_path_length, flat_obs))
-    all_rewards = np.zeros((args.num_rollouts, args.max_path_length))
+    # Load data into arrays
+    all_obs = np.zeros((args.num_rollouts, max_path_length, flat_obs))
+    all_rewards = np.zeros((args.num_rollouts, max_path_length))
     for j in range(args.num_rollouts):
-        path = rollout(env, policy, max_path_length=args.max_path_length,
-                   animated=False, speedup=args.speedup)
+        path = rollout(env, policy, max_path_length=max_path_length,
+                   animated=False, speedup=1)
         obs = path['observations'] # length of rollout x flattened observation
         all_obs[j] = obs
         all_rewards[j] = path["rewards"]
@@ -68,21 +66,21 @@ if __name__ == "__main__":
             # mean is horizontal, across num_rollouts
             center = np.mean(all_obs[:, :, tot_cars*obs_var_idx + car], axis=0)
             # stdev = np.std(carvels[car], axis=1)
-            plt.plot(range(args.max_path_length), center, lw=2.0, label='Car {}'.format(car))
-            # plt.plot(range(args.max_path_length), center + stdev, lw=2.0, c='black', ls=':')
-            # plt.plot(range(args.max_path_length), center - stdev, lw=2.0, c='black', ls=':')
+            plt.plot(range(max_path_length), center, lw=2.0, label='Car {}'.format(car))
+            # plt.plot(range(max_path_length), center + stdev, lw=2.0, c='black', ls=':')
+            # plt.plot(range(max_path_length), center - stdev, lw=2.0, c='black', ls=':')
         plt.ylabel(obs_var, fontsize=15)
         plt.xlabel("Rollout/Path Length", fontsize=15)
-        plt.title("Cars {0} / {1} Itr {2}".format(auton_cars, tot_cars, num_itr), fontsize=16)
+        plt.title("Cars {0} / {1} Itr {2}".format(rl_cars, tot_cars, num_itr), fontsize=16)
         plt.legend(loc=0)
         plt.savefig("visualizer/{0}_{1}.png".format(args.plotname, obs_var), bbox="tight")
 
     plt.figure()
     # print(path["rewards"].shape)
-    plt.plot(range(args.max_path_length), np.mean(all_rewards, axis=0), lw=2.0)
+    plt.plot(range(max_path_length), np.mean(all_rewards, axis=0), lw=2.0)
     plt.ylabel("Reward", fontsize=15)
     plt.xlabel("Rollout/Path Length", fontsize=15)
-    plt.title("Cars {0} / {1} Itr {2}".format(auton_cars, tot_cars, num_itr), fontsize=16)
+    plt.title("Cars {0} / {1} Itr {2}".format(rl_cars, tot_cars, num_itr), fontsize=16)
     plt.savefig("visualizer/{0}_reward.png".format(args.plotname), bbox="tight")
     # print('Total reward: ', sum(path["rewards"]))
     env.terminate()
