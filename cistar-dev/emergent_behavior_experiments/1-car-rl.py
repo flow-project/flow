4bc77a08b039b5f74c4e94e47d05825cb3a7ad53
Eugene Vinitsky <eugenevinitsky@airbears2-10-142-37-31.airbears2.1918.berkeley.edu>
2017-05-02 15:04:08 -0700

    1-car-rl now works with ExtendedEmissionEnvironment instead

---
+++ cistar-dev/emergent_behavior_experiments/1-car-rl.py
@@ -0,0 +1,82 @@
+import logging
+
+from rllab.envs.normalized_env import normalize
+from rllab.misc.instrument import stub, run_experiment_lite
+from rllab.algos.trpo import TRPO
+from rllab.baselines.linear_feature_baseline import LinearFeatureBaseline
+from rllab.policies.gaussian_mlp_policy import GaussianMLPPolicy
+
+# from cistar.core.exp import SumoExperiment
+from cistar.envs.loop_accel import SimpleAccelerationEnvironment
+from cistar.envs.loop_accel_pos_vel import ExtendedAccelerationEnvironment
+from cistar.envs.loop_emission import SimpleEmissionEnvironment
+from cistar.scenarios.loop.loop_scenario import LoopScenario
+from cistar.controllers.rlcontroller import RLController
+from cistar.controllers.lane_change_controllers import *
+from cistar.controllers.car_following_models import OVMController
+
+logging.basicConfig(level=logging.INFO)
+
+stub(globals())
+
+sumo_params = {"port": 8873, "time_step":0.01}
+sumo_binary = "sumo"
+
+env_params = {"target_velocity": 20, "max-deacc": 0, "max-acc": 3, "fail-safe": 'instantaneous'}
+
+net_params = {"length": 230, "lanes": 1, "speed_limit": 35, "resolution": 40,
+              "net_path": "debug/rl/net/"}
+
+cfg_params = {"start_time": 0, "end_time": 3000, "cfg_path": "debug/rl/cfg/"}
+
+initial_config = {"shuffle": False}
+
+num_tot = 2
+num_auto = 2
+
+#exp_tag = str(num_auto) + '-car-rl' +'-deaccel'
+exp_tag = '2-car-only-rl-16x16'
+
+type_params = {"rl":(num_auto, (RLController, {}), (StaticLaneChanger, {}), 0)}
+                #, "ovm": (num_tot-num_auto, (OVMController, {}), (StaticLaneChanger, {}), 0)}
+
+scenario = LoopScenario(exp_tag, type_params, net_params, cfg_params, initial_config=initial_config)
+
+env = ExtendedAccelerationEnvironment(env_params, sumo_binary, sumo_params, scenario)
+
+env = normalize(env)
+
+for seed in [15]:#[15, 39, 20, 89, 94]:
+    policy = GaussianMLPPolicy(
+        env_spec=env.spec,
+        hidden_sizes=(16,16)
+    )
+
+    baseline = LinearFeatureBaseline(env_spec=env.spec)
+
+    algo = TRPO(
+        env=env,
+        policy=policy,
+        baseline=baseline,
+        batch_size=1000,
+        max_path_length=1500,
+        n_itr=750,  # 1000
+        # whole_paths=True,
+        # discount=0.99,
+        # step_size=0.01,
+    )
+    # algo.train()
+
+    run_experiment_lite(
+        algo.train(),
+        # Number of parallel workers for sampling
+        n_parallel=1,
+        # Only keep the snapshot parameters for the last iteration
+        snapshot_mode="all",
+        # Specifies the seed for the experiment. If this is not provided, a random seed
+        # will be used
+        seed=seed,
+        mode="local",
+        exp_prefix=exp_tag
+        # plot=True,
+    )
