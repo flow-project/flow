e75436063abb9e78fa266b34d0ad6f6ea6aeba8c
AboudyKreidieh <akreidieh@gmail.com>
2017-07-27 22:38:18 -0700

    additional options for getState and sorted_by_position which may speed up convergence

--- cistar-dev/cistar/envs/loop_merges.py
+++ cistar-dev/cistar/envs/loop_merges.py
@@ -73,9 +73,23 @@ class SimpleLoopMergesEnvironment(LoopEnvironment):
         The state is an array the velocities for each vehicle
         :return: a matrix of velocities and absolute positions for each vehicle
         """
+        pos = []
+        for veh_id in self.sorted_ids:
+            # the position of non-merging vehicles is their relative position on the ring
+            if "merge" not in veh_id:
+                pos.append(self.vehicles[veh_id]["absolute_position"] % self.scenario.length)
+            # the position of merging vehicles is their absolute position in the network
+            # (in the ring, it would be their relative position on the ring)
+            else:
+                pos.append(self.vehicles[veh_id]["absolute_position"])
+
         return np.array([[self.vehicles[veh_id]["speed"] + normal(0, self.observation_vel_std),
-                          self.vehicles[veh_id]["absolute_position"] + normal(0, self.observation_pos_std)]
-                         for veh_id in self.sorted_ids]).T
+                          pos[i] + normal(0, self.observation_pos_std)]
+                         for i, veh_id in enumerate(self.sorted_ids)]).T
+
+        # return np.array([[self.vehicles[veh_id]["speed"] + normal(0, self.observation_vel_std),
+        #                   self.vehicles[veh_id]["absolute_position"] + normal(0, self.observation_pos_std)]
+        #                  for veh_id in self.sorted_ids]).T
 
     def render(self):
         print('current state/velocity:', self.state)
@@ -113,40 +127,61 @@ class SimpleLoopMergesEnvironment(LoopEnvironment):
         are sorted according to their position of their respective edge.
         Vehicles are sorted by position on the ring, the in-merge, then the out-merge
         """
-        abs_pos = []
+        # abs_pos = []
+        # for veh_id in self.ids:
+        #     # for not non-merging-vehicles, the absolute position is not changed
+        #     if "merge" not in self.vehicles[veh_id]["type"]:
+        #         abs_pos.append((veh_id, self.vehicles[veh_id]["absolute_position"]))
+        #
+        #     # for merging vehicles in the ring, the absolute position is augmented by the number of loops
+        #     # the vehicle
+        #     elif self.vehicles[veh_id]["edge"] not in ["merge_in", "merge_out"]:
+        #         if 0 < self.vehicles[veh_id]["absolute_position"] < self.scenario.length:
+        #             # closest vehicle behind the merge in node
+        #             non_merge_vehicle_pos = [(veh_id, self.vehicles[veh_id]["absolute_position"])
+        #                                      for veh_id in self.ids if "merge" not in veh_id]
+        #             lag_id = max(non_merge_vehicle_pos, key=lambda x: x[1] % self.scenario.length)[0]
+        #
+        #             # number of loops performed by the lagging vehicle
+        #             num_loops = int(self.vehicles[lag_id]["absolute_position"] / self.scenario.length)
+        #
+        #             self.vehicles[veh_id]["absolute_position"] += (num_loops + 1) * self.scenario.length
+        #
+        #         abs_pos.append((veh_id, self.vehicles[veh_id]["absolute_position"]))
+        #
+        #     # merging vehicles outside the ring have absolute positions equal to some constant plus their relative
+        #     # position on the given edge
+        #     else:
+        #         if self.vehicles[veh_id]["edge"] == "merge_in":
+        #             abs_pos.append((veh_id, 1000 * self.scenario.length + self.vehicles[veh_id]["position"]))
+        #         else:
+        #             abs_pos.append((veh_id, 1000 * self.scenario.length + self.vehicles[veh_id]["position"] +
+        #                             self.scenario.merge_in_len))
+        #
+        # # sort absolute positions and collect ids
+        # abs_pos.sort(key=lambda tup: tup[1])
+        # sorted_ids = [tup[0] for tup in abs_pos]
+        #
+        # return sorted_ids
+
+        # position of merging vehicles and non-merging vehicles are collected and ordered separately
+        merge_veh = []
+        non_merge_veh = []
         for veh_id in self.ids:
-            # for not non-merging-vehicles, the absolute position is not changed
-            if "merge" not in self.vehicles[veh_id]["type"]:
-                abs_pos.append((veh_id, self.vehicles[veh_id]["absolute_position"]))
-
-            # for merging vehicles in the ring, the absolute position is augmented by the number of loops
-            # the vehicle
-            elif self.vehicles[veh_id]["edge"] not in ["merge_in", "merge_out"]:
-                if 0 < self.vehicles[veh_id]["absolute_position"] < self.scenario.length:
-                    # closest vehicle behind the merge in node
-                    non_merge_vehicle_pos = [(veh_id, self.vehicles[veh_id]["absolute_position"])
-                                             for veh_id in self.ids if "merge" not in veh_id]
-                    lag_id = max(non_merge_vehicle_pos, key=lambda x: x[1] % self.scenario.length)[0]
-
-                    # number of loops performed by the lagging vehicle
-                    num_loops = int(self.vehicles[lag_id]["absolute_position"] / self.scenario.length)
-
-                    self.vehicles[veh_id]["absolute_position"] += (num_loops + 1) * self.scenario.length
-
-                abs_pos.append((veh_id, self.vehicles[veh_id]["absolute_position"]))
-
-            # merging vehicles outside the ring have absolute positions equal to some constant plus their relative
-            # position on the given edge
+            if "merge" in veh_id:
+                merge_veh.append((veh_id, self.vehicles[veh_id]["absolute_position"]))
             else:
-                if self.vehicles[veh_id]["edge"] == "merge_in":
-                    abs_pos.append((veh_id, 1000 * self.scenario.length + self.vehicles[veh_id]["position"]))
-                else:
-                    abs_pos.append((veh_id, 1000 * self.scenario.length + self.vehicles[veh_id]["position"] +
-                                    self.scenario.merge_in_len))
+                non_merge_veh.append((veh_id, self.vehicles[veh_id]["absolute_position"]))
 
-        # sort absolute positions and collect ids
-        abs_pos.sort(key=lambda tup: tup[1])
-        sorted_ids = [tup[0] for tup in abs_pos]
+        merge_veh.sort(key=lambda tup: tup[1])
+        non_merge_veh.sort(key=lambda tup: tup[1])
+
+        sorted_merge_ids = [tup[0] for tup in merge_veh]
+        sorted_non_merge_ids = [tup[0] for tup in non_merge_veh]
+
+        # the sorted ids consists of the sorted ids of the non-merging vehicles, followed by the sorted ids of the
+        # merging vehicles
+        sorted_ids = sorted_non_merge_ids + sorted_merge_ids
 
         return sorted_ids
 
