2c9a41dfb48ea8c3a011b93ef7739935088543c2
Cathy Wu <cathywu@eecs.berkeley.edu>
2017-09-13 16:08:19 -0700

    make config file into a template, since everyone will have their own

--- cistar_dev/cistar/core/config.py
+++
@@ -1,5 +0,0 @@
-"""
-Contains config variables unique to the user
-"""
-
-PYTHON_COMMAND = "/Users/kanaad/anaconda2/envs/rllab3/bin/python3.5"
---
+++ cistar_dev/cistar/core/config.template.py
@@ -0,0 +1,5 @@
+"""
+Contains config variables unique to the user
+"""
+
+PYTHON_COMMAND = "python"
