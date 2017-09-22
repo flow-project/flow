3a5c97f223296e84ae63a3d75a0877267aed280d
Eugene Vinitsky <eugenevinitsky@airbears2-10-142-36-254.airbears2.1918.berkeley.edu>
2017-09-22 13:00:54 -0700

    Revert "removed missing environments"
    This reverts commit 3fa212034c471131272485ed98891bed39b45e92.

--- cistar/envs/__init__.py
+++ cistar/envs/__init__.py
@@ -1,4 +1,5 @@
 from cistar.envs.two_intersection import TwoIntersectionEnvironment
+from cistar.envs.two_intersection_multi import TwoIntersectionMultiAgentEnvironment
 from cistar.envs.loop_accel import *
 from cistar.envs.lane_changing import *
 from cistar.envs.loop_merges import SimpleLoopMergesEnvironment
