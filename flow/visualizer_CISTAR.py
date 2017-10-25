318d28dfe3fb9bde54c63a4ef0337e7def830731
aboudy <akreidieh@gmail.com>
2017-10-25 16:33:03 -0700

    modified visualizer_CISTAR and added a function to convert emission files to csv files

--- flow/visualizer_CISTAR.py
+++ flow/visualizer_CISTAR.py
@@ -3,17 +3,8 @@ from rllab.sampler.utils import rollout
 import argparse
 import joblib
 import numpy as np
-from rllab.algos.trpo import TRPO
 from matplotlib import pyplot as plt
-from flow.scenarios.loop.loop_scenario import LoopScenario
-from flow.scenarios.loop.gen import CircleGenerator
-from flow.scenarios.figure8.figure8_scenario import Figure8Scenario
-from flow.scenarios.figure8.gen import Figure8Generator
-from flow.scenarios.intersections.intersection_scenario import TwoWayIntersectionScenario
-from flow.scenarios.intersections.gen import TwoWayIntersectionGenerator
-from flow.scenarios.loop_merges.loop_merges_scenario import LoopMergesScenario
-from flow.scenarios.loop_merges.gen import LoopMergesGenerator
-#from flow.scenarios.braess_paradox.braess_paradox_scenario import BraessParadoxScenario
+from flow.core.util import emission_to_csv
 
 import pickle
 
@@ -32,12 +23,9 @@ if __name__ == "__main__":
                         help='Flag for using sumo-gui vs sumo binary')
     parser.add_argument('--run_long', type=float, default=1,
                         help='Number by which to increase max_path_length')
-    parser.add_argument('--loop_length', type=float, default=230,
-                        help='Length of loop over which to simulate')
-    parser.add_argument('--radius_ring', type=float, default=30,
-                        help='Radius of the ring in the case of figure 8 scenarios')
-    parser.add_argument('--scenario_type', type=str, default='loop',
-                        help='type of scenario being implemented ("loop","figure8", or "intersection"')
+    parser.add_argument('--emission_to_csv', type=bool, default=True,
+                        help='Specifies whether to convert the emission file '
+                             'created by sumo into a csv file')
 
     args = parser.parse_args()
 
@@ -45,59 +33,47 @@ if __name__ == "__main__":
     policy = data['policy']
     baseline = data['baseline']
     env = data['env']
-    # algo = data['algo']
-    algo = TRPO(
-        env=env,
-        policy=policy,
-        baseline=baseline,
-        batch_size=15000,
-        max_path_length=1500,
-        n_itr=2000,
-        # whole_paths=True,
-        discount=0.999,
-        step_size=0.01,
-    )
+
+    # extract the flow environment
+    unwrapped_env = env._wrapped_env._wrapped_env.env.unwrapped
 
     # Input
-    unwrapped_env = env._wrapped_env.env.unwrapped
     if unwrapped_env.obs_var_labels:
         obs_vars = unwrapped_env.obs_var_labels
     else:
-        obs_vars = ["Velocity"] # , "Fuel", "Distance"
+        obs_vars = []
 
     # Recreate experiment params
     vehicles = unwrapped_env.vehicles
     tot_cars = vehicles.num_vehicles
     rl_cars = vehicles.num_rl_vehicles
-    num_itr = algo.n_itr
     max_path_length = int(np.floor(env.horizon*args.run_long))
     flat_obs = env._wrapped_env.observation_space.flat_dim
     num_obs_var = flat_obs / tot_cars
 
+    # TODO: can we do this in a robust way?
     # Recreate the sumo scenario, change the loop length
     scenario = unwrapped_env.scenario
     exp_tag = scenario.name
     net_params = scenario.net_params
     cfg_params = scenario.initial_config
     initial_config = scenario.initial_config
-    if args.scenario_type == 'figure8':
-        net_params.additional_params["radius_ring"] = args.radius_ring
-        scenario = Figure8Scenario(exp_tag, Figure8Generator, vehicles, net_params, initial_config=initial_config)
-    elif args.scenario_type == 'loop':
-        net_params.additional_params["length"] = args.loop_length
-        scenario = LoopScenario(exp_tag, CircleGenerator, vehicles, net_params, initial_config=initial_config)
-    elif args.scenario_type == 'loop_merges':
-        scenario = LoopMergesScenario(exp_tag, LoopMergesGenerator, vehicles, net_params,initial_config=initial_config)
-    # elif args.scenario_type == 'braess_paradox':
-    #     scenario = BraessParadoxScenario(exp_tag, type_params, net_params, cfg_params, initial_config=initial_config)
-    elif args.scenario_type == 'intersection':
-        scenario = TwoWayIntersectionScenario(exp_tag, TwoWayIntersectionGenerator, vehicles, net_params,
-                                              initial_config=initial_config)
+
+    scenario_class = scenario.__class__
+    generator_class = scenario.generator_class
+
     env._wrapped_env.scenario = scenario
 
-    # Set sumo to make a video 
-    sumo_params = unwrapped_env.sumo_params
+    # update the max path length  # FIXME: still not working
+    new_max_path_length = int(np.floor(env.horizon * args.run_long))
+    # env_params.additional_params["num_steps"] = new_max_path_length
+
+    # specify an emission path for sumo to generate an emission file for the
+    # rollout
     unwrapped_env.emission_path = "./test_time_rollout/"
+
+    # Set sumo to make a video
+    sumo_params = unwrapped_env.sumo_params
     sumo_binary = 'sumo-gui' if args.use_sumogui else 'sumo'
     unwrapped_env.restart_sumo(sumo_params, sumo_binary=sumo_binary)
 
@@ -105,63 +81,98 @@ if __name__ == "__main__":
     all_obs = np.zeros((args.num_rollouts, max_path_length, flat_obs))
     all_rewards = np.zeros((args.num_rollouts, max_path_length))
     for j in range(args.num_rollouts):
-        path = rollout(env, policy, max_path_length=max_path_length, animated=False, speedup=1)
-        obs = path['observations']  # length of rollout x flattened observation
-        try:
-            all_obs[j] = obs
-            all_rewards[j] = path["rewards"]
-        except ValueError:
-            pass
-        logging.info("\n Done: {0} / {1}, {2}%".format(j+1, args.num_rollouts, (j+1) / args.num_rollouts))
+        # run a single rollout of the experiment
+        path = rollout(env, policy,
+                       max_path_length=max_path_length,
+                       animated=False, speedup=1)
 
-    # export observations in a pickle file
+        # collect the observations and rewards from the rollout
+        new_obs = path['observations']
+        all_obs[j, :new_obs.shape[0], :new_obs.shape[1]] = new_obs
+        new_rewards = path['rewards']
+        all_rewards[j, :len(new_rewards)] = new_rewards
+
+        logging.info("\n Done: {0} / {1}, {2}%".format(
+            j+1, args.num_rollouts, (j+1) / args.num_rollouts * 100))
+
+    # export observations to a pickle file
     output_filename = 'observations.pkl'
     output = open(output_filename, 'wb')
     pickle.dump(all_obs, output)
     output.close()
 
-    # export rewards in a pickle file
+    # export rewards to a pickle file
     output_filename = 'rewards.pkl'
     output = open(output_filename, 'wb')
     pickle.dump(all_rewards, output)
     output.close()
 
-    # TODO: savefig doesn't like making new directories
-    # Make a separate figure for each observation variable
+    # ensure that a reward_plots folder exists in the directory, and if not,
+    # create one
+    if not os.path.exists("plots"):
+        os.makedirs("plots")
 
-    if not os.path.exists("visualizer"):
-        os.makedirs("visualizer")
+    # create an array of time
+    time_step = sumo_params.time_step
+    t = np.arange(max_path_length) * time_step
 
-    for obs_var_idx in range(len(obs_vars)):
-        obs_var = obs_vars[obs_var_idx]
+    for obs_var_idx in range(int(num_obs_var)):
+        if len(obs_vars) < obs_var_idx + 1:
+            obs_var = "Observation {0}".format(obs_var_idx)
+        else:
+            obs_var = obs_vars[obs_var_idx]
+
+        # plot mean value for observation for each vehicle across rollouts
         plt.figure()
         for car in range(tot_cars):
-            # mean is horizontal, across num_rollouts
             center = np.mean(all_obs[:, :, tot_cars*obs_var_idx + car], axis=0)
-            # stdev = np.std(carvels[car], axis=1)
-            plt.plot(range(max_path_length), center, lw=2.0, label='Car {}'.format(car))
-            # plt.plot(range(max_path_length), center + stdev, lw=2.0, c='black', ls=':')
-            # plt.plot(range(max_path_length), center - stdev, lw=2.0, c='black', ls=':')
+            plt.plot(range(max_path_length), center, lw=2.0,
+                     label='Veh {}'.format(car))
         plt.ylabel(obs_var, fontsize=15)
-        plt.xlabel("Rollout/Path Length", fontsize=15)
-        plt.title("Cars {0} / {1} Itr {2}".format(rl_cars, tot_cars, num_itr), fontsize=16)
+        plt.xlabel("time (s)", fontsize=15)
+        plt.title("{2}, Autonomous Penetration: {0}/{1}".
+                  format(rl_cars, tot_cars, obs_var), fontsize=16)
         plt.legend(loc=0)
-        plt.savefig("visualizer/{0}_{1}.png".format(args.plotname, obs_var), bbox="tight")
 
-        # plot mean values across all cars
-        car_mean = np.mean(np.mean(all_obs[:, :, tot_cars*obs_var_idx:tot_cars*(obs_var_idx + 1)], 
-                        axis=0), axis = 1)
+        # save the plot in the "plots" directory
+        plt.savefig("plots/{0}_{1}.png".format(args.plotname, obs_var),
+                    bbox="tight")
+
+        # plot mean values for the observations across all vehicles and all
+        # rollouts
+        car_mean = np.mean(np.mean(
+            all_obs[:, :, tot_cars*obs_var_idx:tot_cars*(obs_var_idx + 1)],
+            axis=0), axis=1)
         plt.figure()
-        plt.plot(range(max_path_length), car_mean)
+        plt.plot(t, car_mean)
         plt.ylabel(obs_var, fontsize=15)
-        plt.xlabel("Rollout/Path Length", fontsize=15)
-        plt.title("Cars {0} / {1} Itr {2}".format(rl_cars, tot_cars, num_itr), fontsize=16)
-        plt.savefig("visualizer/{0}_{1}_mean.png".format(args.plotname, obs_var), bbox="tight")
+        plt.xlabel("time (s)", fontsize=15)
+        plt.title("Mean {2}, Autonomous Penetration: {0}/{1}".
+                  format(rl_cars, tot_cars, obs_var), fontsize=16)
+
+        # save the plot in the "plots" directory
+        plt.savefig("plots/{0}_{1}_mean.png".format(args.plotname, obs_var),
+                    bbox="tight")
 
     # Make a figure for the mean rewards over the course of the rollout
+    mean_reward = np.mean(all_rewards, axis=0)
+
     plt.figure()
-    plt.plot(range(max_path_length), np.mean(all_rewards, axis=0), lw=2.0)
-    plt.ylabel("Reward", fontsize=15)
-    plt.xlabel("Rollout/Path Length", fontsize=15)
-    plt.title("Cars {0} / {1} Itr {2}".format(rl_cars, tot_cars, num_itr), fontsize=16)
-    plt.savefig("visualizer/{0}_reward.png".format(args.plotname), bbox="tight")
+    plt.plot(t, mean_reward, lw=2.0)
+    plt.ylabel("reward", fontsize=15)
+    plt.xlabel("time (s)", fontsize=15)
+    plt.title("Reward, Autonomous Penetration: {0}/{1}".
+              format(rl_cars, tot_cars), fontsize=16)
+
+    # save the rewards plot in the "reward_plots" directory
+    plt.savefig("plots/{0}_reward.png".format(args.plotname), bbox="tight")
+
+    # if prompted, convert the emission file into a csv file
+    if args.emission_to_csv:
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        emission_filename = "{0}-emission.xml".format(scenario.name)
+
+        emission_path = \
+            "{0}/test_time_rollout/{1}".format(dir_path, emission_filename)
+
+        emission_to_csv(emission_path)
