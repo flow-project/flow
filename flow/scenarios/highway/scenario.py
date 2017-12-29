5470beb0699654eb6df3d1f256639c7323f5a924
aboudy <akreidieh@gmail.com>
2017-12-29 03:02:00 -0800

    created highway scenario

--- flow/scenarios/highway/scenario.py
+++ flow/scenarios/highway/scenario.py
@@ -2,7 +2,7 @@ from flow.scenarios.base_scenario import Scenario
 from flow.core.params import InitialConfig
 
 
-class LoopScenario(Scenario):
+class HighwayScenario(Scenario):
     def __init__(self, name, generator_class, vehicles, net_params,
                  initial_config=InitialConfig()):
         """
@@ -12,20 +12,16 @@ class LoopScenario(Scenario):
         See Scenario.py for description of params.
         """
         if "length" not in net_params.additional_params:
-            raise ValueError("length of circle not supplied")
+            raise ValueError("length of highway not supplied")
         self.length = net_params.additional_params["length"]
 
         if "lanes" not in net_params.additional_params:
-            raise ValueError("lanes of circle not supplied")
+            raise ValueError("lanes of highway not supplied")
         self.lanes = net_params.additional_params["lanes"]
 
         if "speed_limit" not in net_params.additional_params:
-            raise ValueError("speed limit of circle not supplied")
+            raise ValueError("speed limit of highway not supplied")
         self.speed_limit = net_params.additional_params["speed_limit"]
-
-        if "resolution" not in net_params.additional_params:
-            raise ValueError("resolution of circle not supplied")
-        self.resolution = net_params.additional_params["resolution"]
 
         super().__init__(name, generator_class, vehicles, net_params,
                          initial_config=initial_config)
@@ -34,9 +30,6 @@ class LoopScenario(Scenario):
         """
         See parent class
         """
-        edgelen = self.length / 4
-
-        edgestarts = [("bottom", 0), ("right", edgelen),
-                      ("top", 2 * edgelen), ("left", 3 * edgelen)]
+        edgestarts = [("highway", 0)]
 
         return edgestarts
