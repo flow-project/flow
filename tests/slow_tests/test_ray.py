c228de73e06761d75aea96067f636af881753089
Cathy Wu <cathywu@eecs.berkeley.edu>
2018-01-02 13:17:13 -0800

    Hierarchical policy example (#226)
    ## Summary
    
    Prerequisite PR: #225.
    
    - `examples/rllib/ring_two_level_policy.py`: Example usage of two-level (hierarchical) policies in rllib (currently only in `cathywu/ray:testing` and `cathywu/ray:hierarchy`).
    - Refactor `stabilizing_the_ring` example for re-use in integration tests and two-level policy usage example.
    - Added integration test for two-level policy in rllib.
    - Somewhat more tuned parameters for `stabilizing_the_ring` rllib example.
    
    -----
    
    * Refactored ray dependencies and examples for integration tests; added integration test for an end-to-end ray (rllib) run using stabilizing_the_ring example.
    
    * separated tests into fast and slow tests, now that we have our first integration test
    
    * add ray install to travis
    
    * fix ray install for travis
    
    * fix ray install for travis
    
    * set up conda in travis
    
    * link ray in docker and add ray to pythonpath
    
    * oops my bad
    
    * not sure..
    
    * linux version
    
    * remove docker
    
    * non-interactive linux setup for SUMO
    
    * add conda
    
    * switch order to see why ray install is failing
    
    * Partial implementation of two-level fcnet example
    
    * missing init
    
    * sigh
    
    * oops
    
    * BROKEN passing function through options
    
    * install ray from wheels
    
    * restructure config params, use cloudpickle
    
    * my bad
    
    * add ray testing branch
    
    * Manually move ray clone to site-packages
    
    * path
    
    * rllib
    
    * make sure
    
    * Cleanup
    
    * integration test for two-level policy
    
    * change env name to permit re-registration
    
    * missing test file
    
    * tensorflow
    
    * refactor to support multiple ray tests
    
    * add conda and opencv
    
    * minor edit
    
    * conda switches python versions?
    
    * install opencv without conda
    
    * download sumo build directly
    
    * fix
    
    * chmod
    
    * different ubuntu setup scripts
    
    * sumo build - ubuntu14.04 version
    
    * typo
    
    * add netconvert
    
    * python path
    
    * add data, indent stuff
    
    * wrong dir
    
    * opencv
    
    * py3.5 -> py3.6 (miniconda) to support opencv dependency of ray
    
    * updated travis
    
    * cleanup
    
    * cleanup, remove apt-get update; fix test?
    
    * remove linux setup
    
    * typo
    
    * integration test fix
    
    * organize examples by usage
    
    * fix tests
    
    * rename two level policy example
    
    * Update params.py
    
    * Revised stabilizing the ring example
    
    * stabilizing the ring params
    
    * merge reverted this change
    
    * pass params was missing version

--- tests/slow_tests/test_ray.py
+++ tests/slow_tests/test_ray.py
@@ -1,18 +1,23 @@
+from importlib import reload
 import os
 import unittest
 
+import ray
+import ray.rllib.ppo as ppo
+import ray.tune.registry as registry
+
+from examples.rllib.stabilizing_the_ring import make_create_env
+
 
 class TestRay(unittest.TestCase):
-    """
-    Integration test for ray/rllib
-    """
+    # def setUp(self):
+    #     # reload modules, required upon repeated ray.init()
 
-    def runTest(self):
-        import ray
-        import ray.rllib.ppo as ppo
-        from ray.tune.registry import get_registry, register_env as register_rllib_env
-        from examples.rllib.stabilizing_the_ring import create_env
-
+    def test_ray(self):
+        """
+        Integration test for ray/rllib + flow
+        """
+        # Test 1: test_two_level_ray
         config = ppo.DEFAULT_CONFIG.copy()
         horizon = 500
         num_workers = 3
@@ -23,17 +28,57 @@ class TestRay(unittest.TestCase):
         config["model"].update({"fcnet_hiddens": [3, 3]})
         config["gamma"] = 0.999
         config["horizon"] = horizon
+
         flow_env_name = "WaveAttenuationPOEnv"
-        env_name = flow_env_name+'-v0'
+        create_env, env_name = make_create_env(flow_env_name, 0)
 
         # Register as rllib env
-        register_rllib_env(env_name, create_env)
+        registry.register_env(env_name, create_env)
 
-        alg = ppo.PPOAgent(env=env_name, registry=get_registry(), config=config)
-        for i in range(1):
+        alg = ppo.PPOAgent(env=env_name, registry=registry.get_registry(),
+                           config=config)
+        for i in range(2):
             alg.train()
             checkpoint_path = alg.save()
             self.assertTrue("%s.index" % os.path.exists(checkpoint_path))
+
+        # Test 2: test_two_level_ray
+        # Integration test for two-level fcnet policy
+        # FIXME(cathywu) ray restart currently not supported, so need to tie
+        # integration tests together for the time being.
+        # reload(ppo)
+        # reload(registry)
+        import cloudpickle
+        config = ppo.DEFAULT_CONFIG.copy()
+        horizon = 500
+        num_workers = 3
+        # ray.init(num_cpus=num_workers, redirect_output=True)
+        config["num_workers"] = num_workers
+        config["timesteps_per_batch"] = horizon * num_workers
+        config["num_sgd_iter"] = 2
+        config["gamma"] = 0.999
+        config["horizon"] = horizon
+
+        config["model"].update(
+            {"fcnet_hiddens": [[5, 3]] * 2})
+        config["model"]["user_data"] = {}
+        config["model"]["user_data"].update({"num_subpolicies": 2,
+                                             "fn_choose_subpolicy": list(
+                                                 cloudpickle.dumps(lambda x: 0))})
+
+        flow_env_name = "WaveAttenuationPOEnv"
+        create_env, env_name = make_create_env(flow_env_name, 1)
+
+        # Register as rllib env
+        registry.register_env(env_name, create_env)
+
+        alg = ppo.PPOAgent(env=env_name, registry=registry.get_registry(),
+                           config=config)
+        for i in range(1):
+            alg.train()
+
+    def tearDown(self):
+        ray.worker.cleanup()
 
 
 if __name__ == '__main__':
