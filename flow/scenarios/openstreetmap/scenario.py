40ba112f41e92dc49d58747259693cbd32941eca
aboudy <akreidieh@gmail.com>
2018-02-01 00:35:18 -0800

    speedups to starting position methods

--- flow/scenarios/openstreetmap/scenario.py
+++ flow/scenarios/openstreetmap/scenario.py
@@ -34,7 +34,7 @@ class OpenStreetMapScenario(Scenario):
         self.length = 0
 
         edgestarts = []
-        for edge_id in self._edges:
+        for edge_id in self._edge_list:
             # the current edge starts (in 1D position) where the last edge ended
             edgestarts.append((edge_id, self.length))
             # increment the total length of the network with the length of the
