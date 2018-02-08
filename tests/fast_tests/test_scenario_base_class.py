28af80a0e4fea5db750efa553f2204001261fbb1
aboudy <akreidieh@gmail.com>
2018-02-08 13:39:23 -0800

    removed print

--- tests/fast_tests/test_scenario_base_class.py
+++ tests/fast_tests/test_scenario_base_class.py
@@ -726,7 +726,6 @@ class TestNextPrevEdge(unittest.TestCase):
         """
         env, scenario = highway_exp_setup()
         prev_edge = scenario.prev_edge(env.scenario.get_edge_list()[0], 0)
-        print(prev_edge, len(prev_edge))
         self.assertTrue(len(prev_edge) == 0)
 
 
