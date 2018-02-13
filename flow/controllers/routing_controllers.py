8adf67e7561ef69eae1ab2d14e23ff9648758b93
AboudyKreidieh <akreidieh@gmail.com>
2018-02-12 19:43:29 -0800

    working bay bridge and bottleneck examples

--- flow/controllers/routing_controllers.py
+++ flow/controllers/routing_controllers.py
@@ -19,3 +19,24 @@ class ContinuousRouter(BaseRouter):
             new_route = None
 
         return new_route
+
+
+class BayBridgeRouter(ContinuousRouter):
+    """
+    Extension to the Continuous Router. Assists in choosing routes in select
+    cases.
+    """
+    def choose_route(self, env):
+        """
+        See parent class
+        """
+        edge = env.vehicles.get_edge(self.veh_id)
+        lane = env.vehicles.get_lane(self.veh_id)
+
+        if edge == "183343422" and lane in [2] \
+                or edge == "124952179" and lane in [1, 2]:
+            new_route = env.available_routes[edge + "_1"]
+        else:
+            new_route = super().choose_route(env)
+
+        return new_route
