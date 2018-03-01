5f095a69e4a7c94849a2202bf225d9cadc7c99b7
eugenevinitsky <vinitsky.eugene@gmail.com>
2018-02-28 17:31:28 -0800

    blah

--- examples/rllab/bottlenecks.py
+++ examples/rllab/bottlenecks.py
@@ -21,7 +21,7 @@ from rllab.algos.trpo import TRPO
 from rllab.baselines.linear_feature_baseline import LinearFeatureBaseline
 from rllab.policies.gaussian_mlp_policy import GaussianMLPPolicy
 
-SCALING = 4
+SCALING = 1
 NUM_LANES = 4*SCALING  # number of lanes in the widest highway
 
 logging.basicConfig(level=logging.INFO)
@@ -133,17 +133,17 @@ def run_task(*_):
     algo.train()
 
 exp_tag = "BottleNeckVerySmall"  # experiment prefix
-for seed in [1, 2, 3, 4, 5, 6]:  # , 1, 5, 10, 73]:
+for seed in [1]:  # , 1, 5, 10, 73]:
     run_experiment_lite(
         run_task,
         # Number of parallel workers for sampling
-        n_parallel=8,
+        n_parallel=1,
         # Only keep the snapshot parameters for the last iteration
         snapshot_mode="all",
         # Specifies the seed for the experiment. If this is not provided, a
         # random seed will be used
         seed=seed,
-        mode="ec2",
+        mode="local",
         exp_prefix=exp_tag,
         # python_command="/home/aboudy/anaconda2/envs/rllab-multiagent/bin/python3.5"
         # plot=True,
