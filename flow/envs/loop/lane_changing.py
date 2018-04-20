9040eb7d17956ea0d98d8bc0ab3c9c04d08cf7ea
AboudyKreidieh <akreidieh@gmail.com>
2018-04-19 20:03:57 -0700

    removed loop_merge

--- flow/envs/loop/lane_changing.py
+++ flow/envs/loop/lane_changing.py
@@ -5,6 +5,13 @@ from gym.spaces.box import Box
 from gym.spaces.tuple_space import Tuple
 import numpy as np
 
+ADDITIONAL_ENV_PARAMS = {
+    "max_accel": 3,
+    "max_decel": 3,
+    "lane_change_duration": 5,
+    "target_velocity": 10,
+}
+
 
 class LaneChangeAccelEnv(Env):
     """Environment used to train autonomous vehicles to improve traffic flows
@@ -36,10 +43,19 @@ class LaneChangeAccelEnv(Env):
     A rollout is terminated if the time horizon is reached or if two vehicles
     collide into one another.
     """
+
+    def __init__(self, env_params, sumo_params, scenario):
+        for p in ADDITIONAL_ENV_PARAMS.keys():
+            if p not in env_params.additional_params:
+                raise KeyError('Environment parameter "{}" not supplied'.
+                               format(p))
+
+        super().__init__(env_params, sumo_params, scenario)
+
     @property
     def action_space(self):
-        max_decel = self.env_params.max_decel
-        max_accel = self.env_params.max_accel
+        max_decel = self.env_params.additional_params["max_decel"]
+        max_accel = self.env_params.additional_params["max_accel"]
 
         lb = [-abs(max_decel), -1] * self.vehicles.num_rl_vehicles
         ub = [max_accel, 1] * self.vehicles.num_rl_vehicles
@@ -73,11 +89,6 @@ class LaneChangeAccelEnv(Env):
         return reward
 
     def get_state(self):
-        # specify observed vehicles
-        if self.vehicles.num_rl_vehicles > 0:
-            for veh_id in self.vehicles.get_human_ids():
-                self.vehicles.set_observed(veh_id)
-
         return np.array([[self.vehicles.get_speed(veh_id),
                           self.vehicles.get_absolute_position(veh_id),
                           self.vehicles.get_lane(veh_id)]
@@ -93,7 +104,8 @@ class LaneChangeAccelEnv(Env):
 
         # represents vehicles that are allowed to change lanes
         non_lane_changing_veh = \
-            [self.time_counter <= self.lane_change_duration
+            [self.time_counter <=
+             self.env_params.additional_params["lane_change_duration"]
              + self.vehicles.get_state(veh_id, 'last_lc')
              for veh_id in sorted_rl_ids]
         # vehicle that are not allowed to change have their directions set to 0
@@ -103,66 +115,61 @@ class LaneChangeAccelEnv(Env):
         self.apply_acceleration(sorted_rl_ids, acc=acceleration)
         self.apply_lane_change(sorted_rl_ids, direction=direction)
 
+    def additional_command(self):
+        # specify observed vehicles
+        if self.vehicles.num_rl_vehicles > 0:
+            for veh_id in self.vehicles.get_human_ids():
+                self.vehicles.set_observed(veh_id)
 
-class LaneChangeOnlyEnv(LaneChangeAccelEnv):
-    """Environment used to train autonomous vehicles to improve traffic flows
-    when lane-change actions are only permitted by the rl agent.
+
+class LaneChangeAccelPOEnv(Env):
+    """Partially observable variant of LaneChangeAccelEnv.
 
     States
     ------
-    (blank)
+    The state consists of the velocities, absolute position, and lane index of
+    all vehicles in the network. This assumes a constant number of vehicles.
 
     Actions
     -------
-    (blank)
+    Actions consist of:
+    - a (continuous) acceleration from -abs(max_decel) to max_accel, specified
+      in env_params
+    - a (continuous) lane-change action from -1 to 1, used to determine the
+      lateral direction the vehicle will take.
+    Lane change actions are performed only if the vehicle has not changed lanes
+    for the lane change duration specified in env_params.
 
     Rewards
     -------
-    (blank)
+    The reward function is the two-norm of the distance of the speed of the
+    vehicles in the network from a desired speed, combined with a penalty to
+    discourage excess lane changes by the rl vehicle.
 
     Termination
     -----------
     A rollout is terminated if the time horizon is reached or if two vehicles
     collide into one another.
     """
-    def __init__(self, env_params, sumo_params, scenario):
-
-        super().__init__(env_params, sumo_params, scenario)
-
-        # acceleration controller used for rl cars
-        self.rl_controller = dict()
-
-        for veh_id in self.vehicles.get_rl_ids():
-            acc_params = env_params.get_additional_param("rl_acc_controller")
-            self.rl_controller[veh_id] = \
-                acc_params[0](veh_id=veh_id, **acc_params[1])
 
     @property
-    def action_space(self):
-        return Box(low=-1, high=1, shape=(self.vehicles.num_rl_vehicles,))
+    def observation_space(self):
+        num_lanes = max(self.scenario.num_lanes(edge)
+                        for edge in self.scenario.get_edge_list())
 
-    def _apply_rl_actions(self, actions):
-        direction = actions
+        return Box(low=-float("inf"),
+                   high=float("inf"),
+                   shape=(4 * self.vehicles.num_rl_vehicles * num_lanes,),
+                   dtype=np.float32)
 
-        # re-arrange actions according to mapping in observation space
-        sorted_rl_ids = [veh_id for veh_id in self.sorted_ids
-                         if veh_id in self.vehicles.get_rl_ids()]
+    def get_state(self):
+        return np.array([[self.vehicles.get_speed(veh_id),
+                          self.vehicles.get_absolute_position(veh_id),
+                          self.vehicles.get_lane(veh_id)]
+                         for veh_id in self.sorted_ids])
 
-        # represents vehicles that are allowed to change lanes
-        non_lane_changing_veh = \
-            [self.time_counter <= self.lane_change_duration
-             + self.vehicles[veh_id]['last_lc']
-             for veh_id in sorted_rl_ids]
-        # vehicle that are not allowed to change have their directions set to 0
-        direction[non_lane_changing_veh] = \
-            np.array([0] * sum(non_lane_changing_veh))
-
-        self.apply_lane_change(sorted_rl_ids, direction=direction)
-
-        # collect the accelerations for the rl vehicles as specified by the
-        # human controller
-        acceleration = []
-        for veh_id in sorted_rl_ids:
-            acceleration.append(self.rl_controller[veh_id].get_action(self))
-
-        self.apply_acceleration(sorted_rl_ids, acc=acceleration)
+    def additional_command(self):
+        # specify observed vehicles
+        if self.vehicles.num_rl_vehicles > 0:
+            for veh_id in self.vehicles.get_human_ids():
+                self.vehicles.set_observed(veh_id)
