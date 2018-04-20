de3fc8fff7f381ab44e9c85ce52c776c6edc829a
eugenevinitsky <vinitsky.eugene@gmail.com>
2018-04-20 15:54:11 -0700

    fixed utils for rllib test

--- flow/core/util.py
+++ flow/core/util.py
@@ -26,6 +26,8 @@ from flow.core.params import SumoCarFollowingParams, SumoLaneChangeParams
 
 from flow.controllers.rlcontroller import RLController
 from flow.controllers.car_following_models import *
+from flow.core.params import InFlows
+from flow.controllers.velocity_controllers import *
 from flow.controllers.lane_change_controllers import *
 from flow.controllers.routing_controllers import ContinuousRouter
 from flow.scenarios.loop.loop_scenario import LoopScenario
@@ -38,7 +40,7 @@ class NameEncoder(json.JSONEncoder):
     """
     Custom encoder used to generate ``flow_params.json``
     Extends ``json.JSONEncoder``.
-    
+
     Attributes
     ----------
     allowed_types : list
@@ -47,16 +49,16 @@ class NameEncoder(json.JSONEncoder):
     """
 
     allowed_types = [dict, list, tuple, str, int, float, bool, type(None)]
-    
+
     def default(self, obj):
         """
         Default encoder (required to extend ``JSONEncoder``)
-        
+
         Parameters
         ----------
         obj : Object
             Object to encode
-        
+
         Returns
         -------
         Object
@@ -84,14 +86,14 @@ def rllib_logger_creator(result_dir, env_name, loggerfn):
 def unstring_flow_params(flow_params):
     """
     Summary
-    
+
     Parameters
     ----------
     flow_params : dict
         Large dictionary of flow parameters for experiment,
         passed in to ``make_create_env`` and used to create
         ``flow_params.json`` file used by exp visualizer
-    
+
     Returns
     -------
     dict
@@ -104,7 +106,9 @@ def unstring_flow_params(flow_params):
 
     veh_params = flow_params['veh']
     better_veh_params = [eval_veh_params(veh_param) for veh_param in veh_params]
+    better_net_params = eval_net_params(flow_params)
     better_params['veh'] = better_veh_params
+    better_params['net'] = better_net_params
 
     if 'additional_params' in flow_params['env']:
         if 'scenario_type' in flow_params['env']['additional_params']:
@@ -114,22 +118,63 @@ def unstring_flow_params(flow_params):
     return better_params
 
 
+def eval_net_params(flow_params):
+    """
+        Evaluates net parameters, since params like Inflows can't be
+        serialized and output to JSON.
+
+        Parameters
+        ----------
+        orig_params : dict
+            Original flow parameters, read in from ``flow_params.json``
+
+        Returns
+        -------
+        dict
+            Evaluated net params with instantiated inflow
+        """
+
+    better_params = flow_params.copy()
+    inflow = InFlows()
+    new_inflow_list = []
+    if 'in_flows' in flow_params['net']:
+        inflow_obj = flow_params['net']['in_flows']['_InFlows__flows']
+        for obj in inflow_obj:
+            temp = {}
+            for key in obj.keys():
+                if key == 'vtype':
+                    temp['veh_type'] = obj[key]
+                elif key == 'begin':
+                    temp['edge'] = str(obj[key])
+                elif key =='depart_speed':
+                    temp['departSpeed'] = obj[key]
+                elif key == 'probability' or key =='departSpeed' or \
+                    key == 'departLane' or key == 'vehsPerHour':
+                    temp[key] = obj[key]
+            new_inflow_list.append(temp)
+        # add the created inflow to the inflow container
+        [inflow.add(**inflow_i) for inflow_i in new_inflow_list]
+        better_params['net']['in_flows'] = inflow
+
+    return better_params['net']
+
+
 def eval_veh_params(orig_params):
     """
     Evaluates vehicle parameters, since params like IDMController can't be
     serialized and output to JSON. Thus, the JSON file stores those as
     their names, with string 'IDMController' instead of the object
-    ``<flow.controllers.car_following_models.IDMController``. ``util.py`` 
-    imports required car-following models, lane-change controllers, 
+    ``<flow.controllers.car_following_models.IDMController``. ``util.py``
+    imports required car-following models, lane-change controllers,
     routers, and SUMO parameters. This function evaluates those names
     and returns a dict with the actual objects (evaluated) instead of
     their names.
-    
+
     Parameters
     ----------
     orig_params : dict
         Original vehicle parameters, read in from ``flow_params.json``
-    
+
     Returns
     -------
     dict
@@ -138,15 +183,15 @@ def eval_veh_params(orig_params):
     """
 
     new_params = orig_params.copy()
-    
+
     if 'acceleration_controller' in new_params:
-        new_params['acceleration_controller'] = (eval(orig_params['acceleration_controller'][0]), 
+        new_params['acceleration_controller'] = (eval(orig_params['acceleration_controller'][0]),
                                         orig_params['acceleration_controller'][1])
     if 'lane_change_controller' in new_params:
-        new_params['lane_change_controller'] = (eval(orig_params['lane_change_controller'][0]), 
+        new_params['lane_change_controller'] = (eval(orig_params['lane_change_controller'][0]),
                                         orig_params['lane_change_controller'][1])
     if 'routing_controller' in new_params:
-        new_params['routing_controller'] = (eval(orig_params['routing_controller'][0]), 
+        new_params['routing_controller'] = (eval(orig_params['routing_controller'][0]),
                                         orig_params['routing_controller'][1])
     if 'sumo_car_following_params' in new_params:
         cf_params = SumoCarFollowingParams()
@@ -164,12 +209,12 @@ def eval_veh_params(orig_params):
 def get_rllib_params(path):
     """
     Returns rllib experiment parameters, given an experiment result folder
-    
+
     Parameters
     ----------
     path : str
         Path to an rllib experiment result directory
-    
+
     Returns
     -------
     dict
@@ -183,10 +228,10 @@ def get_rllib_params(path):
     gamma = jsondata['gamma']
     horizon = jsondata['horizon']
     hidden_layers = jsondata['model']['fcnet_hiddens']
-    rllib_params = {'gamma':gamma, 
+    rllib_params = {'gamma':gamma,
                     'horizon':horizon,
                     'hidden_layers':hidden_layers}
-    
+
     return rllib_params
 
 def get_rllib_config(path):
@@ -195,30 +240,29 @@ def get_rllib_config(path):
     return jsondata
 
 
-def get_flow_params(path):
+def get_flow_params(config):
     """
-    Returns Flow experiment parameters, given an experiment reuslt folder
-    
+    Returns Flow experiment parameters, given an experiment result folder
+
     Parameters
     ----------
     path : str
         Path to an rllib experiment result directory (``flow_params.json`` is
         in the same folder as ``params.json``)
-    
+
     Returns
     -------
     dict
         Dict of flow parameters, like net_params, env_params, vehicle
         characteristics, etc
     function
-        ``make_create_env`` is the higher-order function passed to 
+        ``make_create_env`` is the higher-order function passed to
         rllib as the environment in which to train
     """
-    
-    flow_params_file = path + '/flow_params.json'
-    flow_params = json.loads(open(flow_params_file).read())
+
+    flow_params = json.loads(config['env_config']['flow_params'])
     flow_params = unstring_flow_params(flow_params)
-    
+
     module_name = 'examples.rllib.' + flow_params['module']
 
     env_module = importlib.import_module(module_name)
