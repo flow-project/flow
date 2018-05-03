c88644e2bda51e561bec8a0875584ee6185db9e9
AboudyKreidieh <akreidieh@gmail.com>
2018-05-03 12:43:04 -0700

    initial changes to rllib run scripts

---
+++ flow/scenarios/__init__.py
@@ -0,0 +1,28 @@
+# base scenario class
+from flow.scenarios.base_scenario import Scenario
+
+# custom generators
+from flow.scenarios.bay_bridge.gen import BayBridgeGenerator
+from flow.scenarios.bottleneck.gen import BottleneckGenerator
+from flow.scenarios.bridge_toll.gen import BBTollGenerator
+from flow.scenarios.figure8.gen import Figure8Generator
+from flow.scenarios.grid.gen import SimpleGridGenerator
+from flow.scenarios.highway.gen import HighwayGenerator
+from flow.scenarios.loop.gen import CircleGenerator
+from flow.scenarios.merge.gen import MergeGenerator
+from flow.scenarios.netfile.gen import NetFileGenerator
+from flow.scenarios.two_loops_one_merging.gen \
+    import TwoLoopOneMergingGenerator
+
+# custom scenarios
+from flow.scenarios.bay_bridge.scenario import BayBridgeScenario
+from flow.scenarios.bottleneck.scenario import BottleneckScenario
+from flow.scenarios.bridge_toll.scenario import BBTollScenario
+from flow.scenarios.figure8.figure8_scenario import Figure8Scenario
+from flow.scenarios.grid.grid_scenario import SimpleGridScenario
+from flow.scenarios.highway.scenario import HighwayScenario
+from flow.scenarios.loop.loop_scenario import LoopScenario
+from flow.scenarios.merge.scenario import MergeScenario
+from flow.scenarios.netfile.scenario import NetFileScenario
+from flow.scenarios.two_loops_one_merging.scenario \
+    import TwoLoopsOneMergingScenario
