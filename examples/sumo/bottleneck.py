a9dac69db2dbfe3f36fcc269caebae044f8c6502
eugenevinitsky <vinitsky.eugene@gmail.com>
2018-05-14 12:16:30 -0700

    updated tests

--- examples/sumo/bottleneck.py
+++ examples/sumo/bottleneck.py
@@ -43,7 +43,8 @@ def bottleneck(flow_rate, horizon, sumo_binary=None):
                              "add_rl_if_exit": False,
                              "disable_tb": DISABLE_TB,
                              "disable_ramp_metering": DISABLE_RAMP_METER}
-    env_params = EnvParams(additional_params=additional_env_params)
+    env_params = EnvParams(horizon=horizon,
+                           additional_params=additional_env_params)
 
     inflow = InFlows()
     inflow.add(veh_type="human", edge="1", vehsPerHour=flow_rate,
