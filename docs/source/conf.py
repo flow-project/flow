1232a8b976d21c0c1cb57f942d9ceaae8629d752
Nishant <nishantkheterpal@gmail.com>
2018-07-13 16:00:14 -0700

    mock imports for autodoc functionality on readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,6 +37,8 @@ napoleon_google_docstring = False
 napoleon_use_param = False
 napoleon_use_ivar = True
 
+autodoc_mock_imports = ["traci", "ray", "rllab"]
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
