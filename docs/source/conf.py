2a3233e213f4f334637e52a005c96f99274e919a
Nishant <nishantkheterpal@gmail.com>
2018-07-18 12:32:07 -0700

    removing sumolib mock import

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,7 +37,7 @@ napoleon_google_docstring = False
 napoleon_use_param = False
 napoleon_use_ivar = True
 
-autodoc_mock_imports = ["traci", "sumolib"]
+autodoc_mock_imports = ["traci"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
