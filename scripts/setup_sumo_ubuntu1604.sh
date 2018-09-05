d14e3f7d982f645eb2329b7cb085a7d7edc6e20c
AboudyKreidieh <akreidieh@gmail.com>
2018-09-04 22:29:28 -0700

    moved location of binaries to somewhere global

--- scripts/setup_sumo_ubuntu1604.sh
+++ scripts/setup_sumo_ubuntu1604.sh
@@ -9,14 +9,14 @@ sudo apt-get install -y python3-dev
 sudo pip3 install cmake cython
 
 echo "Installing sumo binaries"
-mkdir bin
-pushd bin
+mkdir -p $HOME/sumo_binaries/bin
+pushd $HOME/sumo_binaries/bin
 wget https://akreidieh.s3.amazonaws.com/sumo/flow-0.2.0/binaries-ubuntu1604.tar.xz
 tar -xf binaries-ubuntu1604.tar.xz
 rm binaries-ubuntu1604.tar.xz
 chmod +x *
 popd
-echo 'export PATH=$PATH:'$(pwd)'/bin' >> ~/.bashrc
-echo 'export SUMO_HOME='$(pwd)'/bin' >> ~/.bashrc
+echo 'export PATH=$PATH:$HOME/sumo_binaries/bin' >> ~/.bashrc
+echo 'export SUMO_HOME=$HOME/sumo_binaries/bin' >> ~/.bashrc
 
 source ~/.bashrc
