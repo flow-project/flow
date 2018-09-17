323705e693c99255874650a83aaad36057ae4d22
eugenevinitsky <vinitsky.eugene@gmail.com>
2018-09-16 17:09:45 -0700

    minor fixes to highway class

--- flow/scenarios/base_scenario.py
+++ flow/scenarios/base_scenario.py
@@ -590,6 +590,7 @@ class Scenario(Serializable):
         try:
             return self._edges[edge_id]["length"]
         except KeyError:
+            import ipdb; ipdb.set_trace()
             print('Error in edge length with key', edge_id)
             return -1001
 
