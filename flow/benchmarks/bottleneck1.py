d04dbadc6eadb866eed70f1ab387231d7a9a4ab1
eugenevinitsky <vinitsky.eugene@gmail.com>
2018-09-23 16:21:15 -0700

    added correct inflow values to bottleneck

--- flow/benchmarks/bottleneck1.py
+++ flow/benchmarks/bottleneck1.py
@@ -55,11 +55,11 @@ additional_env_params = {
     "lane_change_duration": 5,
     "max_accel": 3,
     "max_decel": 3,
-    "inflow_range": [1000, 2000]
+    "inflow_range": [1200, 2500]
 }
 
 # flow rate
-flow_rate = 1900 * SCALING
+flow_rate = 2200 * SCALING
 
 # percentage of flow coming out of each lane
 inflow = InFlows()
