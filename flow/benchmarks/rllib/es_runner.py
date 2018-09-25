31cf1016e8bad7eff182ecff1f99a81933c1c767
eugenevinitsky <vinitsky.eugene@gmail.com>
2018-09-25 14:44:50 -0700

    exp params

--- flow/benchmarks/rllib/es_runner.py
+++ flow/benchmarks/rllib/es_runner.py
@@ -48,10 +48,10 @@ if __name__ == "__main__":
             "config": {
                 **config
             },
-            "checkpoint_freq": 5,
+            "checkpoint_freq": 25,
             "max_failures": 999,
             "stop": {"training_iteration": 500},
             "num_samples": 3,
-            # "upload_dir": "s3://bucket"
+            "upload_dir": "s3://public.flow.results/corl_exps/exp_tests3"
         },
     })
