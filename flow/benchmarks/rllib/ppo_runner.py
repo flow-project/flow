7b26a6bb06ffef7335601658f7b662c2d109ef30
eugenevinitsky <vinitsky.eugene@gmail.com>
2018-09-25 21:19:08 -0700

    commit for bottleneck 0 ppo

--- flow/benchmarks/rllib/ppo_runner.py
+++ flow/benchmarks/rllib/ppo_runner.py
@@ -18,7 +18,7 @@ from flow.utils.registry import make_create_env
 from flow.utils.rllib import FlowParamsEncoder
 
 # use this to specify the environment to run
-from flow.benchmarks.figureeight2 import flow_params
+from flow.benchmarks.bottleneck0 import flow_params
 
 # number of rollouts per training iteration
 N_ROLLOUTS = 30
