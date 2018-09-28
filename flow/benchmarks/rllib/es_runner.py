b8377fadafbdea7748d4dde4041a852722df1656
eugenevinitsky <vinitsky.eugene@gmail.com>
2018-09-27 20:04:30 -0700

    commit for es bottleneck0_s0

--- flow/benchmarks/rllib/es_runner.py
+++ flow/benchmarks/rllib/es_runner.py
@@ -34,7 +34,7 @@ if __name__ == "__main__":
     config["num_workers"] = N_ROLLOUTS
     config["eval_prob"] = 0.05
     config["noise_stdev"] = grid_search([0.01, 0.02])
-    config["stepsize"] = grid_search([0.01, 0.02])
+    config["stepsize"] = grid_search([0.01])
     config["fcnet_hiddens"] = [100, 50, 25]
     config["observation_filter"] = "NoFilter"
     # save the flow params for replay
