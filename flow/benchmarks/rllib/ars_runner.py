bec36fbc3598c1b03683b151e60f1c08428f8bd7
eugenevinitsky <vinitsky.eugene@gmail.com>
2018-09-28 23:33:31 -0700

    decreased lambda for ppo grid 1

--- flow/benchmarks/rllib/ars_runner.py
+++ flow/benchmarks/rllib/ars_runner.py
@@ -16,7 +16,7 @@ from flow.utils.registry import make_create_env
 from flow.utils.rllib import FlowParamsEncoder
 
 # use this to specify the environment to run
-from flow.benchmarks.grid1 import flow_params
+from flow.benchmarks.grid0 import flow_params
 
 # number of rollouts per training iteration
 N_ROLLOUTS = 50
