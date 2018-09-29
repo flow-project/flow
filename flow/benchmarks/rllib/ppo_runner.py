0995986ae425402c690a6abdff365bb64cd2d24c
eugenevinitsky <vinitsky.eugene@gmail.com>
2018-09-28 23:33:31 -0700

    decreased lambda for ppo grid 1

--- flow/benchmarks/rllib/ppo_runner.py
+++ flow/benchmarks/rllib/ppo_runner.py
@@ -36,8 +36,8 @@ if __name__ == "__main__":
     config["train_batch_size"] = horizon * N_ROLLOUTS
     config["use_gae"] = True
     config["horizon"] = horizon
-    config["lambda"] = grid_search([0.97, 1.0])
-    config["lr"] = grid_search([.01])
+    config["lambda"] = grid_search([0.5])
+    config["lr"] = grid_search([5e-5, 5e-4])
     config["vf_clip_param"] = 1e6
     config["num_sgd_iter"] = 10
     config["model"]["fcnet_hiddens"] = [100, 50, 25]
