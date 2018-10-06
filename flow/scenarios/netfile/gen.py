ea53b9fd7b2efef4f12e9c23e43c203f93226bf9
Lucas Fischer <lucasfischer@airbears2-10-142-135-169.airbears2.1918.berkeley.edu>
2018-10-05 18:22:00 -0700

    Create routes from xml file

--- flow/scenarios/netfile/gen.py
+++ flow/scenarios/netfile/gen.py
@@ -40,3 +40,43 @@ class NetFileGenerator(Generator):
     def specify_edges(self, net_params):
         """See class definition."""
         pass
+
+
+    def _import_routes_from_net(self, filename):
+        """Import route from a configuration file.
+
+        This is a utility function for computing route information. It imports a
+        network configuration file, and returns the information on the routes
+        taken by all the vehicle located in the file.
+
+        Returns
+        -------
+        routes_data : dict <dict>
+            Key = name of the first route taken
+            Element = list of all the routes taken by the vehicle starting in that route
+
+        """
+        # import the .net.xml file containing all edge/type data
+        parser = etree.XMLParser(recover=True)
+        tree = ElementTree.parse(filename, parser=parser)
+
+        root = tree.getroot()
+
+        # Collect information on the available types (if any are available).
+        # This may be used when specifying some route data.
+        routes_data = dict()
+
+        
+        for vehicle in root.findall('vehicle'):
+            for route in vehicle.findall('route'):
+                route_edges = route.attrib["edges"].split(' ')
+                key=route_edges[0]
+                if key in routes_data.keys():
+                    for routes in routes_data[key]:
+                        if routes==route_edges:
+                            pass
+                        else:
+                            routes_data[key].append(route_edges)
+                else:
+                    routes_data[key] = [route_edges]
+        return routes_data
---
+++ tests/fast_tests/test_netfile.py
@@ -0,0 +1,15 @@
+import unittest
+
+from tests.setup_scripts import setup_bottlenecks
+from flow.core.experiment import SumoExperiment
+
+
+class TestNetfile(unittest.TestCase):
+    def test_it_runs(self):
+        self.env, self.scenario = setup_bottlenecks()
+        self.exp = SumoExperiment(self.env, self.scenario)
+        self.exp.run(5, 50)
+
+
+if __name__ == '__main__':
+    unittest.main()
