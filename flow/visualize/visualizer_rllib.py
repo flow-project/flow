0a6da1fe69d8fb9acf577ec1d871485e82fd8708
eugenevinitsky <vinitsky.eugene@gmail.com>
2018-10-16 15:18:07 -0700

    flake8

--- flow/visualize/visualizer_rllib.py
+++ flow/visualize/visualizer_rllib.py
@@ -20,10 +20,11 @@ import ray
 from ray.rllib.agents.agent import get_agent_class
 from ray.tune.registry import register_env
 
+from flow.core.util import emission_to_csv
+from flow.core.util import get_rllib_config
+from flow.core.util import get_rllib_pkl
 from flow.utils.registry import make_create_env
 from flow.utils.rllib import get_flow_params
-from flow.core.util import get_rllib_config, get_rllib_pkl
-from flow.core.util import emission_to_csv
 
 EXAMPLE_USAGE = """
 example usage:
@@ -36,28 +37,28 @@ PPO - the name of the algorithm the code was run with
 
 parser = argparse.ArgumentParser(
     formatter_class=argparse.RawDescriptionHelpFormatter,
-    description="[Flow] Evaluates a reinforcement learning agent "
-    "given a checkpoint.",
+    description='[Flow] Evaluates a reinforcement learning agent '
+    'given a checkpoint.',
     epilog=EXAMPLE_USAGE)
 
 # required input parameters
 parser.add_argument(
-    "result_dir", type=str, help="Directory containing results")
-parser.add_argument("checkpoint_num", type=str, help="Checkpoint number.")
+    'result_dir', type=str, help='Directory containing results')
+parser.add_argument('checkpoint_num', type=str, help='Checkpoint number.')
 
 # optional input parameters
 parser.add_argument(
-    "--run",
+    '--run',
     type=str,
-    help="The algorithm or model to train. This may refer to "
-    "the name of a built-on algorithm (e.g. RLLib's DQN "
-    "or PPO), or a user-defined trainable function or "
-    "class registered in the tune registry.")
+    help='The algorithm or model to train. This may refer to '
+    'the name of a built-on algorithm (e.g. RLLib\'s DQN '
+    'or PPO), or a user-defined trainable function or '
+    'class registered in the tune registry.')
 parser.add_argument(
     '--num_rollouts',
     type=int,
     default=1,
-    help="The number of rollouts to visualize.")
+    help='The number of rollouts to visualize.')
 parser.add_argument(
     '--emission_to_csv',
     action='store_true',
@@ -66,10 +67,10 @@ parser.add_argument(
 parser.add_argument(
     '--evaluate',
     action='store_true',
-    help='Specifies whether to use the "evaluate" reward for the environment.')
+    help='Specifies whether to use the \'evaluate\' '
+    'reward for the environment.')
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     args = parser.parse_args()
 
     result_dir = args.result_dir if args.result_dir[-1] != '/' \
@@ -89,7 +90,7 @@ if __name__ == "__main__":
 
     # Run on only one cpu for rendering purposes
     ray.init(num_cpus=1)
-    config["num_workers"] = 1
+    config['num_workers'] = 1
 
     flow_params = get_flow_params(config)
 
@@ -104,14 +105,14 @@ if __name__ == "__main__":
     agent._restore(checkpoint)
 
     # Recreate the scenario from the pickled parameters
-    exp_tag = flow_params["exp_tag"]
+    exp_tag = flow_params['exp_tag']
     net_params = flow_params['net']
     vehicles = flow_params['veh']
     initial_config = flow_params['initial']
-    module = __import__("flow.scenarios", fromlist=[flow_params["scenario"]])
-    scenario_class = getattr(module, flow_params["scenario"])
-    module = __import__("flow.scenarios", fromlist=[flow_params["generator"]])
-    generator_class = getattr(module, flow_params["generator"])
+    module = __import__('flow.scenarios', fromlist=[flow_params['scenario']])
+    scenario_class = getattr(module, flow_params['scenario'])
+    module = __import__('flow.scenarios', fromlist=[flow_params['generator']])
+    generator_class = getattr(module, flow_params['generator'])
 
     scenario = scenario_class(
         name=exp_tag,
@@ -122,22 +123,22 @@ if __name__ == "__main__":
 
     # Start the environment with the gui turned on and a path for the
     # emission file
-    module = __import__("flow.envs", fromlist=[flow_params["env_name"]])
-    env_class = getattr(module, flow_params["env_name"])
+    module = __import__('flow.envs', fromlist=[flow_params['env_name']])
+    env_class = getattr(module, flow_params['env_name'])
     env_params = flow_params['env']
     if args.evaluate:
         env_params.evaluate = True
     sumo_params = flow_params['sumo']
     sumo_params.render = True
-    sumo_params.emission_path = "./test_time_rollout/"
+    sumo_params.emission_path = './test_time_rollout/'
 
     env = env_class(
         env_params=env_params, sumo_params=sumo_params, scenario=scenario)
 
     if multiagent:
         rets = {}
-        ids = config["multiagent"]["policy_graphs"].keys()
-        for key in config["multiagent"]["policy_graphs"].keys():
+        ids = config['multiagent']['policy_graphs'].keys()
+        for key in config['multiagent']['policy_graphs'].keys():
             rets[key] = []
     else:
         rets = []
@@ -158,8 +159,8 @@ if __name__ == "__main__":
             if multiagent:
                 action = {}
                 for agent_id in ids:
-                    action[agent_id] = agent.compute_action(state[agent_id],
-                                                            policy_id=agent_id)
+                    action[agent_id] = agent.compute_action(
+                        state[agent_id], policy_id=agent_id)
             else:
                 action = agent.compute_action(state)
             state, reward, done, _ = env.step(action)
@@ -183,20 +184,21 @@ if __name__ == "__main__":
         mean_speed.append(np.mean(vel))
         if multiagent:
             for agent, rew in rets.items():
-                print("Round {}, Return: {} for agent {}".format(i,
-                                                                 ret, agent))
+                print('Round {}, Return: {} for agent {}'.format(
+                    i, ret, agent))
         else:
-            print("Round {}, Return: {}".format(i, ret))
+            print('Round {}, Return: {}'.format(i, ret))
     if multiagent:
         for agent, rew in rets.items():
-            print("Average, std return: {}, {} for agent {}".format(np.mean(rew),
-                                                       np.std(rew), agent))
+            print('Average, std return: {}, {} for agent {}'.format(
+                np.mean(rew), np.std(rew), agent))
     else:
-        print("Average, std return: {}, {}".format(np.mean(rets), np.std(rets)))
-    print("Average, std speed: {}, {}".format(np.mean(mean_speed),
-                                              np.std(mean_speed)))
-    print("Average, std outflow: {}, {}".format(np.mean(final_outflows),
-                                                np.std(final_outflows)))
+        print('Average, std return: {}, {}'.format(
+            np.mean(rets), np.std(rets)))
+    print('Average, std speed: {}, {}'.format(
+        np.mean(mean_speed), np.std(mean_speed)))
+    print('Average, std outflow: {}, {}'.format(
+        np.mean(final_outflows), np.std(final_outflows)))
 
     # terminate the environment
     env.terminate()
@@ -204,9 +206,9 @@ if __name__ == "__main__":
     # if prompted, convert the emission file into a csv file
     if args.emission_to_csv:
         dir_path = os.path.dirname(os.path.realpath(__file__))
-        emission_filename = "{0}-emission.xml".format(scenario.name)
+        emission_filename = '{0}-emission.xml'.format(scenario.name)
 
         emission_path = \
-            "{0}/test_time_rollout/{1}".format(dir_path, emission_filename)
+            '{0}/test_time_rollout/{1}'.format(dir_path, emission_filename)
 
         emission_to_csv(emission_path)
