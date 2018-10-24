395891cec74997bb65f0b0d090d7679d1ff6e536
eugenevinitsky <vinitsky.eugene@gmail.com>
2018-10-24 14:50:28 -0700

    congestion penalty

--- flow/envs/bottleneck_env.py
+++ flow/envs/bottleneck_env.py
@@ -1017,7 +1017,12 @@ class MultiBottleneckEnv(BottleneckEnv):
             #reward = self.vehicles.get_speed(self.vehicles.get_ids())
             pass
         if rl_actions:
-            return {rl_id: self.vehicles.get_speed(rl_id)/20 for rl_id in self.vehicles.get_rl_ids()}
+            edge = '4'
+            speed_rew = lambda x: self.vehicles.get_speed(x)/40
+            num_ids = len(self.vehicles.get_ids_by_edge(edge))
+            cong_penalty = max(num_ids - 30, 0)/60
+            return {rl_id: speed_rew(rl_id) - cong_penalty for rl_id
+                    in self.vehicles.get_rl_ids()}
         else:
             return {}
 
