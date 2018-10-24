c003eb0c3d6466604eb396490a943b98718a4f0a
eugenevinitsky <vinitsky.eugene@gmail.com>
2018-10-23 23:03:20 -0700

    minor

--- examples/rllib/multiagent_exps/multiagent_bottleneck.py
+++ examples/rllib/multiagent_exps/multiagent_bottleneck.py
@@ -166,11 +166,13 @@ if __name__ == '__main__':
     config['simple_optimizer'] = True
     config['gamma'] = 0.999  # discount rate
     #config['model'].update({'fcnet_hiddens': [100, 50, 25]})
+    config['model']['use_lstm'] = tune.grid_search([True, False])
     config['use_gae'] = True
     config['lambda'] = 0.97
     #config['sgd_minibatch_size'] = 128
     config['kl_target'] = 0.02
-    config['lr'] = tune.grid_search([1e-7, 5e-7])
+    config['vf_clip_param'] = 10000
+    config['lr'] = tune.grid_search([1e-5, 1e-6, 1e-7])
     config['num_sgd_iter'] = tune.grid_search([10, 30])
     config['horizon'] = HORIZON
     config['observation_filter'] = 'NoFilter'
