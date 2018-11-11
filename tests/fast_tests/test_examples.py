993635878ac0de9b78cb2e43eee6b5e520dd49ad
eugenevinitsky <vinitsky.eugene@gmail.com>
2018-11-11 01:10:58 -1000

    test of the problem

--- tests/fast_tests/test_examples.py
+++ tests/fast_tests/test_examples.py
@@ -24,85 +24,85 @@ from ray.tune import run_experiments
 os.environ['TEST_FLAG'] = 'True'
 
 
-class TestSumoExamples(unittest.TestCase):
-    """Tests the example scripts in examples/sumo.
-
-    This is done by running the experiment function within each script for a
-    few time steps. Note that, this does not test for any refactoring changes
-    done to the functions within the experiment class.
-    """
-
-    def test_bottleneck(self):
-        """Verifies that examples/sumo/bottleneck.py is working."""
-        # import the experiment variable from the example
-        exp = bottleneck_example(1000, 5, render=False)
-
-        # run the experiment for a few time steps to ensure it doesn't fail
-        exp.run(1, 5)
-
-    def test_figure_eight(self):
-        """Verifies that examples/sumo/figure_eight.py is working."""
-        # import the experiment variable from the example
-        exp = figure_eight_example(render=False)
-
-        # run the experiment for a few time steps to ensure it doesn't fail
-        exp.run(1, 5)
-
-    def test_grid(self):
-        """Verifies that examples/sumo/grid.py is working."""
-        # import the experiment variable from the example
-        exp = grid_example(render=False)
-
-        # run the experiment for a few time steps to ensure it doesn't fail
-        exp.run(1, 5)
-
-    def test_highway(self):
-        """Verifies that examples/sumo/highway.py is working."""
-        # import the experiment variable from the example
-        exp = highway_example(render=False)
-
-        # run the experiment for a few time steps to ensure it doesn't fail
-        exp.run(1, 5)
-
-    def test_merge(self):
-        """Verifies that examples/sumo/merge.py is working."""
-        # import the experiment variable from the example
-        exp = merge_example(render=False)
-
-        # run the experiment for a few time steps to ensure it doesn't fail
-        exp.run(1, 5)
-
-    def test_sugiyama(self):
-        """Verifies that examples/sumo/sugiyama.py is working."""
-        # import the experiment variable from the example
-        exp = sugiyama_example(render=False)
-
-        # run the experiment for a few time steps to ensure it doesn't fail
-        exp.run(1, 5)
-
-    def test_loop_merge(self):
-        """Verify that examples/sumo/two_loops_merge_straight.py is working."""
-        # import the experiment variable from the example
-        exp = loop_merge_example(render=False)
-
-        # run the experiment for a few time steps to ensure it doesn't fail
-        exp.run(1, 5)
-
-    def test_bay_bridge(self):
-        """Verifies that examples/sumo/bay_bridge.py is working."""
-        # import the experiment variable from the example
-        exp = bay_bridge_example(render=False)
-
-        # run the experiment for a few time steps to ensure it doesn't fail
-        exp.run(1, 5)
-
-    def test_bay_bridge_toll(self):
-        """Verifies that examples/sumo/bay_bridge_toll.py is working."""
-        # import the experiment variable from the example
-        exp = bay_bridge_toll_example(render=False)
-
-        # run the experiment for a few time steps to ensure it doesn't fail
-        exp.run(1, 5)
+# class TestSumoExamples(unittest.TestCase):
+#     """Tests the example scripts in examples/sumo.
+#
+#     This is done by running the experiment function within each script for a
+#     few time steps. Note that, this does not test for any refactoring changes
+#     done to the functions within the experiment class.
+#     """
+#
+#     def test_bottleneck(self):
+#         """Verifies that examples/sumo/bottleneck.py is working."""
+#         # import the experiment variable from the example
+#         exp = bottleneck_example(1000, 5, render=False)
+#
+#         # run the experiment for a few time steps to ensure it doesn't fail
+#         exp.run(1, 5)
+#
+#     def test_figure_eight(self):
+#         """Verifies that examples/sumo/figure_eight.py is working."""
+#         # import the experiment variable from the example
+#         exp = figure_eight_example(render=False)
+#
+#         # run the experiment for a few time steps to ensure it doesn't fail
+#         exp.run(1, 5)
+#
+#     def test_grid(self):
+#         """Verifies that examples/sumo/grid.py is working."""
+#         # import the experiment variable from the example
+#         exp = grid_example(render=False)
+#
+#         # run the experiment for a few time steps to ensure it doesn't fail
+#         exp.run(1, 5)
+#
+#     def test_highway(self):
+#         """Verifies that examples/sumo/highway.py is working."""
+#         # import the experiment variable from the example
+#         exp = highway_example(render=False)
+#
+#         # run the experiment for a few time steps to ensure it doesn't fail
+#         exp.run(1, 5)
+#
+#     def test_merge(self):
+#         """Verifies that examples/sumo/merge.py is working."""
+#         # import the experiment variable from the example
+#         exp = merge_example(render=False)
+#
+#         # run the experiment for a few time steps to ensure it doesn't fail
+#         exp.run(1, 5)
+#
+#     def test_sugiyama(self):
+#         """Verifies that examples/sumo/sugiyama.py is working."""
+#         # import the experiment variable from the example
+#         exp = sugiyama_example(render=False)
+#
+#         # run the experiment for a few time steps to ensure it doesn't fail
+#         exp.run(1, 5)
+#
+#     def test_loop_merge(self):
+#         """Verify that examples/sumo/two_loops_merge_straight.py is working."""
+#         # import the experiment variable from the example
+#         exp = loop_merge_example(render=False)
+#
+#         # run the experiment for a few time steps to ensure it doesn't fail
+#         exp.run(1, 5)
+#
+#     def test_bay_bridge(self):
+#         """Verifies that examples/sumo/bay_bridge.py is working."""
+#         # import the experiment variable from the example
+#         exp = bay_bridge_example(render=False)
+#
+#         # run the experiment for a few time steps to ensure it doesn't fail
+#         exp.run(1, 5)
+#
+#     def test_bay_bridge_toll(self):
+#         """Verifies that examples/sumo/bay_bridge_toll.py is working."""
+#         # import the experiment variable from the example
+#         exp = bay_bridge_toll_example(render=False)
+#
+#         # run the experiment for a few time steps to ensure it doesn't fail
+#         exp.run(1, 5)
 
 
 class TestRllibExamples(unittest.TestCase):
@@ -161,5 +161,5 @@ class TestRllibExamples(unittest.TestCase):
 
 if __name__ == '__main__':
 
-    ray.init(num_cpus=3, redirect_output=False)
+    ray.init(num_cpus=3, redirect_output=True)
     unittest.main()
