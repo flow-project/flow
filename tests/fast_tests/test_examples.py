bb0652d259d8f0431dfa0e3850919c547f8049a1
eugenevinitsky <vinitsky.eugene@gmail.com>
2018-11-11 12:39:57 -1000

    travis only has 2 cores

--- tests/fast_tests/test_examples.py
+++ tests/fast_tests/test_examples.py
@@ -161,5 +161,5 @@ class TestRllibExamples(unittest.TestCase):
 
 if __name__ == '__main__':
 
-    ray.init(num_cpus=3, redirect_output=True)
+    ray.init(num_cpus=2, redirect_output=True)
     unittest.main()
