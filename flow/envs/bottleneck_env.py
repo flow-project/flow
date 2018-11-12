606d50319e349b1be7df6bcf4548866af80cc5c4
eugenevinitsky <vinitsky.eugene@gmail.com>
2018-11-11 16:36:18 -1000

    another merge conflict

--- flow/envs/bottleneck_env.py
+++ flow/envs/bottleneck_env.py
@@ -749,12 +749,8 @@ class DesiredVelocityEnv(BottleneckEnv):
         max_accel = add_params.get("max_accel")
         max_decel = add_params.get("max_decel")
         return Box(
-<<<<<<< HEAD
-            low=-1.5, high=1.0, shape=(int(action_size),), dtype=np.float32)
-=======
             low=-max_decel*self.sim_step, high=max_accel*self.sim_step,
             shape=(int(action_size), ), dtype=np.float32)
->>>>>>> new_sumo
 
     def get_state(self):
         """See class definition."""
