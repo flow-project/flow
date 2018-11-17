a252e36e06033defff23cc3ae9d2469787d20566
eugenevinitsky <vinitsky.eugene@gmail.com>
2018-11-17 02:01:14 -0800

    set setup file back

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ class build_ext(_build_ext.build_ext):
         subprocess.check_call(
             ['pip', 'install',
              'https://akreidieh.s3.amazonaws.com/sumo/flow-0.2.0/'
-             'sumotools-0.2.0-py3-none-any.whl'])
+             'sumotools-0.1.0-py3-none-any.whl'])
 
 
 class BinaryDistribution(Distribution):
