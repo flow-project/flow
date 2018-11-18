fb5dbdc2ae6148f2b683b5aba164f523557bbec2
Kanaad Parvate <kanaadp@gmail.com>
2018-11-18 15:00:08 -0800

    progress on benchmarks script

--- flow/benchmarks/run_all_benchmarks.sh
+++ flow/benchmarks/run_all_benchmarks.sh
@@ -16,11 +16,10 @@ echo # just a final linefeed, optics...
 return $retval
 }
 
-declare -a benchmarks=("bottleneck0"
-                        "bottleneck1" "bottleneck2"
+declare -a benchmarks=(
+                        "bottleneck0" "bottleneck1" "bottleneck2"
                         "figureeight0" "figureeight1" "figureeight2"
-                        "figureeight0" "figureeight1" "figureeight2"
-#                        "grid0" "grid1"
+                        "grid0" "grid1"
                         "merge0" "merge1"
                         )
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
@@ -34,6 +33,7 @@ for run_script in rllib/*_runner.py; do
         declare alg=`echo ${run_script} | cut -d'/' -f 2 | cut -d'_' -f 1`
         echo "====================================================================="
         echo "Training ${benchmark} with ${alg}"
+        echo "ray exec ../../scripts/benchmark_autoscale.yaml \"python ./flow/flow/benchmarks/${run_script} --upload_dir=\"flow-benchmark.results/${dt}/\" --benchmark_name=${benchmark}\" --start --stop --cluster-name=all_benchmark_${benchmark}_${alg}_$dt --tmux"
         echo "====================================================================="
         ray exec ../../scripts/benchmark_autoscale.yaml "python ./flow/flow/benchmarks/${run_script} --upload_dir=\"flow-benchmark.results/${dt}/\" --benchmark_name=${benchmark}" --start --stop --cluster-name=all_benchmark_${benchmark}_${alg}_$dt --tmux
     done
