a7dfd6109c521d87c30ed1e11dd2e5d3b627173c
Fangyu Wu <fywu85@gmail.com>
2018-11-24 19:42:10 -0800

    Modify rebuild ec2 script.

--- lab/rebuild_ec2.sh
+++ lab/rebuild_ec2.sh
@@ -6,12 +6,15 @@ python setup.py develop
 cd ~
 sudo apt-get install -y \
     xorg-dev \
-    libglu1-mesa libgl1-mesa-dev \
+    libglu1-mesa \
+    libgl1-mesa-dev \
     xvfb \
-    libxinerama1 libxcursor1
+    libxinerama1 \
+    libxcursor1 \
+    gfortran
 pip install imutils
-cd ~/flow/examples/ecc/
+
 # Running script in the background.
-#nohup xvfb-run -a -s \
-#    "-screen 0 1400x900x24 +extension RANDR"\
-#     -- python circle_cnnidm.py 0e-1 > circle_cnnidm0e-1.out 2>&1 &
+nohup xvfb-run -a -s \
+    "-screen 0 1400x900x24 +extension RANDR"\
+    -- python script.py 0e-1 > script.log 2>&1 &
