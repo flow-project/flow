df6b724740e8fa962de4fc59b1daac4a47d49af7
eugenevinitsky <vinitsky.eugene@gmail.com>
2018-11-29 10:56:00 -0800

    renamed

---
+++ scripts/create_movies.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+echo "Preparing to make and save movies"
+
+script_path=$(pwd)
+
+file_path=
+if [ $# == 0 ]; then
+    echo "Please pass the path to the outer folder containing benchmarks"
+    exit 1
+else
+    file_path=$1
+fi
+cd "$file_path"
+
+# step into every pulled folder
+for outer_folder in */; do
+    cd $outer_folder
+    for inner_folder in */; do
+        cd $inner_folder
+        checkpoint_num="$(ls | grep '^checkpoint_[0-9]\+$' | cut -c12- | sort -n | tail -n1)"
+        echo "Visualizing highest checkpoints in "$outer_folder
+        file_path=$(pwd)
+        python $script_path/../flow/visualize/visualizer_rllib.py $file_path $checkpoint_num \
+        --save_render
+        cd "../"
+    done
+    cd "../"
+done
