44c46e525d38a3acfb7894209df7e19ef989b964
eugenevinitsky <vinitsky.eugene@gmail.com>
2018-11-29 11:50:02 -0800

    minor

--- scripts/create_movies.sh
+++ scripts/create_movies.sh
@@ -18,7 +18,9 @@ for outer_folder in */; do
     for inner_folder in */; do
         cd $inner_folder
         checkpoint_num="$(ls | grep '^checkpoint_[0-9]\+$' | cut -c12- | sort -n | tail -n1)"
+        echo "====================================================================="
         echo "Visualizing highest checkpoints in "$outer_folder
+        echo "====================================================================="
         file_path=$(pwd)
         python $script_path/../flow/visualize/visualizer_rllib.py $file_path $checkpoint_num \
         --save_render
