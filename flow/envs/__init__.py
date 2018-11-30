112d994026bd07d62b6196178fd11cea3a121bde
eugenevinitsky <vinitsky.eugene@gmail.com>
2018-11-30 10:14:58 -0800

    bug fix for issue

--- flow/envs/__init__.py
+++ flow/envs/__init__.py
@@ -1,7 +1,7 @@
 """Contains all callable environments in Flow."""
 
+# single agent envs
 from flow.envs.base_env import Env
-from flow.envs.multiagent_env import MultiEnv
 from flow.envs.bay_bridge.base import BayBridgeEnv
 from flow.envs.bottleneck_env import BottleNeckAccelEnv, BottleneckEnv, \
     DesiredVelocityEnv
@@ -10,19 +10,33 @@ from flow.envs.green_wave_env import TrafficLightGridEnv, \
 from flow.envs.loop.lane_changing import LaneChangeAccelEnv, \
     LaneChangeAccelPOEnv
 from flow.envs.loop.loop_accel import AccelEnv
-from flow.envs.loop.loop_accel import MultiAgentAccelEnv
 from flow.envs.loop.loop_merges import TwoLoopsMergePOEnv
 from flow.envs.loop.wave_attenuation import WaveAttenuationEnv, \
-    WaveAttenuationPOEnv, MultiWaveAttenuationPOEnv
+    WaveAttenuationPOEnv
 from flow.envs.merge import WaveAttenuationMergePOEnv
 from flow.envs.test import TestEnv
 
-__all__ = [
+# multi agent envs
+try:
+    from flow.envs.multiagent_env import MultiEnv
+    multi_flag = True
+except:
+    multi_flag = False
+
+env_list = [
     "Env", "MultiEnv", "AccelEnv", "LaneChangeAccelEnv",
-    "LaneChangeAccelPOEnv",
-    "GreenWaveTestEnv", "GreenWaveTestEnv", "WaveAttenuationMergePOEnv",
-    "TwoLoopsMergePOEnv", "BottleneckEnv", "BottleNeckAccelEnv",
-    "WaveAttenuationEnv", "WaveAttenuationPOEnv", "MultiWaveAttenuationPOEnv",
+    "LaneChangeAccelPOEnv", "GreenWaveTestEnv", "GreenWaveTestEnv",
+    "WaveAttenuationMergePOEnv", "TwoLoopsMergePOEnv", "BottleneckEnv",
+    "BottleNeckAccelEnv", "WaveAttenuationEnv", "WaveAttenuationPOEnv",
     "TrafficLightGridEnv", "PO_TrafficLightGridEnv", "DesiredVelocityEnv",
-    "TestEnv", "BayBridgeEnv", "MultiAgentAccelEnv",
+    "TestEnv", "BayBridgeEnv",
 ]
+
+
+if multi_flag:
+    from flow.envs.loop.wave_attenuation import MultiWaveAttenuationPOEnv
+    from flow.envs.loop.loop_accel import MultiAgentAccelEnv
+    env_list += ["MultiAgentAccelEnv", "MultiWaveAttenuationPOEnv"]
+
+else:
+__all__ = env_list
