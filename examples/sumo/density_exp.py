2c7da3283acccbed02f698cb000d9c7266968034
eugenevinitsky <vinitsky.eugene@gmail.com>
2018-12-05 12:24:05 -0800

    changed paths, fixed bug

--- examples/sumo/density_exp.py
+++ examples/sumo/density_exp.py
@@ -3,6 +3,7 @@ Run density experiment to generate capacity diagram for the
 bottleneck experiment
 """
 
+import os
 import ray
 
 from examples.sumo.bottlenecks import bottleneck_example
@@ -10,21 +11,23 @@ from examples.sumo.bottlenecks import bottleneck_example
 import numpy as np
 import multiprocessing
 
+
 @ray.remote
 def run_bottleneck(flow_rate, num_trials, num_steps, render=None):
     print('Running experiment for inflow rate: ', flow_rate, render)
-    exp = bottleneck_example(flow_rate, num_steps, restart_instance=True)
+    exp = bottleneck_example(flow_rate, num_steps, restart_instance=False)
     info_dict = exp.run(num_trials, num_steps)
 
     return info_dict['average_outflow'], \
-        np.mean(info_dict['velocities']), \
-        np.mean(info_dict['average_rollout_density_outflow']), \
-        info_dict['per_rollout_outflows']
+           np.mean(info_dict['velocities']), \
+           np.mean(info_dict['average_rollout_density_outflow']), \
+           info_dict['per_rollout_outflows'], \
+           flow_rate
 
 
 if __name__ == '__main__':
     # import the experiment variable
-    densities = list(range(400, 3000, 100))
+    densities = list(range(400, 600, 100))
     outflows = []
     velocities = []
     bottleneckdensities = []
@@ -41,18 +44,23 @@ if __name__ == '__main__':
     bottleneck_outputs = [run_bottleneck.remote(d, 10, 2000)
                           for d in densities]
     for output in ray.get(bottleneck_outputs):
-        outflow, velocity, bottleneckdensity, per_rollout_outflows = output
-        for i, x in enumerate(per_rollout_outflows):
-            rollout_outflows.append(x)
-            rollout_inflows.append(densities[i])
+        outflow, velocity, bottleneckdensity, \
+        per_rollout_outflows, flow_rate = output
+        for i, _ in enumerate(per_rollout_outflows):
+            rollout_outflows.append(np.mean(per_rollout_outflows))
+            rollout_inflows.append(flow_rate)
         outflows.append(outflow)
         velocities.append(velocity)
         bottleneckdensities.append(bottleneckdensity)
 
-    np.savetxt('rets.csv', np.matrix([densities,
-                                      outflows,
-                                      velocities,
-                                      bottleneckdensities]).T, delimiter=',')
-    np.savetxt('inflows_outflows.csv', np.matrix([rollout_inflows,
-                                                  rollout_outflows]).T,
+    path = os.path.dirname(os.path.abspath(__file__))
+    np.savetxt(path + '/../../data/rets.csv',
+               np.matrix([densities,
+                          outflows,
+                          velocities,
+                          bottleneckdensities]).T,
+               delimiter=',')
+    np.savetxt(path + '/../../data/inflows_outflows.csv',
+               np.matrix([rollout_inflows,
+                          rollout_outflows]).T,
                delimiter=',')
