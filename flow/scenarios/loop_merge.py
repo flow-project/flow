eba4583025f18ae36a90f70ded4e83ea19dc06fe
AboudyKreidieh <akreidieh@gmail.com>
2018-12-15 20:28:16 -0800

    minor bug fixes

--- flow/scenarios/loop_merge.py
+++ flow/scenarios/loop_merge.py
@@ -63,7 +63,7 @@ class TwoLoopsOneMergingScenario(Scenario):
 
         num_vehicles = vehicles.num_vehicles
         num_merge_vehicles = sum("merge" in vehicles.get_type(veh_id)
-                                 for veh_id in vehicles.get_ids())
+                                 for veh_id in vehicles.ids)
         self.n_inner_vehicles = num_merge_vehicles
         self.n_outer_vehicles = num_vehicles - num_merge_vehicles
 
