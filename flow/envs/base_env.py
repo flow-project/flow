da0039050c8fefd3791be4b634561bee478878eb
Fangyu Wu <fywu85@gmail.com>
2018-12-23 18:01:02 -0800

    Replace slowDown with setSpeed in base env.

--- flow/envs/base_env.py
+++ flow/envs/base_env.py
@@ -667,7 +667,7 @@ class Env(*classdef):
             if acc[i] is not None:
                 this_vel = self.vehicles.get_speed(vid)
                 next_vel = max([this_vel + acc[i] * self.sim_step, 0])
-                self.traci_connection.vehicle.slowDown(vid, next_vel, 1)
+                self.traci_connection.vehicle.setSpeed(vid, next_vel)
 
     def apply_lane_change(self, veh_ids, direction):
         """Apply an instantaneous lane-change to a set of vehicles.
