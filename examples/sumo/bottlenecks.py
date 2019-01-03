038254b4f54e99405f0affcb962786d5aeca124c
eugenevinitsky <vinitsky.eugene@gmail.com>
2019-01-03 16:21:10 -0500

    pep8

--- examples/sumo/bottlenecks.py
+++ examples/sumo/bottlenecks.py
@@ -71,7 +71,8 @@ class BottleneckDensityExperiment(Experiment):
                 ret += reward
                 ret_list.append(reward)
 
-                step_outflow = self.env.get_bottleneck_outflow_vehicles_per_hour(20)
+                env = self.env
+                step_outflow = env.get_bottleneck_outflow_vehicles_per_hour(20)
                 density = self.env.get_bottleneck_density()
 
                 step_outflows.append(step_outflow)
@@ -82,7 +83,8 @@ class BottleneckDensityExperiment(Experiment):
             vels.append(vel)
             mean_densities.append(sum(step_densities[100:]) /
                                   (num_steps - 100))
-            outflow = self.env.get_bottleneck_outflow_vehicles_per_hour(10000)
+            env = self.env
+            outflow = env.get_bottleneck_outflow_vehicles_per_hour(10000)
             mean_outflows.append(outflow)
             mean_rets.append(np.mean(ret_list))
             ret_lists.append(ret_list)
