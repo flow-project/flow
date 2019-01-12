721232c090c316b5cdf4bb9b12291b4c3608a705
Yasharzf <y.zeinali@gmail.com>
2019-01-11 20:16:12 -0800

    added traffic light api

--- flow/core/kernel/traffic_light/aimsun.py
+++ flow/core/kernel/traffic_light/aimsun.py
@@ -21,12 +21,12 @@ class AimsunKernelTrafficLight(KernelTrafficLight):
 
         # names of nodes with traffic lights
         self.__ids = []
+        self.num_meters = 0
+
 
     def pass_api(self, kernel_api):
         """See parent class."""
         self.kernel_api = kernel_api
-        self.__ids = []
-        # self.__ids = self.kernel_api.get_traffic_light_ids()
 
     def update(self, reset):
         """See parent class."""
@@ -34,16 +34,16 @@ class AimsunKernelTrafficLight(KernelTrafficLight):
 
     def get_ids(self):
         """See parent class."""
-        return self.__ids
+        return self.kernel_api.get_traffic_light_ids()
 
-    def set_state(self, node_id, state, link_index="all"):
-        """Set the state of the traffic lights on a specific node.
+    def set_state(self, meter_aimsun_id, state):
+        """Set the state of the traffic lights on a specific meter.
 
         Parameters
         ----------
-        node_id : str
-            name of the node with the controlled traffic lights
-        state : str
+        meter_aimsun_id : int
+            aimsun id of the meter
+        state : int
             desired state(s) for the traffic light
             0: red
             1: green
@@ -52,32 +52,23 @@ class AimsunKernelTrafficLight(KernelTrafficLight):
             index of the link whose traffic light state is meant to be changed.
             If no value is provided, the lights on all links are updated.
         """
-        time = self.kernel_api.AKIGetCurrentSimulationTime()  # simulation time
-        sim_step = self.master_kernel.simulation.sim_step
-        identity = 0  # TODO double check
-        self.kernel_api.ECIChangeStateMeteringById(
-            int(node_id), state, time, sim_step, identity)
+        self.kernel_api.set_traffic_light_state(meter_aimsun_id, None, state)
 
-    def get_state(self, node_id):
+    def get_state(self, meter_aimsun_id):
         """Return the state of the traffic light(s) at the specified node.
 
         Parameters
         ----------
-        node_id: str
-            name of the node
+        meter_aimsun_id: int
+            aimsun id of the meter
 
         Returns
         -------
-        state : str
-            Index = lane index
-            Element = state of the traffic light at that node/lane
+        state : int
+            desired state(s) for the traffic light
+            0: red
+            1: green
+            2: yellow
         """
-        # FIXME: pass all
-        return None
-        # return self.kernel_api.ECIGetCurrentStateofMeteringById(
-        #         int(node_id), int(lane_id))
+        return self.kernel_api.get_traffic_light_state(meter_aimsun_id)
 
-    @property
-    def num_traffic_lights(self):
-        """See parent class."""
-        return len(self.__ids)
