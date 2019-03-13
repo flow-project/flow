e312b9703b099472e377381ccfb510a07cd70efb
Lucas Fischer <lf@datakalab.com>
2019-03-13 14:53:05 -0700

    input pixel

--- examples/rllib/minicity_subroutes.py
+++ examples/rllib/minicity_subroutes.py
@@ -350,14 +350,22 @@ initial_config = InitialConfig(
     edges_distribution=list(SUBROUTE_EDGES[SUBNETWORK.value].keys())
 )
 
-
+xmin = int(input("What is xmin? 0}"))
+xmax = int(input("What is xmax? 1200"))
+ymin = int(input("What is ymin? 0"))
+ymax = int(input("What is ymax? 1200"))
 additional_env_params = {
     'target_velocity': 11,
     'switch_time': 7,
     'num_observed': 2,
     'discrete': False,
     'tl_type': 'controlled',
-    'subnetwork': SUBNETWORK.value
+    'subnetwork': SUBNETWORK.value,
+    'xmin':xmin,
+    'xmax':xmax,
+    'ymin':ymin,
+    'ymax':ymax,
+
 }
 
 
