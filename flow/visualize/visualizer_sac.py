c14c2f0192df567f41279c05fdef8719bdcf7bf5
nathanlct <nathanlct@icloud.com>
2019-05-14 20:29:51 -0700

    added CLI to SAC visualizer

--- flow/visualize/visualizer_sac.py
+++ flow/visualize/visualizer_sac.py
@@ -1,3 +1,8 @@
+"""Visualizer for experiments using the 'softlearning' library (SAC).
+
+Run 'python3.7 visualizer_sac --help' to get example usage
+"""
+
 import argparse
 from distutils.util import strtobool
 import json
@@ -6,29 +11,37 @@ import pickle
 
 import tensorflow as tf
 
-from softlearning.environments.utils import get_environment_from_params
 from softlearning.policies.utils import get_policy_from_variant
 from softlearning.samplers import rollouts
 
 from flow.utils.registry import make_create_env
-
 from flow.utils.rllib import get_flow_params
 
 
-"""
-checkpoint_path: path to the checkpoint_<id> folder
-                e.g. ~/ray_results/gym/HalfCheetah/v3/2018-12-12T16-48-37-my-sac-experiment-1-0/
-                        mujoco-runner_0_seed=7585_2018-12-12_16-48-37xuadh9vd/checkpoint_1000/
-render_mode: 'human', 'rgb_array' or None
-deterministic: whether or not to evaluate the policy deterministically
-"""
-def simulate_policy(checkpoint_path, max_path_length=5000, num_rollouts=1,
-                    render_mode='human', deterministic=False):
-    session = tf.keras.backend.get_session()
-    checkpoint_path = checkpoint_path.rstrip('/')
-    experiment_path = os.path.dirname(checkpoint_path)
+EXAMPLE_USAGE = "Example usage:\n\n" + \
+    "python3.7 ./visualizer_sac.py /tmp/ray/result_dir 1 " + \
+    "--num-rollouts 3 --horizon 3000 --deterministic False\n\n" + \
+    "Here the arguments are in order:\n" + \
+    "\t\"/tmp/ray/result_dir\" the directory containing the results\n" + \
+    "\t\"1\" the number (id) of the checkpoint\n" + \
+    "\t\"3\" the number of rollouts to simulate (optional)\n" + \
+    "\t\"3000\" the number of steps to roll for each rollout (optional)\n" + \
+    "\t\"False\" whether the policy should be made deterministic (optional)\n"
 
+
+def simulate_policy(args):
+    session = tf.keras.backend.get_session()
+
+    experiment_path = args.result_dir
     variant_path = os.path.join(experiment_path, 'params.json')
+    checkpoint_dir = f"checkpoint_{args.checkpoint_num}"
+    checkpoint_path = os.path.join(experiment_path, checkpoint_dir)
+
+    if not os.path.exists(variant_path):
+        raise ValueError(f"Couldn't find file {variant_path}")
+    if not os.path.exists(checkpoint_path):
+        raise ValueError(f"Couldn't find file {checkpoint_path}")
+
     with open(variant_path, 'r') as f:
         variant = json.load(f)
 
@@ -39,7 +52,6 @@ def simulate_policy(checkpoint_path, max_path_length=5000, num_rollouts=1,
 
     flow_params = get_flow_params(variant)
     flow_params['sim'].render = True
-    print(flow_params)
 
     create_env, _ = make_create_env(params=flow_params, version=0)
     evaluation_environment = create_env()
@@ -48,20 +60,54 @@ def simulate_policy(checkpoint_path, max_path_length=5000, num_rollouts=1,
         get_policy_from_variant(variant, evaluation_environment, Qs=[None]))
     policy.set_weights(picklable['policy_weights'])
 
-    with policy.set_deterministic(deterministic):
-        paths = rollouts(num_rollouts,
+    if args.horizon:
+        horizon = args.horizon
+    else:
+        horizon = variant['algorithm_params']['kwargs']['epoch_length']
+
+    with policy.set_deterministic(args.deterministic):
+        paths = rollouts(args.num_rollouts,
                          evaluation_environment,
                          policy,
-                         path_length=max_path_length,
-                         render_mode=render_mode)
-
-    if render_mode != 'human':
-        from pprint import pprint; import pdb; pdb.set_trace()
-        pass
+                         path_length=horizon,
+                         render_mode='human')
 
     return paths
 
 
+def create_parser():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description='[Flow] Evaluates a reinforcement learning agent '
+                    'given a checkpoint.',
+        epilog=EXAMPLE_USAGE)
+
+    # required input parameters
+    parser.add_argument(
+        'result_dir', type=str, help='Directory containing the results.')
+    parser.add_argument('checkpoint_num', type=str, help='Checkpoint number.')
+
+    # optional input parameters
+    parser.add_argument(
+        '--num-rollouts',
+        type=int,
+        default=1,
+        help='The number of rollouts to visualize (by default 1).')
+    parser.add_argument(
+        '--horizon',
+        type=int,
+        help='The horizon for each rollout (by default the same as' + \
+             'during the simulation).')
+    parser.add_argument(
+        '--deterministic',
+        type=bool,
+        default=False,
+        help='Whether or not the policy should be made deterministic' + \
+             '(False by default).')
+    return parser
+
+
 if __name__ == '__main__':
-    simulate_policy("/Users/nathan/ray_results/SAC_ring/2019-05-12T11-06-49-experiment_name/id=28d3ad94-seed=1711_2019-05-12_11-06-49_8i_8w_g/checkpoint_150")
-    # TODO cli
+    parser = create_parser()
+    args = parser.parse_args()
+    simulate_policy(args)
