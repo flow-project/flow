84ebb743bf7e82ca776d2796ec78b156b652a5d9
AboudyKreidieh <akreidieh@gmail.com>
2019-05-29 11:57:51 -0700

    removed obsolete tests

--- examples/rllab/cooperative_merge.py
+++ examples/rllab/cooperative_merge.py
@@ -94,7 +94,7 @@ def run_task(*_):
         net_params=net_params,
         initial_config=initial_config)
 
-    env_name = "TwoLoopsMergePOEnv"
+    env_name = "AccelEnv"
     pass_params = (env_name, sim_params, vehicles, env_params, net_params,
                    initial_config, scenario)
 
