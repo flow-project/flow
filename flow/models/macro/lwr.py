62b5c4c657cfc29905592a5f220c77f2d9da762d
gilbertbahati <bahati95@berkeley.edu>
2019-06-09 20:57:38 -0700

    documented the lwr model

--- flow/models/macro/lwr.py
+++ flow/models/macro/lwr.py
@@ -6,35 +6,45 @@ import matplotlib.pyplot as plt
 matplotlib.use('TkAgg')
 
 
-def initial(x):
-    """TODO
+def initial(points):
+    """Note: This is a calibration that's used just an example"""
+
+    """ Calculate the initial density data at each
+        specified point on the road length
 
     Parameters
     ----------
-    x : TODO
-        TODO
+    points : array_like
+        points on the road length from 0 to road length
 
     Returns
     -------
-    TODO
-        TODO
+    values: array_like
+            calculated initial density data
     """
-    values = 1 * (x <= 5) + (-4 + x) * (x > 5) * (x <= 6) + 2 * (x > 6) * (
-                x < 15) + (2 * x - 28) * (x > 15) * (x <= 16) + 4 * (
-                         x > 16) * (x < 25) + 1 * (x >= 25)
+
+    values = 1 * (points <= 5) + (-4 + points) * (points > 5) * (points <= 6) + 2 * (points > 6) * (
+            points < 15) + (2 * points - 28) * (points > 15) * (points <= 16) + 4 * (
+            points > 16) * (points < 25) + 1 * (points >= 25)
     return values
 
 
 def Gflux(u, v, r):
-    """TODO
+    """ Calculate the flux vector of our data
 
     Parameters
     ----------
+    u: array_like
+       data containing boundary conditions to be analysed
+    v: double
+       maximum velocity
+    r: double
+       maximum density
 
     Returns
     -------
-    TODO
-        TODO
+    array_like
+        array of fluxes calibrated at every point of our data
     """
     # demand
     d = v * u * (1 - u / r) * (u < 0.5 * r) + 0.25 * v * r * (u >= 0.5 * r)
@@ -47,60 +57,57 @@ def Gflux(u, v, r):
     return np.minimum(d, s)
 
 
-def IBVP(u, u_r, u_l):
+def IBVP(u, u_right, u_left):
     """Implement Godunov scheme for multi-populations.
 
-    P.Goatin, June 2017 TODO: replace with full citation
+    Friedrich, Jan & Kolb, Oliver & Goettlich, Simone. (2018).
+    A Godunov type scheme for a class of LWR traffic flow models with non-local flux.
+    Networks & Heterogeneous Media. 13. 10.3934/nhm.2018024.
 
     Parameters
     ----------
-    u : TODO
-        TODO
-    u_r : TODO
-        TODO
-    u_l : TODO
-        TODO
+    u: array_like
+        density data to be analyzed and calculate next points for this data using Godunov scheme.
+        Note: at time = 0, u = initial density data
+    u_right: double
+        right boundary condition
+    u_left: double
+        left boundary condition
 
     Returns
     -------
-    TODO
-        TODO
+    array_like
+          next density data point points as culculated by the Godunov scheme
     """
-    #
-    # P.Goatin, June 2017
-    # clf
-    # """"parameters"""
 
-    dt = CFL * dx / V
+    # lam = time/distance step
     lam = dt / dx
 
-    u = np.insert(np.append(u, u_r), 0, u_l)
+    u = np.insert(np.append(u, u_right), 0, u_left)
 
     # Godunov numerical flux
-    F = Gflux(u, V, R)
+    f = Gflux(u, V, R)
 
-    Fm = np.insert(F[0:len(F) - 1], 0, F[0])
+    fm = np.insert(f[0:len(f) - 1], 0, f[0])
 
-    # Godunov scheme  (UPDATING U)
-    u = u - lam * (F - Fm)
+    # Godunov scheme  (updating u)
+    u = u - lam * (f - fm)
 
-    u = np.insert(np.append(u[1:len(u) - 1], u_r), 0, u_l)
-
+    u = np.insert(np.append(u[1:len(u) - 1], u_right), 0, u_left)
 
     return u[1:len(u) - 1]
 
 
 class LWR(gym.Env):
-    """TODO
-
-    TODO
+    """ Create an rl environment to train and test the lwr model
 
     Attributes
     ----------
-    init : TODO
-        TODO
-    boundary : TODO
-        TODO
+    init : array_like
+        density data to be analyzed.
+        Note: at time = 0, init = initial density data
+    boundary : double
+        left boundary condition
     obs : array_like
         the most recent observation
     """
@@ -110,10 +117,10 @@ class LWR(gym.Env):
 
         Parameters
         ----------
-        initial_conditions : TODO
-            TODO
-        boundary_left : TODO
-            TODO
+        initial_conditions : array_like
+            initial density data on the road length at time = 0
+        boundary_left : double
+            left boundary condition
         """
         self.init = initial_conditions
         self.boundary = boundary_left
@@ -163,45 +170,88 @@ class LWR(gym.Env):
 
 
 if __name__ == "__main__":
-    # A few more parameters we need to preset
-    #
-    # Length of road and spacial grid resolution --> this will be important
-    # when calibrating
-    global L, N, dx, x, V, R, CFL
-    L = 30  # length of road
-    N = 100  # (N = L/dx)reduce spacial grid resolution
-    dx = L / N
 
-    x = np.arange(0.5 * dx, (L - 0.5 * dx), dx)
+    """ Run our environment.
+    
+    Parameters to be set are:
+    -------------------------
+    L : double
+        Length of Road    
+    N : double
+        Spacial Grid Resolution (number of points we need on street)
+    CFL: double
+        CFL Condition (dictates the speed of information travel)
+        Note: must be between 0 and 1
+    V: double
+        maximum velocity
+    R: double
+        maximum density
+    U: array_like
+        initial data
+    u_r: double
+        right boundary condition
+    u_l: double
+        left boundary condition
+        
+    ?? Time: double
+        specify time horizon in seconds (is theoretical and depends on speed of code (dt, N and CFL) )
+    """
+    global dt
+    # Length of road
+    L = 35
 
+    # Spacial Grid Resolution
+    N = 150
+
+    # CFL condition
+    CFL = 0.95
+
+    # maximum velocity and maximum Density
     V = 1
     R = 4
 
-    CFL = 0.95
+    # change in length and points on road we are plotting against
+    dx = L / N
+    x = np.arange(0.5 * dx, (L - 0.5 * dx), dx)
 
-    # points on x axis to plot
+    # dt = change in time
+    dt = CFL * dx / V
 
-    # compute initial points
+    # initial density Points
     U = initial(x)
 
-    # initialize the right and left boundary conditions
+    # right and left boundary conditions
     u_r = 0
     u_l = 0
 
+    # #specify time horizon
+    # Time = 15
+    # specify time horizon #Time/dt find a more accurate way of finding the time should be dt/
+    iterations = 50
+
     env = LWR(U, u_l)
 
-    # run a single rollout of the environment
+    # run a single roll out of the environment
     obs = env.reset()
-    for _ in range(50):
+
+    for _ in range(int(iterations)):
         action = u_r  # agent.compute(obs)
         obs, rew, done, _ = env.step(action)
+
         # plot current profile during execution
         plt.plot(x, env.obs, 'b-')
-        plt.axis([0, L, -0.1, 4.1]) # must be preset (0, length of street(x_axis), y_axis)
+        plt.axis([0, L, -0.1, R+1])
+        plt.ylabel('Density')
+        plt.xlabel('Street Length')
+        plt.title("LWR Evolution of Density")
         plt.draw()
         plt.pause(0.00001)
         plt.clf()
-    #plot final plot
+
+    # plot final plot
     plt.plot(x, env.obs, 'b-')
-    plt.axis([0, L, -0.1, 4.1])  # must be preset (0, length of street(x_axis), y_axis)
+    plt.axis([0, L, -0.1, R+1])
+    plt.ylabel('Density')
+    plt.xlabel('Street Length')
+    plt.title("LWR Evolution of Density Final State")
     plt.show()
