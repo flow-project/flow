47f3c4c4692ef8b94039df7d15da4907cf2eb9e1
AboudyKreidieh <akreidieh@gmail.com>
2019-07-10 15:50:39 -0700

    switching to slow setup (temporarily)

--- scripts/setup_sumo_ubuntu1604.sh
+++ scripts/setup_sumo_ubuntu1604.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 echo "Installing system dependencies for SUMO"
 sudo apt-get update
-sudo apt-get install -y cmake swig libgtest-dev python-pygame python-scipy
-sudo apt-get install -y autoconf libtool pkg-config libgdal-dev libxerces-c-dev
-sudo apt-get install -y libproj-dev libfox-1.6-dev libxml2-dev libxslt1-dev
-sudo apt-get install -y build-essential curl unzip flex bison python python-dev
-sudo apt-get install -y python3-dev
-sudo pip3 install cmake cython
+sudo apt-get install cmake python g++ libxerces-c-dev libfox-1.6-dev
+sudo apt-get install libgdal-dev libproj-dev libgl2ps-dev swig
 
 echo "Installing sumo binaries and python tools"
 mkdir -p $HOME/sumo_binaries
 pushd $HOME/sumo_binaries
-wget https://akreidieh.s3.amazonaws.com/sumo/flow-0.4.1/binaries-ubuntu1604.tar.xz
-tar -xf binaries-ubuntu1604.tar.xz
-rm binaries-ubuntu1604.tar.xz
-chmod +x bin/*
-python tools/build/setup-traci.py install
+git clone https://github.com/eclipse/sumo.git
+cd sumo
+git checkout 2147d155b1
+cmake .
+make -j$(nproc)
 popd
 
-echo 'export PATH="$PATH:$HOME/sumo_binaries/bin"' >> ~/.bashrc
-echo 'export SUMO_HOME="$HOME/sumo_binaries"' >> ~/.bashrc
+echo 'export PATH="$PATH:$HOME/sumo_binaries/sumo/bin"' >> ~/.bashrc
+echo 'export SUMO_HOME="$HOME/sumo_binaries/sumo"' >> ~/.bashrc
+echo 'export PYTHONPATH="$PYTHONPATH:$HOME/sumo_binaries/sumo/tools"' >> ~/.bashrc
