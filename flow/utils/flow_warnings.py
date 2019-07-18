22f3374a5172cf3ef5fad80aa7a826dd062abdf0
Cathy Wu <cathywu@eecs.berkeley.edu>
2019-07-17 23:02:43 -0700

    add deprecated decorator

--- flow/utils/flow_warnings.py
+++ flow/utils/flow_warnings.py
@@ -18,3 +18,25 @@ def deprecation_warning(obj, dep_from, dep_to):
     warnings.warn(
         "The attribute {} in {} is deprecated, use {} instead.".format(
             dep_from, obj.__class__.__name__, dep_to))
+
+
+def deprecated(message):
+    """Print a deprecation warning. Use as decorator.
+
+    Parameters
+    ----------
+    message : str
+        Description of new method to use
+    """
+    def deprecated_decorator(func):
+        def deprecated_func(*args, **kwargs):
+            warnings.warn(
+                "{} is a deprecated function. {}".format(func.__name__,
+                                                         message),
+                category=DeprecationWarning, stacklevel=2)
+            warnings.simplefilter('default', DeprecationWarning)
+            return func(*args, **kwargs)
+
+        return deprecated_func
+
+    return deprecated_decorator
