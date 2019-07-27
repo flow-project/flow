def2a81809fb7f333194219add24e9739dd44f3a
nathanlct <nathanlct@icloud.com>
2019-07-27 02:25:34 -0700

    Default num_vehicles to 0 when adding vehicle type

--- flow/core/params.py
+++ flow/core/params.py
@@ -239,7 +239,7 @@ class VehicleParams:
             lane_change_controller=(SimLaneChangeController, {}),
             routing_controller=None,
             initial_speed=0,
-            num_vehicles=1,
+            num_vehicles=0,
             car_following_params=None,
             lane_change_params=None):
         """Add a sequence of vehicles to the list of vehicles in the network.
