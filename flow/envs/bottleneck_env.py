64186b40e2c4405fd77fb46bdbd41e43ac1bdc18
Ashkan Y <yousefpour.ashkan@gmail.com>
2019-07-27 18:23:29 -0700

    Document all attributes in classes (#661)
    * add doc in merge
    
    * Added attribute documentation for loop_accel.py
    
    * Added attribute documenation for green_wave_env.py
    
    * added attributes for kernel/simulation/aimsun.py
    
    * added attributes to LaneChangeAccelPOEnv in lane_changing.py
    
    * added attributes to kernel.scenario.aimsun
    
    * Add missing attributes for base env.
    
    * Add missing attribute documentation to base env.
    
    * Add missing attribute documentation to pyglet renderer.
    
    * Fix pip8.
    
    * removed private attributes from the docstring
    
    * changed <str> to 'of str'
    
    * Added attributes for bottleneck env
    
    * pep8
    
    * Apply suggestions from code review

--- flow/envs/bottleneck_env.py
+++ flow/envs/bottleneck_env.py
@@ -96,9 +96,99 @@ class BottleneckEnv(Env):
     portion of the bay bridge. Contains ramp meters, and a toll both.
 
     Additional
+    ----------
         Vehicles are rerouted to the start of their original routes once
         they reach the end of the network in order to ensure a constant
         number of vehicles.
+
+    Attributes
+    ----------
+    scaling : int
+        A factor describing how many lanes are in the system. Scaling=1 implies
+        4 lanes going to 2 going to 1, scaling=2 implies 8 lanes going to 4
+        going to 2, etc.
+    edge_dict : dict of dicts
+        A dict mapping edges to a dict of lanes where each entry in the lane
+        dict tracks the vehicles that are in that lane. Used to save on
+        unnecessary lookups.
+    cars_waiting_for_toll : {veh_id: {lane_change_mode: int, color: (int)}}
+        A dict mapping vehicle ids to a dict tracking the color and lane change
+        mode of vehicles before they entered the toll area. When vehicles exit
+        the tollbooth area, these values are used to set the lane change mode
+        and color of the vehicle back to how they were before they entered the
+        toll area.
+    cars_before_ramp : {veh_id: {lane_change_mode: int, color: (int)}}
+        Identical to cars_waiting_for_toll, but used to track cars approaching
+        the ramp meter versus approaching the tollbooth.
+    toll_wait_time : np.ndarray(float)
+        Random value, sampled from a gaussian indicating how much a vehicle in
+        each lane should wait to pass through the toll area. This value is
+        re-sampled for each approaching vehicle. That is, whenever a vehicle
+        approaches the toll area, we re-sample from the Gaussian to determine
+        its weight time.
+    fast_track_lanes : np.ndarray(int)
+        Middle lanes of the tollbooth are declared fast-track lanes, this numpy
+        array keeps track of which lanes these are. At a fast track lane, the
+        mean of the Gaussian from which we sample wait times is given by
+        MEAN_NUM_SECONDS_WAIT_AT_FAST_TRACK.
+    tl_state : str
+        String tracking the color of the traffic lights at the tollbooth. These
+        traffic lights are used imitate the effect of a tollbooth. If lane 1-4
+        are respectively green, red, red, green, then this string would be
+        "GrrG"
+    n_crit : int
+        The ALINEA algorithm adjusts the ratio of red to green time for the
+        ramp-metering traffic light based on feedback on how congested the
+        system is. As the measure of congestion, we use the number of vehicles
+        stuck in the bottleneck (edge 4). The critical operating value it tries
+        to stabilize the number of vehicles in edge 4 is n_crit. If there are
+        more than n_crit vehicles on edge 4, we increase the fraction of red
+        time to decrease the inflow to edge 4.
+    q_max : float
+        The ALINEA algorithm tries to control the flow rate through the ramp
+        meter. q_max is the maximum possible estimated flow we allow through
+        the bottleneck and can be converted into a maximum value for the ratio
+        of green to red time that we allow.
+    q_min : float
+        Similar to q_max, this is used to set the minimum value of green to red
+        ratio that we allow.
+    q : float
+        This value tracks the flow we intend to allow through the bottleneck.
+        For details on how it is computed, please read the alinea method or the
+        paper linked in that method.
+    feedback_update_time : float
+        The parameters of the ALINEA algorithm are only updated every
+        feedback_update_time seconds.
+    feedback_timer : float
+        This keeps track of how many seconds have passed since the ALINEA
+        parameters were last updated. If it exceeds feedback_update_time, the
+        parameters are updated
+    cycle_time : int
+        This value tracks how long a green-red cycle of the ramp meter is. The
+        first self.green_time seconds will be green and the remainder of the
+        cycle will be red.
+    ramp_state : np.ndarray
+        Array of floats of length equal to the number of lanes. For each lane,
+        this value tracks how many seconds of a given cycle have passed in that
+        lane. Each lane is offset from its adjacent lanes by
+        cycle_offset/(self.scaling * MAX_LANES) seconds. This offsetting means
+        that lights are offset in when they releasse vehicles into the
+        bottleneck. This helps maximize the throughput of the ramp meter.
+    green_time : float
+        How many seconds of a given cycle the light should remain green 4.
+        Defaults to 4 as this is just enough time for two vehicles to enter the
+        bottleneck from a given traffic light.
+    feedback_coeff : float
+        This is the gain on the feedback in the ALINEA algorithm
+    smoothed_num : np.ndarray
+        Numpy array keeping track of how many vehicles were in edge 4 over the
+        last 10 time seconds. This provides a more stable estimate of the
+        number of vehicles in edge 4.
+    outflow_index : int
+        Keeps track of which index of smoothed_num we should update with the
+        latest number of vehicles in the bottleneck. Should eventually be
+        deprecated as smoothed_num should be switched to a queue instead of an
+        array.
     """
 
     def __init__(self, env_params, sim_params, scenario, simulator='traci'):
@@ -122,7 +212,6 @@ class BottleneckEnv(Env):
             int(np.ceil(1.5 * self.scaling)), int(np.ceil(2.6 * self.scaling)))
 
         self.tl_state = ""
-        self.next_period = START_RECORD_TIME / self.sim_step
 
         # values for the ALINEA ramp meter algorithm
         self.n_crit = env_add_params.get("n_crit", 8)
@@ -178,9 +267,6 @@ class BottleneckEnv(Env):
         self.outflow_index = \
             (self.outflow_index + 1) % self.smoothed_num.shape[0]
 
-        if self.time_counter > self.next_period:
-            self.next_period += PERIOD / self.sim_step
-
     def ramp_meter_lane_change_control(self):
         """Control the lane changing behavior.
 
@@ -235,6 +321,14 @@ class BottleneckEnv(Env):
         Papageorgiou. "Toll plaza merging traffic control for throughput
         maximization." Journal of Transportation Engineering 136.1 (2009):
         67-76.
+
+        Essentially, we apply feedback control around the value self.n_crit.
+        We keep track of the number of vehicles in edge 4, average them across
+        time ot get a smoothed value and then compute
+        q_{t+1} = clip(q_t + K * (n_crit - n_avg), q_min, q_max). We then
+        convert this into a cycle_time value via cycle_time = 7200 / q.
+        Cycle_time = self.green_time + red_time i.e. the first self.green_time
+        seconds of a cycle will be green, and the remainder will be all red.
         """
         self.feedback_timer += self.sim_step
         self.ramp_state += self.sim_step
