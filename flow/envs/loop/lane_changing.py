beb2d93c7eb639037542954d137c7e64fbe77a53
AboudyKreidieh <akreidieh@gmail.com>
2019-09-09 12:43:47 -0700

    added deprecation support
    - added a deprecation function for classes and functions
    - renames deprecation_warning ->deprecated_attribute
    - added deprecation warnings for all old names of environments
    - updated version to 0.5.0.dev

---
+++ flow/envs/loop/lane_changing.py
@@ -0,0 +1,19 @@
+"""Pending deprecation file.
+
+To view the actual content, go to: flow/envs/ring/lane_change_accel.py
+"""
+from flow.utils.flow_warnings import deprecated
+from flow.envs.ring.lane_change_accel import LaneChangeAccelEnv as LCEnv
+from flow.envs.ring.lane_change_accel import LaneChangeAccelPOEnv as LCPOEnv
+
+
+@deprecated('flow.envs.loop.lane_changing',
+            'flow.envs.ring.lane_change_accel.LaneChangeAccelEnv')
+class LaneChangeAccelEnv(LCEnv):
+    pass
+
+
+@deprecated('flow.envs.loop.lane_changing',
+            'flow.envs.ring.lane_change_accel.LaneChangeAccelPOEnv')
+class LaneChangeAccelPOEnv(LCPOEnv):
+    pass
--- flow/utils/flow_warnings.py
+++ flow/utils/flow_warnings.py
@@ -1,9 +1,13 @@
 """Warnings that may be printed by Flow (e.g. deprecation warnings)."""
 
+import functools
+import inspect
 import warnings
 
+string_types = (type(b''), type(u''))
 
-def deprecation_warning(obj, dep_from, dep_to):
+
+def deprecated_attribute(obj, dep_from, dep_to):
     """Print a deprecation warning.
 
     Parameters
@@ -18,3 +22,81 @@ def deprecation_warning(obj, dep_from, dep_to):
     warnings.warn(
         "The attribute {} in {} is deprecated, use {} instead.".format(
             dep_from, obj.__class__.__name__, dep_to))
+
+
+def deprecated(base, new_path):
+    """Print a deprecation warning.
+
+    This is a decorator which can be used to mark functions as deprecated. It
+    will result in a warning being emitted when the function is used.
+    """
+    # if isinstance(base, string_types):
+
+    # The @deprecated is used with a 'reason'.
+    #
+    # .. code-block:: python
+    #
+    #    @deprecated("please, use another function")
+    #    def old_function(x, y):
+    #      pass
+
+    def decorator(func1):
+
+        if inspect.isclass(func1):
+            fmt1 = "The class {base}.{name} is deprecated, use " \
+                   "{new_path} instead."
+        else:
+            fmt1 = "The function {base}.{name} is deprecated, use " \
+                   "{new_path} instead."
+
+        @functools.wraps(func1)
+        def new_func1(*args, **kwargs):
+            warnings.simplefilter('always', PendingDeprecationWarning)
+            warnings.warn(
+                fmt1.format(
+                    base=base,
+                    name=func1.__name__,
+                    new_path=new_path
+                ),
+                category=PendingDeprecationWarning,
+                stacklevel=2
+            )
+            warnings.simplefilter('default', PendingDeprecationWarning)
+            return func1(*args, **kwargs)
+
+        return new_func1
+
+    return decorator
+    #
+    # elif inspect.isclass(reason) or inspect.isfunction(reason):
+    #
+    #     # The @deprecated is used without any 'reason'.
+    #     #
+    #     # .. code-block:: python
+    #     #
+    #     #    @deprecated
+    #     #    def old_function(x, y):
+    #     #      pass
+    #
+    #     func2 = reason
+    #
+    #     if inspect.isclass(func2):
+    #         fmt2 = "Call to deprecated class {name}."
+    #     else:
+    #         fmt2 = "Call to deprecated function {name}."
+    #
+    #     @functools.wraps(func2)
+    #     def new_func2(*args, **kwargs):
+    #         warnings.simplefilter('always', DeprecationWarning)
+    #         warnings.warn(
+    #             fmt2.format(name=func2.__name__),
+    #             category=DeprecationWarning,
+    #             stacklevel=2
+    #         )
+    #         warnings.simplefilter('default', DeprecationWarning)
+    #         return func2(*args, **kwargs)
+    #
+    #     return new_func2
+    #
+    # else:
+    #     raise TypeError(repr(type(reason)))
