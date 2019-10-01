74fe447fd4501357387e4f2f65e255fe80c17584
Alben Bagabaldo <36107964+albenbagabaldo@users.noreply.github.com>
2019-10-01 15:01:40 -0700

    Addressing issue #743
    This is a fix for Conda having removed support for Python 2.7.4 and not being able to run setup_aimsun.sh

--- scripts/setup_aimsun.sh
+++ scripts/setup_aimsun.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# If the following error is encountered upon installation:
+# 'PackagesNotFoundError: The following packages are not available from current channels: - python=2.7.4'
+# Run the two lines of code below:
+
+# conda config --append channels https://repo.anaconda.com/pkgs/free
+# conda config --append channels https://repo.anaconda.com/pkgs/pro
+
 # create the conda environment
 conda create -y -n aimsun_flow python=2.7.4
 
