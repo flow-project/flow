3997e793673e9d98e67cd66254efdfc0c128ae9e
Ashkan Y <yousefpour.ashkan@gmail.com>
2019-10-02 14:52:28 -0700

    fix test for highway

--- examples/exp_configs/non_rl/highway.py
+++ examples/exp_configs/non_rl/highway.py
@@ -3,7 +3,7 @@
 from flow.controllers import IDMController
 from flow.core.params import SumoParams, EnvParams, NetParams, InitialConfig
 from flow.core.params import VehicleParams, InFlows
-from flow.envs.ring.accel import ADDITIONAL_ENV_PARAMS
+from flow.envs.ring.lane_change_accel import ADDITIONAL_ENV_PARAMS
 from flow.networks.highway import ADDITIONAL_NET_PARAMS
 
 vehicles = VehicleParams()
