2aefb6075b49d0c24ec9243dc046d66aca06d90b
Ashkan Y <yousefpour.ashkan@gmail.com>
2019-11-21 16:40:01 -0800

    Revert "Trying isolating bug"
    This reverts commit c359d8fc9c8c32d000c102b5a2622a11e23660df.

--- tests/fast_tests/test_visualizers.py
+++ tests/fast_tests/test_visualizers.py
@@ -37,22 +37,22 @@ class TestVisualizerRLlib(unittest.TestCase):
         visualizer_rllib(pass_args)
 
     # FIXME(ev) set the horizon so that this runs faster
-    # def test_visualizer_multi(self):
-    #     """Test for multi-agent visualization"""
-    #     try:
-    #         ray.init(num_cpus=1)
-    #     except Exception:
-    #         pass
-    #     # current path
-    #     current_path = os.path.realpath(__file__).rsplit('/', 1)[0]
+    def test_visualizer_multi(self):
+        """Test for multi-agent visualization"""
+        try:
+            ray.init(num_cpus=1)
+        except Exception:
+            pass
+        # current path
+        current_path = os.path.realpath(__file__).rsplit('/', 1)[0]
 
-    #     # run the experiment and check it doesn't crash
-    #     arg_str = '{}/../data/rllib_data/multi_agent 1 --num_rollouts 1 ' \
-    #               '--render_mode no_render ' \
-    #               '--horizon 10'.format(current_path).split()
-    #     parser = vs_rllib.create_parser()
-    #     pass_args = parser.parse_args(arg_str)
-    #     visualizer_rllib(pass_args)
+        # run the experiment and check it doesn't crash
+        arg_str = '{}/../data/rllib_data/multi_agent 1 --num_rollouts 1 ' \
+                  '--render_mode no_render ' \
+                  '--horizon 10'.format(current_path).split()
+        parser = vs_rllib.create_parser()
+        pass_args = parser.parse_args(arg_str)
+        visualizer_rllib(pass_args)
 
 
 # class TestVisualizerRLlab(unittest.TestCase):
