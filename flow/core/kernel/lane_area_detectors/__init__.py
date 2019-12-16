d30d0860a54d3c528028701e5b118dc92d9aaa33
Vallout <valentin.duee@gmail.com>
2019-12-16 15:33:08 +0100

    small change

--- flow/core/kernel/lane_area_detectors/__init__.py
+++ flow/core/kernel/lane_area_detectors/__init__.py
@@ -1,4 +1,4 @@
-"""Empty init file to ensure documentation for traffic lights is created."""
+"""Empty init file to ensure documentation for lane area detectors is created."""
 
 from flow.core.kernel.lane_area_detectors.base import KernelLaneAreaDetector
 from flow.core.kernel.lane_area_detectors.traci import TraCILaneAreaDetector
