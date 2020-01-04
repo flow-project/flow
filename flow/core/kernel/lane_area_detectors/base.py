9ab6fefaf0ae61ff7f793e4a9f5ce2855cac8fc2
Vallout <valentin.duee@gmail.com>
2020-01-04 16:29:42 +0100

    fix formatting

--- flow/core/kernel/lane_area_detectors/base.py
+++ flow/core/kernel/lane_area_detectors/base.py
@@ -62,37 +62,37 @@ class KernelLaneAreaDetector(object):
     https://sumo.dlr.de/docs/TraCI/Lane_Area_Detector_Value_Retrieval.html
     """
     def get_n_veh_seen(self, detector_id):
-        """Return number of seen vehicles in the last simulation step"""
+        """Return number of seen vehicles in the last simulation step."""
         raise NotImplementedError
 
     def get_m_jam_length(self, detector_id):
-        """Return length of jam in meters during the last simulation step"""
+        """Return length of jam in meters during the last simulation step."""
         raise NotImplementedError
 
     def get_v_jam_length(self, detector_id):
         """Return length of jam by number of vehicles during the last simulation 
-        step
+        step.
         """
         raise NotImplementedError
 
     def get_n_halting(self, detector_id):
         """Return number of vehicles that halted(zero speed) in the last simulation
-        step
+        step.
         """
         raise NotImplementedError
 
     def get_mean_speed(self, detector_id):
-        """Return the mean speed of the vehicles in the last simulation step"""
+        """Return the mean speed of the vehicles in the last simulation step."""
         raise NotImplementedError
 
     def get_occupancy(self, detector_id):
         """Return the percentage of space the detector was occupied by vehicles
-        in the last simulation step
+        in the last simulation step.
         """
         raise NotImplementedError
 
     def get_v_id_list(self, detector_id):
         """Return the ids of all vehicles that were on the detector in the last
-        simulation step
+        simulation step.
         """
         raise NotImplementedError
