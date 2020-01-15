5c4aabfbd8f01b4a7b523467686c363b0a29a15e
Nicholas Liu <nicholas.j.liu@gmail.com>
2020-01-15 15:41:14 -0800

    add sidewalks

--- flow/networks/traffic_light_grid.py
+++ flow/networks/traffic_light_grid.py
@@ -550,12 +550,15 @@ class TrafficLightGridNetwork(Network):
             In SUMO, lanes are numbered from 0, starting from the rightmost lane. Thus, a legal left turn by a left lane
             would be lane n to lane n. A legal right turn from the rightmost lane would be lane 0 to lane 0."""
 
-            right_most_lane, left_most_lane = 0, self.horizontal_lanes - 1
-
+            #right_most_lane, left_most_lane = 0, self.horizontal_lanes - 1
+            right_most_lane, left_most_lane = 1, self.horizontal_lanes
+            
             # TODO: ONLY leftmost lanes can turn left i.e stop non leftmost lanes from turning left as well
             # build vertical connections for RIGHT edge (1,0)
             for hor_l in range(self.horizontal_lanes):
                 for vert_l in range(self.vertical_lanes):
+                    hor_l += 1
+                    vert_l += 1
                     # TODO: fix the strange lane turns
                     if legal_turns:
                         if hor_l == vert_l:
@@ -573,12 +576,16 @@ class TrafficLightGridNetwork(Network):
             # build horizontal connection for RIGHT edge (1,0)
             for hor_l1 in range(self.horizontal_lanes):
                 for hor_l2 in range(self.horizontal_lanes):
+                    hor_l1 += 1
+                    hor_l2 += 1
                     if hor_l1 == hor_l2: # when going straight, you can only go directly straight
                         node_cons_list += single_con_dict(right_edge_in, left_edge_out, hor_l1, hor_l2, signal_group)
 
             # build vertical connections for LEFT edge (-1,0)
             for hor_l in range(self.horizontal_lanes):
                 for vert_l in range(self.vertical_lanes):
+                    hor_l += 1
+                    vert_l += 1
                     if legal_turns:
                         if hor_l == vert_l:  # only allow legal lane transitions
                             if hor_l == right_most_lane and vert_l == right_most_lane: # only right most lane can turn right
@@ -594,18 +601,24 @@ class TrafficLightGridNetwork(Network):
             # build horizontal connection for LEFT edge (-1,0)
             for hor_l1 in range(self.horizontal_lanes):
                 for hor_l2 in range(self.horizontal_lanes):
+                    hor_l1 += 1
+                    hor_l2 += 1
                     if hor_l1 == hor_l2:
                         node_cons_list += single_con_dict(left_edge_in, right_edge_out, hor_l1, hor_l2, signal_group)
 
             # build vertical connection for TOP edge (0, 1)
             for vert_l1 in range(self.vertical_lanes):
                 for vert_l2 in range(self.vertical_lanes):
+                    vert_l1 += 1
+                    vert_l2 += 1
                     if vert_l1 == vert_l2:
                         node_cons_list += single_con_dict(top_edge_in, bottom_edge_out, vert_l1, vert_l2, signal_group)
 
             # build horizontal connections for TOP edge (0, 1)
             for vert_l in range(self.vertical_lanes):
                 for hor_l in range(self.horizontal_lanes):
+                    vert_l += 1
+                    hor_l += 1
                     if legal_turns:
                         if vert_l == hor_l:  # only allow legal lane transitions
                             if vert_l == right_most_lane and hor_l == right_most_lane: # only right most lane can turn right
@@ -621,12 +634,16 @@ class TrafficLightGridNetwork(Network):
             # build vertical connection for BOTTOM edge (0, -1)
             for vert_l1 in range(self.horizontal_lanes):
                 for vert_l2 in range(self.vertical_lanes):
+                    vert_l1 += 1
+                    vert_l2 += 1
                     if vert_l1 == vert_l2:
                         node_cons_list += single_con_dict(bottom_edge_in, top_edge_out, vert_l1, vert_l2, signal_group)
 
             # build horizontal connections for BOTTOM edge (0, -1)
             for hor_l in range(self.horizontal_lanes):
                 for vert_l in range(self.vertical_lanes):
+                    vert_l += 1
+                    hor_l += 1
                     # if legal_turns:
                     #     if hor_l == vert_l:
                     #         node_cons_list += single_con_dict(bottom_edge_in, left_edge_out, vert_l, hor_l, signal_group)
