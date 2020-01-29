7dfbd17edfcf3ebaf253cb36d330f1aa2a3cea64
AboudyKreidieh <akreidieh@gmail.com>
2020-01-28 20:24:21 -0800

    added MultiAgentWaveAttenuationPOEnv

--- flow/envs/multiagent/ring/wave_attenuation.py
+++ flow/envs/multiagent/ring/wave_attenuation.py
@@ -11,6 +11,8 @@ abs/1710.05465, 2017. [Online]. Available: https://arxiv.org/abs/1710.05465
 import numpy as np
 from gym.spaces.box import Box
 from flow.envs.multiagent.base import MultiEnv
+from flow.envs.ring.wave_attenuation import WaveAttenuationEnv
+
 
 ADDITIONAL_ENV_PARAMS = {
     # maximum acceleration of autonomous vehicles
@@ -35,6 +37,7 @@ class MultiWaveAttenuationPOEnv(MultiEnv):
 
     States
         See parent class
+
     Actions
         See parent class
 
@@ -43,7 +46,6 @@ class MultiWaveAttenuationPOEnv(MultiEnv):
 
     Termination
         See parent class
-
     """
 
     @property
@@ -125,7 +127,117 @@ class MultiWaveAttenuationPOEnv(MultiEnv):
             lead_id = self.k.vehicle.get_leader(rl_id) or rl_id
             self.k.vehicle.set_observed(lead_id)
 
-    def gen_edges(self, i):
+    @staticmethod
+    def gen_edges(i):
         """Return the edges corresponding to the rl id."""
         return ['top_{}'.format(i), 'left_{}'.format(i),
                 'right_{}'.format(i), 'bottom_{}'.format(i)]
+
+
+class MultiAgentWaveAttenuationPOEnv(MultiEnv, WaveAttenuationEnv):
+    """Multi-agent variant of WaveAttenuationPOEnv.
+
+    Required from env_params:
+
+    * max_accel: maximum acceleration of autonomous vehicles
+    * max_decel: maximum deceleration of autonomous vehicles
+    * ring_length: bounds on the ranges of ring road lengths the autonomous
+      vehicle is trained on. If set to None, the environment sticks to the ring
+      road specified in the original network definition.
+
+    States
+        The state of each agent (AV) consists of the speed and headway of the
+        ego vehicle, as well as the difference in speed between the ego vehicle
+        and its leader. There is no assumption on the number of vehicles in the
+        network.
+
+    Actions
+        Actions are an acceleration for each rl vehicle, bounded by the maximum
+        accelerations and decelerations specified in EnvParams.
+
+    Rewards
+        The reward function rewards high average speeds from all vehicles in
+        the network, and penalizes accelerations by the rl vehicle. This reward
+        is shared by all agents.
+
+    Termination
+        A rollout is terminated if the time horizon is reached or if two
+        vehicles collide into one another.
+    """
+
+    @property
+    def observation_space(self):
+        """See class definition."""
+        return Box(low=-1, high=1, shape=(3,), dtype=np.float32)
+
+    @property
+    def action_space(self):
+        """See class definition."""
+        return Box(
+            low=-np.abs(self.env_params.additional_params['max_decel']),
+            high=self.env_params.additional_params['max_accel'],
+            shape=(1,),
+            dtype=np.float32)
+
+    def get_state(self):
+        """See class definition."""
+        obs = {}
+        for rl_id in self.k.vehicle.get_rl_ids():
+            lead_id = self.k.vehicle.get_leader(rl_id) or rl_id
+
+            # normalizers
+            max_speed = 15.
+            max_length = self.env_params.additional_params['ring_length'][1]
+
+            observation = np.array([
+                self.k.vehicle.get_speed(rl_id) / max_speed,
+                (self.k.vehicle.get_speed(lead_id) -
+                 self.k.vehicle.get_speed(rl_id))
+                / max_speed,
+                self.k.vehicle.get_headway(rl_id) / max_length
+            ])
+            obs.update({rl_id: observation})
+
+        return obs
+
+    def _apply_rl_actions(self, rl_actions):
+        """Split the accelerations by ring."""
+        if rl_actions:
+            rl_ids = list(rl_actions.keys())
+            accel = list(rl_actions.values())
+            self.k.vehicle.apply_acceleration(rl_ids, accel)
+
+    def compute_reward(self, rl_actions, **kwargs):
+        """See class definition."""
+        # in the warmup steps
+        if rl_actions is None:
+            return {}
+
+        vel = np.array([
+            self.k.vehicle.get_speed(veh_id)
+            for veh_id in self.k.vehicle.get_ids()
+        ])
+
+        if any(vel < -100) or kwargs['fail']:
+            return 0.
+
+        # reward average velocity
+        eta_2 = 4.
+        reward = eta_2 * np.mean(vel) / 20
+
+        # punish accelerations (should lead to reduced stop-and-go waves)
+        eta = 4  # 0.25
+        mean_actions = np.mean(np.abs(np.array(rl_actions)))
+        accel_threshold = 0
+
+        if mean_actions > accel_threshold:
+            reward += eta * (accel_threshold - mean_actions)
+
+        return {key: float(reward) for key in self.k.vehicle.get_rl_ids()}
+
+    def additional_command(self):
+        """Define which vehicles are observed for visualization purposes."""
+        # specify observed vehicles
+        for rl_id in self.k.vehicle.get_rl_ids():
+            lead_id = self.k.vehicle.get_leader(rl_id) or rl_id
+            self.k.vehicle.set_observed(lead_id)
