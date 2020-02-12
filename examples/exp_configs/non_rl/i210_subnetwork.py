7114ccbff34baf9cc0b008bc844944d3ca2cd9cc
AboudyKreidieh <akreidieh@gmail.com>
2020-02-12 12:08:39 -0800

    semi-working i-210 subnetwork environment

---
+++ examples/exp_configs/non_rl/i210_subnetwork.py
@@ -0,0 +1,65 @@
+"""I-210 subnetwork example."""
+import os
+
+from flow.core.params import SumoParams, EnvParams, NetParams
+from flow.core.params import VehicleParams, InitialConfig
+from flow.core.params import InFlows
+import flow.config as config
+from flow.envs import TestEnv
+from flow.networks.i210_subnetwork import I210SubNetwork, EDGES_DISTRIBUTION
+
+
+# no vehicles in the network
+vehicles = VehicleParams()
+vehicles.add(
+    "human",
+    num_vehicles=10,
+)
+
+
+NET_TEMPLATE = os.path.join(
+    config.PROJECT_PATH,
+    "examples/exp_configs/templates/sumo/test.net.xml")
+
+
+flow_params = dict(
+    # name of the experiment
+    exp_tag='I-210_subnetwork',
+
+    # name of the flow environment the experiment is running on
+    env_name=TestEnv,
+
+    # name of the network class the experiment is running on
+    network=I210SubNetwork,
+
+    # simulator that is used by the experiment
+    simulator='traci',
+
+    # simulation-related parameters
+    sim=SumoParams(
+        sim_step=0.1,
+        render=True,
+    ),
+
+    # environment related parameters (see flow.core.params.EnvParams)
+    env=EnvParams(
+        horizon=3000,
+    ),
+
+    # network-related parameters (see flow.core.params.NetParams and the
+    # network's documentation or ADDITIONAL_NET_PARAMS component)
+    net=NetParams(
+        inflows=InFlows(),
+        template=NET_TEMPLATE
+    ),
+
+    # vehicles to be placed in the network at the start of a rollout (see
+    # flow.core.params.VehicleParams)
+    veh=vehicles,
+
+    # parameters specifying the positioning of vehicles upon initialization/
+    # reset (see flow.core.params.InitialConfig)
+    initial=InitialConfig(
+        edges_distribution=EDGES_DISTRIBUTION,
+    ),
+)
