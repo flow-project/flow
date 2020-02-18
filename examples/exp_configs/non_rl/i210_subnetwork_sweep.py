235221b7cdafc4781506d395162a001fb824e664
Eugene Vinitsky <vinitsky.eugene@gmail.com>
2020-02-17 21:23:19 -0800

    Set up the values for the hyperparameter sweep

--- examples/exp_configs/non_rl/i210_subnetwork_sweep.py
+++ examples/exp_configs/non_rl/i210_subnetwork_sweep.py
@@ -26,8 +26,8 @@ default_dict={"lane_change_mode": "no_lat_collide",
                  "lc_pushy_gap": 0.6,
                  "lc_assertive": 1,
                  "lc_accel_lat": 1.0}
-# sweep_dict = OrderedDict({"lc_strategic": [0.5, 1.0, 2.0], "lc_cooperative": [0.5, 1.0, 2.0]})
-sweep_dict = OrderedDict({"lc_strategic": [1.0], "lc_cooperative": [1.0]})
+sweep_dict = OrderedDict({"lc_strategic": [0.5, 1.0, 2.0], "lc_cooperative": [0.5, 1.0, 2.0],
+                         "lc_assertive": [1, 2.0], "lcLookaheadLeft": [2.0, 5.0]})
 allNames = sorted(sweep_dict)
 combinations = itertools.product(*(sweep_dict[Name] for Name in allNames))
 combination_list = list(combinations)
