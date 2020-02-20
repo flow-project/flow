555218b037ce8cabb46716a040bf4aab02edbe64
Eugene Vinitsky <vinitsky.eugene@gmail.com>
2020-02-19 18:22:45 -0800

    Add lc look ahead option

--- examples/exp_configs/non_rl/i210_subnetwork_sweep.py
+++ examples/exp_configs/non_rl/i210_subnetwork_sweep.py
@@ -27,8 +27,8 @@ default_dict={"lane_change_mode": "strategic",
                  "lc_pushy_gap": 0.6,
                  "lc_assertive": 1,
                  "lc_accel_lat": 1.0}
-sweep_dict = OrderedDict({"lc_strategic": [1.0, 2.0], "lc_cooperative": [1.0, 2.0],
-                         "lc_assertive": [1, 2.0], "lc_look_ahead_left": [2.0, 5.0]})
+sweep_dict = OrderedDict({"lc_strategic": [1.0, 2.0, 4.0, 8.0], "lc_cooperative": [1.0, 2.0],
+                          "lc_look_ahead_left": [2.0, 4.0]})
 
 allNames = sorted(sweep_dict)
 combinations = itertools.product(*(sweep_dict[Name] for Name in allNames))
