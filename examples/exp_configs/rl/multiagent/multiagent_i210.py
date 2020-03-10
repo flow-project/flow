154441c4143565749f8872f872b8624c95c6a8ec
Eugene Vinitsky <vinitsky.eugene@gmail.com>
2020-03-09 18:22:26 -0700

    Add I210 multienv tests

--- examples/exp_configs/rl/multiagent/multiagent_i210.py
+++ examples/exp_configs/rl/multiagent/multiagent_i210.py
@@ -40,6 +40,8 @@ additional_env_params = ADDITIONAL_ENV_PARAMS.copy()
 additional_env_params.update({
     'max_accel': 1,
     'max_decel': 1,
+    # configure the observation space. Look at the I210MultiEnv class for more info.
+    'lead_obs': True,
 })
 
 # CREATE VEHICLE TYPES AND INFLOWS
@@ -54,7 +56,7 @@ vehicles.add(
 )
 vehicles.add(
     "av",
-    acceleration_controller=RLController,
+    acceleration_controller=(RLController, {}),
     num_vehicles=0,
 )
 
@@ -135,7 +137,7 @@ flow_params = dict(
     # environment related parameters (see flow.core.params.EnvParams)
     env=EnvParams(
         horizon=HORIZON,
-        sims_per_step=10,
+        sims_per_step=1,
         additional_params=additional_env_params,
     ),
 
