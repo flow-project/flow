91e1a09ae87dc702c9a294907326a95f92810b06
Kanaad Parvate <kanaadp@gmail.com>
2020-03-11 17:59:34 -0700

    changes for EC2

--- multi_ring_transfer/run_multilane_ring_transfer.sh
+++ multi_ring_transfer/run_multilane_ring_transfer.sh
@@ -4,8 +4,9 @@ if [ -z "$1" ]
     exit
 fi
 
-date="$(date +'%d_%m_%H%M%S')"
+date="$(date +'%S')"
 echo $date
+echo kp_$date\_$1
 while true; do
     read -p "Run experiment: $1 on cluster? " yn
     case $yn in
@@ -13,7 +14,7 @@ while true; do
                                      --use_s3 --multi_node \
                                      --exp_name $1_$date\
                                      --num_cpus 4 --num_rollouts 8 \
-                                     --num_iter 1" --start --stop --cluster-name kp_$date_$1; break;;
+                                     --num_iter 10 --checkpoint_freq 5" --start --stop --cluster-name kp_$date\_$1; break;;
         [Nn]* ) exit;;
         * ) echo "Please answer yes or no.";;
     esac
