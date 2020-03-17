dde786ef45eb4b8720a72bd174bbf8418bd90046
Eugene Vinitsky <vinitsky.eugene@gmail.com>
2020-03-16 23:11:13 -0700

    increase horizon

--- examples/exp_configs/rl/multiagent/multiagent_i210.py
+++ examples/exp_configs/rl/multiagent/multiagent_i210.py
@@ -25,7 +25,7 @@ from flow.utils.registry import make_create_env
 # SET UP PARAMETERS FOR THE SIMULATION
 
 # number of steps per rollout
-HORIZON = 1000
+HORIZON = 2000
 
 # percentage of autonomous vehicles compared to human vehicles on highway
 PENETRATION_RATE = 10
