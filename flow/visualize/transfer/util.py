3d65ce1ea48a9e081be7c6c5dda3f536124dd0be
Kanaad Parvate <kanaadp@gmail.com>
2020-03-27 19:38:41 -0700

    fixed transfer for i210

--- flow/visualize/transfer/util.py
+++ flow/visualize/transfer/util.py
@@ -6,7 +6,7 @@ from examples.exp_configs.rl.multiagent.multiagent_i210 import VEH_PER_HOUR_BASE
     VEH_PER_HOUR_BASE_27414345, VEH_PER_HOUR_BASE_27414342
 
 
-def make_inflows(penetration_rate=0.1, flow_rate_coef=1.0, departSpeed=20):
+def make_inflows(penetration_rate=0.1, flow_rate_coef=1.0, departSpeed=20, on_ramp=False):
     """Generate inflows object from parameters. Uses default inflows from multiagent_i210.
 
     Keyword Arguments:
@@ -31,27 +31,28 @@ def make_inflows(penetration_rate=0.1, flow_rate_coef=1.0, departSpeed=20):
                             # probability=1.0,
                             departLane="random",
                             departSpeed=departSpeed)
-
-    inflow_27414345 = dict(veh_type="human",
-                           edge="27414345",
-                           vehs_per_hour=VEH_PER_HOUR_BASE_27414345 * penetration_rate * flow_rate_coef,
-                           departLane="random",
-                           departSpeed=departSpeed)
-
-    inflow_27414342 = dict(veh_type="human",
-                           edge="27414342#0",
-                           vehs_per_hour=VEH_PER_HOUR_BASE_27414342 * penetration_rate * flow_rate_coef,
-                           departLane="random",
-                           departSpeed=departSpeed)
-
     inflow_119257914_av = dict(veh_type="av",
-                               edge="119257914",
-                               vehs_per_hour=int(VEH_PER_HOUR_BASE_119257914 * penetration_rate * flow_rate_coef),
-                               # probability=1.0,
-                               departLane="random",
-                               departSpeed=departSpeed)
+                            edge="119257914",
+                            vehs_per_hour=int(VEH_PER_HOUR_BASE_119257914 * penetration_rate * flow_rate_coef),
+                            # probability=1.0,
+                            departLane="random",
+                            departSpeed=departSpeed)
+    if on_ramp:
+        inflow_27414345 = dict(veh_type="human",
+                            edge="27414345",
+                            vehs_per_hour=VEH_PER_HOUR_BASE_27414345 * penetration_rate * flow_rate_coef,
+                            departLane="random",
+                            departSpeed=departSpeed)
 
-    all_inflow_defs = (inflow_119257914, inflow_27414345, inflow_27414342, inflow_119257914_av)
+        inflow_27414342 = dict(veh_type="human",
+                            edge="27414342#0",
+                            vehs_per_hour=VEH_PER_HOUR_BASE_27414342 * penetration_rate * flow_rate_coef,
+                            departLane="random",
+                            departSpeed=departSpeed)
+
+        all_inflow_defs = (inflow_119257914, inflow_27414345, inflow_27414342, inflow_119257914_av)
+    else:
+        all_inflow_defs = (inflow_119257914, inflow_119257914_av)
 
     for inflow_def in all_inflow_defs:
         inflow.add(**inflow_def)
