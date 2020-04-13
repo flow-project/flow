722f4393515247907bd14260b803fd5fb6510533
akashvelu <akashvelu@berkeley.edu>
2020-04-12 18:32:00 -0700

    Added testing for imitation learning with dagger, more code cleanup

---
+++ flow/controllers/dagger/replay_buffer.py
@@ -0,0 +1,64 @@
+import time
+import numpy as np
+import tensorflow as tf
+import gym
+import os
+from utils import *
+
+
+class ReplayBuffer(object):
+    """ Replay buffer class to store state, action, expert_action, reward, next_state, terminal tuples"""
+
+    def __init__(self, max_size=100000):
+
+        self.max_size = max_size
+
+        # store each rollout
+        self.rollouts = []
+
+        # store component arrays from each rollout
+        self.observations = None
+        self.actions = None
+        self.expert_actions = None
+        self.rewards = None
+        self.next_observations = None
+        self.terminals = None
+
+
+    def add_rollouts(self, rollouts_list):
+        """
+        Add a list of rollouts to the replay buffer
+        """
+
+        for rollout in rollouts_list:
+            self.rollouts.append(rollout)
+
+        observations, actions, expert_actions, rewards, next_observations, terminals = unpack_rollouts(rollouts_list)
+
+        assert (not np.any(np.isnan(expert_actions))), "Invalid actions added to replay buffer"
+
+        if self.observations is None:
+            self.observations = observations[-self.max_size:]
+            self.actions = actions[-self.max_size:]
+            self.expert_actions = expert_actions[-self.max_size:]
+            self.rewards = rewards[-self.max_size:]
+            self.next_observations = next_observations[-self.max_size:]
+            self.terminals = terminals[-self.max_size:]
+        else:
+            self.observations = np.concatenate([self.observations, observations])[-self.max_size:]
+            self.actions = np.concatenate([self.actions, actions])[-self.max_size:]
+            self.expert_actions = np.concatenate([self.expert_actions, expert_actions])[-self.max_size:]
+            self.rewards = np.concatenate([self.rewards, rewards])[-self.max_size:]
+            self.next_observations = np.concatenate([self.next_observations, next_observations])[-self.max_size:]
+            self.terminals = np.concatenate([self.terminals, terminals])[-self.max_size:]
+
+    def sample_batch(self, batch_size):
+        """
+            Sample a batch of data (with size batch_size) from replay buffer.
+            Returns data in separate numpy arrays of observations, actions, rewards, next_observations, terminals
+        """
+        assert self.observations is not None and self.actions is not None and self.expert_actions is not None and self.rewards is not None and self.next_observations is not None and self.terminals is not None
+
+        size = len(self.observations)
+        rand_inds = np.random.randint(0, size, batch_size)
+        return self.observations[rand_inds], self.actions[rand_inds], self.expert_actions[rand_inds], self.rewards[rand_inds], self.next_observations[rand_inds], self.terminals[rand_inds]
---
+++ flow/controllers/dagger/utils.py
@@ -0,0 +1,173 @@
+import tensorflow as tf
+import os
+import numpy as np
+import math
+
+""" Class agnostic helper functions """
+
+def sample_trajectory(env, vehicle_id, controller, expert_controller, max_trajectory_length):
+    """
+    Samples a trajectory for a given vehicle using the actions prescribed by specified controller.
+
+    Args:
+        env: environment
+        vehicle_id: id of the vehicle that is being controlled/tracked during trajectory
+        controller: subclass of BaseController, decides actions taken by vehicle
+        expert_controller: subclass of BaseController, "expert" for imitation learning
+        max_trajectory_length: maximum steps in a trajectory
+
+    Returns:
+        Dictionary of numpy arrays, where matching indeces of each array given (state, action, expert_action, reward, next_state, terminal) tuples
+    """
+
+    print("COLLECTING CONTROLLER: ", controller)
+    print("EXPERT CONTROLLER: ", expert_controller)
+    observation = env.reset()
+
+    assert vehicle_id in env.k.vehicle.get_ids(), "Vehicle ID not in env!"
+
+    observations, actions, expert_actions, rewards, next_observations, terminals = [], [], [], [], [], []
+    traj_length = 0
+
+    while True:
+        action = controller.get_action(env)
+
+        if type(action) == np.ndarray:
+            action = action.flatten()[0]
+
+        expert_action = expert_controller.get_action(env)
+        if (expert_action is None or math.isnan(expert_action)):
+            observation, reward, done, _ = env.step(action)
+            traj_length += 1
+            terminate_rollout = traj_length == max_trajectory_length or done
+            if terminate_rollout:
+                break
+            continue
+
+        observations.append(observation)
+        actions.append(action)
+        expert_actions.append(expert_action)
+        observation, reward, done, _ = env.step(action)
+
+        traj_length += 1
+        next_observations.append(observation)
+        rewards.append(reward)
+        terminate_rollout = (traj_length == max_trajectory_length) or done
+        terminals.append(terminate_rollout)
+
+        if terminate_rollout:
+            break
+
+    return traj_dict(observations, actions, expert_actions, rewards, next_observations, terminals)
+
+
+def sample_trajectories(env, vehicle_id, controller, expert_controller, min_batch_timesteps, max_trajectory_length):
+    """
+    Samples trajectories to collect at least min_batch_timesteps steps in the environment
+
+    Args:
+        env: environment
+        vehicle_id: id of vehicle being tracked/controlled
+        controller: subclass of BaseController, decides actions taken by vehicle
+        expert_controller: subclass of BaseController, "expert" for imitation learning
+        min_batch_timesteps: minimum number of environment steps to collect
+        max_trajectory_length: maximum steps in a trajectory
+
+    Returns:
+        List of rollout dictionaries, total steps taken by environment
+    """
+    total_envsteps = 0
+    trajectories = []
+
+    while total_envsteps < min_batch_timesteps:
+        trajectory = sample_trajectory(env, vehicle_id, controller, expert_controller, max_trajectory_length)
+        trajectories.append(trajectory)
+
+        traj_env_steps = len(trajectory["rewards"])
+        total_envsteps += traj_env_steps
+
+    return trajectories, total_envsteps
+
+def sample_n_trajectories(env, vehicle_id, controller, expert_controller, n, max_trajectory_length):
+    """
+    Collects a fixed number of trajectories.
+
+    Args:
+        env: environment
+        vehicle_id: id of vehicle being tracked/controlled
+        controller: subclass of BaseController, decides actions taken by vehicle
+        expert_controller: subclass of BaseController, "expert" for imitation learning
+        n: number of trajectories to collect
+        max_trajectory_length: maximum steps in a trajectory
+
+    Returns:
+        List of rollout dictionaries
+
+    """
+    trajectories = []
+    for _ in range(n):
+        trajectory = sample_trajectory(env, vehicle_id, controller, expert_controller, max_trajectory_length)
+        trajectories.append(trajectory)
+
+    return trajectories
+
+
+def traj_dict(observations, actions, expert_actions, rewards, next_observations, terminals):
+    """
+    Collects individual observation, action, expert_action, rewards, next observation, terminal arrays into a single rollout dictionary
+    """
+    return {"observations" : np.array(observations, dtype=np.float32),
+            "actions" : np.array(actions, dtype=np.float32),
+            "expert_actions": np.array(expert_actions, dtype=np.float32),
+            "rewards" : np.array(rewards, dtype=np.float32),
+            "next_observations": np.array(next_observations, dtype=np.float32),
+            "terminals": np.array(terminals, dtype=np.float32)}
+
+
+def unpack_rollouts(rollouts_list):
+    """
+        Convert list of rollout dictionaries to individual observation, action, rewards, next observation, terminal arrays
+        rollouts: list of rollout dictionaries, rollout dictionary: dictionary with keys "observations", "actions", "rewards", "next_observations", "is_terminals"
+        return separate np arrays of observations, actions, rewards, next_observations, and is_terminals
+    """
+    observations = np.concatenate([rollout["observations"] for rollout in rollouts_list])
+    actions = np.concatenate([rollout["actions"] for rollout in rollouts_list])
+    expert_actions = np.concatenate([rollout["expert_actions"] for rollout in rollouts_list])
+    rewards = np.concatenate([rollout["rewards"] for rollout in rollouts_list])
+    next_observations = np.concatenate([rollout["next_observations"] for rollout in rollouts_list])
+    terminals = np.concatenate([rollout["terminals"] for rollout in rollouts_list])
+
+    return observations, actions, expert_actions, rewards, next_observations, terminals
+
+
+# Below are tensorflow related functions
+
+def build_neural_net(input_placeholder, output_size, scope, n_layers, size, activation=tf.tanh, output_activation=None):
+    """
+        Builds a feedfoward neural network for action prediction
+
+        arguments:
+            input_placeholder: placeholder variable for the state (batch_size, input_size)
+            scope: variable scope of the network
+
+            n_layers: number of hidden layers
+            size: dimension of each hidden layer
+            activation: activation of each hidden layer
+
+            output_size: size of the output layer
+            output_activation: activation of the output layer
+
+        returns:
+            output_placeholder: the result of pass through Neural Network
+    """
+    output_placeholder = input_placeholder
+    with tf.variable_scope(scope):
+        for _ in range(n_layers):
+            output_placeholder = tf.layers.dense(output_placeholder, size, activation=activation)
+        output_placeholder = tf.layers.dense(output_placeholder, output_size, activation=output_activation)
+    return output_placeholder
+
+def create_tf_session():
+    config = tf.ConfigProto(device_count={'GPU': 0})
+    sess = tf.Session(config=config)
+    return sess
