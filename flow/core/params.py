2bd8a049c36f06c1fed33c437c950297ec9db9b3
Eugene Vinitsky <eugenevinitsky@users.noreply.github.com>
2020-04-15 20:24:51 -0700

    Evinitsky/straight road pr (#909)
    Add a straight road training environment. Swap out the reward for a desired velocity squared reward.

--- flow/core/params.py
+++ flow/core/params.py
@@ -637,6 +637,9 @@ class EnvParams:
         specifies whether to clip actions from the policy by their range when
         they are inputted to the reward function. Note that the actions are
         still clipped before they are provided to `apply_rl_actions`.
+    done_at_exit : bool, optional
+        If true, done is returned as True when the vehicle exits. This is only
+        applied to multi-agent environments.
     """
 
     def __init__(self,
@@ -645,7 +648,8 @@ class EnvParams:
                  warmup_steps=0,
                  sims_per_step=1,
                  evaluate=False,
-                 clip_actions=True):
+                 clip_actions=True,
+                 done_at_exit=True):
         """Instantiate EnvParams."""
         self.additional_params = \
             additional_params if additional_params is not None else {}
@@ -654,6 +658,7 @@ class EnvParams:
         self.sims_per_step = sims_per_step
         self.evaluate = evaluate
         self.clip_actions = clip_actions
+        self.done_at_exit = done_at_exit
 
     def get_additional_param(self, key):
         """Return a variable from additional_params."""
