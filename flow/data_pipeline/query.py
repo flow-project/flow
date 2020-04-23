d0c55ed5246973685b13e41cecd039e61a19fe2f
Brent Zhao <brentzhao@berkeley.edu>
2020-04-23 12:58:44 -0700

    fix some more style issues

--- flow/data_pipeline/query.py
+++ flow/data_pipeline/query.py
@@ -1,6 +1,6 @@
 """stores all the pre-defined query strings."""
 from enum import Enum
-from examples.datapipeline_test import apply_energy_one
+from flow.data_pipeline.datapipeline_test import apply_energy_one
 
 # tags for different queries
 tags = {"energy": ["POWER_DEMAND_MODEL"], "analysis": ["POWER_DEMAND_MODEL"]}
@@ -15,8 +15,8 @@ class QueryStrings(Enum):
     SAMPLE = "SELECT * FROM trajectory_table WHERE partition_name=\'{partition}\' LIMIT 15;"
     UPDATE_PARTITION = "ALTER TABLE trajectory_table ADD IF NOT EXISTS PARTITION (partition_name=\'{partition}\');"
     POWER_DEMAND_MODEL = "SELECT id, time, 1200 * speed * " \
-                 "((CASE WHEN acceleration > 0 THEN 1 ELSE 0 END * (1-0.8) * acceleration) + 0.8 " \
-                 "+ 9.81 * SIN(road_grade)) + 1200 * 9.81 * 0.005 * speed + 0.5 * 1.225 * 2.6 * 0.3 " \
-                 "* POW(speed,3) AS power, 1 AS energy_model_id, source_id " \
-                 "FROM trajectory_table " \
-                 "WHERE partition_name=\'{partition}\'"
+                         "((CASE WHEN acceleration > 0 THEN 1 ELSE 0 END * (1-0.8) * acceleration) + 0.8 " \
+                         "+ 9.81 * SIN(road_grade)) + 1200 * 9.81 * 0.005 * speed + 0.5 * 1.225 * 2.6 * 0.3 " \
+                         "* POW(speed,3) AS power, 1 AS energy_model_id, source_id " \
+                         "FROM trajectory_table " \
+                         "WHERE partition_name=\'{partition}\'"
