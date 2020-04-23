28e90caa7f39453a5c8ef21aed313dc81fc1f659
Brent Zhao <brentzhao@berkeley.edu>
2020-04-23 12:35:54 -0700

    fix style issue

--- flow/core/kernel/vehicle/base.py
+++ flow/core/kernel/vehicle/base.py
@@ -702,7 +702,7 @@ class KernelVehicle(object):
         raise NotImplementedError
 
     def get_2D_position(self, veh_id, error=-1001):
-        """ see traci class """
+        """Return (x, y) position of vehicle with veh_id."""
         raise NotImplementedError
 
     def get_accel_without_noise(self, veh_id):
