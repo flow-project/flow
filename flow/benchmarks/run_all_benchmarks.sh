41771610938cace649dd177aa8ec988e5c67eca2
Eugene Vinitsky <vinitsky.eugene@gmail.com>
2020-05-13 18:24:50 -0400

    Increase default number of CPUs in benchmark runner script

--- flow/benchmarks/run_all_benchmarks.sh
+++ flow/benchmarks/run_all_benchmarks.sh
@@ -2,7 +2,7 @@
 echo "Running all benchmarks"
 
 declare -a benchmarks=(
-                        "bottleneck0" "bottleneck1" "bottleneck2"
+                        "bottleneck0" "bottleneck1" #"bottleneck2"
 #                        "grid0" "grid1"
                         )
 
@@ -13,7 +13,7 @@ cd "$parent_path"
 dt=$(date '+%Y_%m_%d');
 echo $dt
 i=0
-for run_script in rllib/sac_runner.py; do
+for run_script in rllib/ppo_runner.py; do
     declare alg=`echo ${run_script} | cut -d'/' -f 2 | cut -d'_' -f 1`
     for benchmark in "${benchmarks[@]}"; do
         i=$((i+1))
@@ -22,7 +22,7 @@ for run_script in rllib/sac_runner.py; do
         echo "ray exec ../../scripts/ray_autoscale.yaml \"python ./flow/flow/benchmarks/${run_script} --upload_dir=\"eugene.experiments/offline_rl/${dt}/\" --benchmark_name=${benchmark} --num_cpus 14\" --start --stop --cluster-name=all_benchmark_${benchmark}_${alg}_$dt --tmux"
         echo "====================================================================="
         ray exec ../../scripts/ray_autoscale.yaml "python ./flow/flow/benchmarks/${run_script} \
-        --upload_dir=\"eugene.experiments/offline_rl/${dt}/\" --benchmark_name=${benchmark} --grid_search" \
-        --start --stop --cluster-name=all_benchmark_${benchmark}_${alg}_$dt --tmux
+        --upload_dir=\"eugene.experiments/offline_rl/${dt}/\" --benchmark_name=${benchmark} --num_cpus 25" \
+        --start --stop --cluster-name=all_benchmark2_${benchmark}_${alg}_$dt --tmux
     done
 done
