62afe9c979b9b41cb4bdfb316443662c3909c8c0
AboudyKreidieh <akreidieh@gmail.com>
2020-05-15 22:24:11 -0700

    added updated single lane highway network config

--- examples/exp_configs/non_rl/highway_single.py
+++ examples/exp_configs/non_rl/highway_single.py
@@ -1,9 +1,5 @@
-"""Multi-agent highway with ramps example.
-
-Trains a non-constant number of agents, all sharing the same policy, on the
-highway with ramps network.
-"""
-from flow.controllers import BandoFTLController
+"""Example of an open network with human-driven vehicles."""
+from flow.controllers import IDMController
 from flow.core.params import EnvParams
 from flow.core.params import NetParams
 from flow.core.params import InitialConfig
@@ -11,15 +7,21 @@ from flow.core.params import InFlows
 from flow.core.params import VehicleParams
 from flow.core.params import SumoParams
 from flow.core.params import SumoLaneChangeParams
+from flow.core.params import SumoCarFollowingParams
 from flow.networks import HighwayNetwork
 from flow.envs import TestEnv
 from flow.networks.highway import ADDITIONAL_NET_PARAMS
 
-TRAFFIC_SPEED = 11
-END_SPEED = 16
-TRAFFIC_FLOW = 2056
-HORIZON = 3600
-INCLUDE_NOISE = False
+# the speed of vehicles entering the network
+TRAFFIC_SPEED = 24.1
+# the maximum speed at the downstream boundary edge
+END_SPEED = 6.0
+# the inflow rate of vehicles
+TRAFFIC_FLOW = 2215
+# the simulation time horizon (in steps)
+HORIZON = 4500
+# whether to include noise in the car-following models
+INCLUDE_NOISE = True
 
 additional_net_params = ADDITIONAL_NET_PARAMS.copy()
 additional_net_params.update({
@@ -31,28 +33,29 @@ additional_net_params.update({
     "speed_limit": 30,
     # number of edges to divide the highway into
     "num_edges": 2,
-    # whether to include a ghost edge of length 500m. This edge is provided a
-    # different speed limit.
+    # whether to include a ghost edge
     "use_ghost_edge": True,
     # speed limit for the ghost edge
-    "ghost_speed_limit": END_SPEED
+    "ghost_speed_limit": END_SPEED,
+    # length of the cell imposing a boundary
+    "boundary_cell_length": 300,
 })
 
 vehicles = VehicleParams()
 vehicles.add(
     "human",
-    num_vehicles=0,
-    lane_change_params=SumoLaneChangeParams(
-        lane_change_mode="strategic",
-    ),
-    acceleration_controller=(BandoFTLController, {
-        'alpha': .5,
-        'beta': 20.0,
-        'h_st': 12.0,
-        'h_go': 50.0,
-        'v_max': 30.0,
-        'noise': 1.0 if INCLUDE_NOISE else 0.0,
+    acceleration_controller=(IDMController, {
+        'a': 1.3,
+        'b': 2.0,
+        'noise': 0.3 if INCLUDE_NOISE else 0.0
     }),
+    car_following_params=SumoCarFollowingParams(
+        min_gap=0.5
+    ),
+    lane_change_params=SumoLaneChangeParams(
+        model="SL2015",
+        lc_sublane=2.0,
+    ),
 )
 
 inflows = InFlows()
@@ -60,15 +63,12 @@ inflows.add(
     veh_type="human",
     edge="highway_0",
     vehs_per_hour=TRAFFIC_FLOW,
-    depart_lane="free",
-    depart_speed=TRAFFIC_SPEED,
-    name="idm_highway_inflow")
-
-# SET UP FLOW PARAMETERS
+    departLane="free",
+    departSpeed=TRAFFIC_SPEED)
 
 flow_params = dict(
     # name of the experiment
-    exp_tag='highway-single',
+    exp_tag='highway',
 
     # name of the flow environment the experiment is running on
     env_name=TestEnv,
@@ -88,9 +88,10 @@ flow_params = dict(
 
     # sumo-related parameters (see flow.core.params.SumoParams)
     sim=SumoParams(
-        sim_step=0.5,
+        sim_step=0.4,
         render=False,
-        restart_instance=False
+        restart_instance=False,
+        use_ballistic=True
     ),
 
     # network-related parameters (see flow.core.params.NetParams and the
