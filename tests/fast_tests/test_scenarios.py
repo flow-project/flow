bdcfb966b1cfd05921de40197fe27d0354d38b2a
AboudyKreidieh <akreidieh@gmail.com>
2020-05-18 02:05:43 -0700

    bug fixes

--- tests/fast_tests/test_scenarios.py
+++ tests/fast_tests/test_scenarios.py
@@ -116,7 +116,8 @@ class TestHighwayNetwork(unittest.TestCase):
                 "speed_limit": 30,
                 "num_edges": 1,
                 "use_ghost_edge": False,
-                "ghost_speed_limit": 25
+                "ghost_speed_limit": 25,
+                "boundary_cell_length": 300,
             })
         )
         env.reset()
