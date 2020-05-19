d16307900fee5c284e853989598bb288f18635e0
liljonnystyle <jonny5@berkeley.edu>
2020-05-18 21:47:38 -0700

    fix merge duplication and excess whitespace

--- flow/core/kernel/vehicle/base.py
+++ flow/core/kernel/vehicle/base.py
@@ -339,24 +339,7 @@ class KernelVehicle(object, metaclass=ABCMeta):
             vehicle id, or list of vehicle ids
         error : any, optional
             value that is returned if the vehicle is not found
-            
-        Returns
-        -------
-        float
-        """
-        pass
 
-    @abstractmethod
-    def get_fuel_consumption(self, veh_id, error=-1001):
-        """Return the mpg / s of the specified vehicle.
-
-        Parameters
-        ----------
-        veh_id : str or list of str
-            vehicle id, or list of vehicle ids
-        error : any, optional
-            value that is returned if the vehicle is not found
-            
         Returns
         -------
         float
