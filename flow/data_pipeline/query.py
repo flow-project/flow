90a4d8b97e96f0ad6838849ecab2f4e42e08d8a8
liljonnystyle <jonny5@berkeley.edu>
2020-05-20 11:49:38 -0700

    specify power demand model names

--- flow/data_pipeline/query.py
+++ flow/data_pipeline/query.py
@@ -15,7 +15,7 @@ VEHICLE_POWER_DEMAND_FINAL_SELECT = """
             (CASE WHEN acceleration > 0 THEN 1 ELSE 0 END * (1-0.8) * acceleration)
             + 0.8 + 9.81 * SIN(road_grade)
             ) + 1200 * 9.81 * 0.005 * speed + 0.5 * 1.225 * 2.6 * 0.3 * POW(speed,3) AS power,
-        'POWER_DEMAND_MODEL' AS energy_model_id,
+        \'{}\' AS energy_model_id,
         source_id
     FROM {}
     ORDER BY id, time_step
@@ -52,7 +52,7 @@ class QueryStrings(Enum):
                 AND date = \'{{date}}\'
                 AND partition_name=\'{{partition}}\'
         )
-        {}""".format(VEHICLE_POWER_DEMAND_FINAL_SELECT.format('regular_cte'))
+        {}""".format(VEHICLE_POWER_DEMAND_FINAL_SELECT.format('POWER_DEMAND_MODEL', 'regular_cte'))
 
     POWER_DEMAND_MODEL_DENOISED_ACCEL = """
         WITH denoised_accel_cte AS (
@@ -68,7 +68,7 @@ class QueryStrings(Enum):
                 AND date = \'{{date}}\'
                 AND partition_name=\'{{partition}}\'
         )
-        {}""".format(VEHICLE_POWER_DEMAND_FINAL_SELECT.format('denoised_accel_cte'))
+        {}""".format(VEHICLE_POWER_DEMAND_FINAL_SELECT.format('POWER_DEMAND_MODEL_DENOISED_ACCEL', 'denoised_accel_cte'))
 
     POWER_DEMAND_MODEL_DENOISED_ACCEL_VEL = """
         WITH lagged_timestep AS (
@@ -96,4 +96,4 @@ class QueryStrings(Enum):
                 source_id
             FROM lagged_timestep
         )
-        {}""".format(VEHICLE_POWER_DEMAND_FINAL_SELECT.format('denoised_speed_cte'))
+        {}""".format(VEHICLE_POWER_DEMAND_FINAL_SELECT.format('POWER_DEMAND_MODEL_DENOISED_ACCEL_VEL', 'denoised_speed_cte'))
