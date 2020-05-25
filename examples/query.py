850d2f73dd752a19fd961eed017bc738f1e12b0d
liljonnystyle <jonny5@berkeley.edu>
2020-05-25 16:57:52 -0700

    fix rebase errors

--- examples/query.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-tags = {}
-
-
-class QueryStrings(Enum):
-    SAMPLE = "SELECT * FROM trajectory_table WHERE partition_name=\'{partition}\' LIMIT 15;"
-    UPDATE_PARTITION = "ALTER TABLE trajectory_table ADD IF NOT EXISTS PARTITION (partition_name=\'{partition}\');"
