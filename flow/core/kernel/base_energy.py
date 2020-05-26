5398d2191dea100474af8442a6ed3a83fe462caa
liljonnystyle <jonny5@berkeley.edu>
2020-05-26 14:58:15 -0700

    rename get method

--- flow/core/kernel/base_energy.py
+++ flow/core/kernel/base_energy.py
@@ -13,18 +13,20 @@ from abc import ABCMeta, abstractmethod
 class BaseEnergyModel(metaclass=ABCMeta):
     """Base energy model class.
 
-    Calculate the instantanoeus power consumption of a vehicle in
+    Calculate the instantaneous power consumption of a vehicle in
     the network.  It returns the power in Watts regardless of the 
     vehicle type: whether EV or Combustion Engine, Toyota Prius or Tacoma
     or non-Toyota vehicles. Non-Toyota vehicles are set by default 
     to be an averaged-size vehicle.
-    
     """
 
     def __init__(self,kernel):
         self.vehicle = vehicle
 
     @abstractmethod
-    def get_energy(self, veh_id,model_param,grade):
-        #return self.calc_energy(veh_id)
+    def get_instantaneous_power(self, veh_id, model_param, grade):
+        """Calculate the instantaneous power consumption of a vehicle.
+        
+        Must be implemented by child classes.
+        """
         pass
