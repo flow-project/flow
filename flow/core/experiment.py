b46d5f933e2560e7c83f18ffa369dcc3081c9ab4
liljonnystyle <jonny5@berkeley.edu>
2020-05-31 21:21:00 -0700

    minor re-formats

--- flow/core/experiment.py
+++ flow/core/experiment.py
@@ -230,14 +230,13 @@ class Experiment:
             
             if to_aws:
                 cur_date = date.today().isoformat()
-                upload_to_s3('circles.data.pipeline', 'fact_vehicle_trace/date={0}/partition_name={1}/{1}.csv'.format(
-                             cur_date, source_id),
-                             trajectory_table_path, {'network': metadata['network']})
-                upload_to_s3('circles.data.pipeline', 'metadata_table/date={0}/partition_name={1}_METADATA/'
-                                                      '{1}_METADATA.csv'.format(cur_date, source_id),
+                upload_to_s3('circles.data.pipeline',
+                             'fact_vehicle_trace/date={0}/partition_name={1}/{1}.csv'.format(cur_date, source_id),
+                             trajectory_table_path,
+                             {'network': metadata['network']})
+                upload_to_s3('circles.data.pipeline',
+                             'metadata_table/date={0}/partition_name={1}_METADATA/',
+                             '{1}_METADATA.csv'.format(cur_date, source_id),
                              metadata_table_path)
-
-            # delete the S3-only version of the trajectory file
-            # os.remove(upload_file_path)
 
         return info_dict
