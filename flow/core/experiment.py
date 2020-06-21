6cecd1987b1f45fc574ad6c284580e2ff2e74d7d
liljonnystyle <jonny5@berkeley.edu>
2020-06-20 17:50:20 -0700

    Update experiment.py

--- flow/core/experiment.py
+++ flow/core/experiment.py
@@ -146,8 +146,6 @@ class Experiment:
 
         if convert_to_csv and self.env.simulator == "traci":
             dir_path = self.env.sim_params.emission_path
-
-        if dir_path:
             trajectory_table_path = os.path.join(dir_path, '{}.csv'.format(source_id))
 
         for i in range(num_runs):
@@ -172,7 +170,7 @@ class Experiment:
                 get_extra_info(self.env.k.vehicle, extra_info, veh_ids, source_id, run_id)
 
                 # write to disk every 100 steps
-                if convert_to_csv and self.env.simulator == "traci" and j % 100 == 0 and dir_path:
+                if convert_to_csv and self.env.simulator == "traci" and j % 100 == 0:
                     write_dict_to_csv(trajectory_table_path, extra_info, not j)
                     extra_info.clear()
 
@@ -207,7 +205,6 @@ class Experiment:
             time.sleep(0.1)
 
             # collect the location of the emission file
-            dir_path = self.env.sim_params.emission_path
             emission_filename = \
                 "{0}-emission.xml".format(self.env.network.name)
             emission_path = os.path.join(dir_path, emission_filename)
