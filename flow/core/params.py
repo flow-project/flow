cc5edbd1380fa4cd4b003186adbc33d34c31a428
Joy Carpio <engrjoycarpio@berkeley.edu>
2020-07-02 00:30:16 +0800

    Update params.py

--- flow/core/params.py
+++ flow/core/params.py
@@ -247,8 +247,9 @@ class VehicleParams:
             num_vehicles=0,
             car_following_params=None,
             lane_change_params=None,
-            color=None,
-            energy_model = (PDMCombustionEngine, {})):
+            energy_model=(PDMCombustionEngine, {}),
+            color=None):
+
         """Add a sequence of vehicles to the list of vehicles in the network.
 
         Parameters
