58e703e4be20bd85070dd741bec6824684a00067
Nathan Lichtlé <nathan.lichtle@gmail.com>
2020-07-02 14:45:48 +0200

    print for fix

--- scripts/run_experiments.sh
+++ scripts/run_experiments.sh
@@ -13,4 +13,4 @@
 ray exec ray_autoscale.yaml \
 "python flow/examples/train.py multiagent_i210 i210_ppo --num_iterations 1 --num_rollouts 1 \
 --checkpoint_freq 5 --use_s3 --num_cpus 20 --algorithm PPO --rl_trainer rllib" \
---start --stop --cluster-name nathan-i210-test
+--start --cluster-name nathan-i210-test
