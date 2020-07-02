028b092af0fa54943faa87070ccb743210352374
Nathan Lichtlé <nathan.lichtle@gmail.com>
2020-07-02 14:42:25 +0200

    mute warnings temporarily

--- flow/utils/flow_warnings.py
+++ flow/utils/flow_warnings.py
@@ -20,11 +20,12 @@ def deprecated_attribute(obj, dep_from, dep_to):
         new name for the attribute
     """
     warnings.simplefilter('always', PendingDeprecationWarning)
-    warnings.warn(
-        "The attribute {} in {} is deprecated, use {} instead.".format(
-            dep_from, obj.__class__.__name__, dep_to),
-        PendingDeprecationWarning
-    )
+    # TMP MUTE WARNINGS
+    # warnings.warn(
+    #     "The attribute {} in {} is deprecated, use {} instead.".format(
+    #         dep_from, obj.__class__.__name__, dep_to),
+    #     PendingDeprecationWarning
+    # )
 
 
 def deprecated(base, new_path):
