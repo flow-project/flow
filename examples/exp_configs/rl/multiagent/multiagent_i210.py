6946832f3dd68324a591d25e2a5d144c0874d39e
liljonnystyle <jonny5@berkeley.edu>
2020-07-08 21:14:37 -0700

    uncomment energy_consumption in multiagent_i210

--- examples/exp_configs/rl/multiagent/multiagent_i210.py
+++ examples/exp_configs/rl/multiagent/multiagent_i210.py
@@ -20,7 +20,7 @@ from flow.core.params import VehicleParams
 from flow.core.params import SumoParams
 from flow.core.params import SumoCarFollowingParams
 from flow.core.params import SumoLaneChangeParams
-# from flow.core.rewards import energy_consumption
+from flow.core.rewards import energy_consumption
 from flow.envs.multiagent.i210 import I210MultiEnv, ADDITIONAL_ENV_PARAMS
 from flow.utils.registry import make_create_env
 from flow.networks.i210_subnetwork import I210SubNetwork, EDGES_DISTRIBUTION
@@ -298,8 +298,8 @@ custom_callables = {
     "avg_speed": lambda env: np.mean([speed for speed in
                                       env.k.vehicle.get_speed(env.k.vehicle.get_ids()) if speed >= 0]),
     "avg_outflow": lambda env: np.nan_to_num(env.k.vehicle.get_outflow_rate(120)),
-    # "avg_energy": lambda env: -1 * energy_consumption(env, 0.1),
-    # "avg_per_step_energy": lambda env: -1 * energy_consumption(env, 0.1) / env.k.vehicle.num_vehicles
-    # if env.k.vehicle.num_vehicles > 0
-    # else 0,
+    "avg_energy": lambda env: -1 * energy_consumption(env, 0.1),
+    "avg_per_step_energy": lambda env: -1 * energy_consumption(env, 0.1) / env.k.vehicle.num_vehicles
+    if env.k.vehicle.num_vehicles > 0
+    else 0,
 }
