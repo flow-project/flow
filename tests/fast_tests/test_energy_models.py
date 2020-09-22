817d80a96e528b870f3bf0c06c58e5b8745d7da4
Joy Carpio <53185340+joyncarpio@users.noreply.github.com>
2020-09-21 17:01:17 -0700

    Update test_energy_models.py (#68)
    * Update test_energy_models.py
    
    * Update test_energy_models.py

--- tests/fast_tests/test_energy_models.py
+++ tests/fast_tests/test_energy_models.py
@@ -4,8 +4,7 @@ import pandas as pd
 
 from pandas.testing import assert_series_equal
 
-from flow.energy_models.power_demand import PDMCombustionEngine
-# from flow.energy_models.power_demand import PDMElectric
+from flow.energy_models.power_demand import PDMCombustionEngine, PDMElectric
 
 os.environ["TEST_FLAG"] = "True"
 
@@ -27,22 +26,19 @@ class TestEnergyModels(unittest.TestCase):
         energy_model = PDMCombustionEngine()
         power_value = energy_model.get_instantaneous_power
         power = self.data.apply(lambda x: power_value(x['a(m/s^2)'], x['v(m/s)'], x['rg(%)']), axis=1)
-        assert_series_equal(self.data['Tacoma_fit(KW)']*1000, power, check_less_precise=True, check_names=False)
+        assert_series_equal(self.data['Tacoma_fit(KW)']*1000, power, rtol=0.1, check_names=False)
 
     def test_pdm_combustion_engine_fuel(self):
         energy_model = PDMCombustionEngine()
         fuel_value = energy_model.get_instantaneous_fuel_consumption
         fuel = self.data.apply(lambda x: fuel_value(x['a(m/s^2)'], x['v(m/s)'], x['rg(%)']), axis=1)
-        assert_series_equal(self.data['Tacoma_fit(gal/hr)'], fuel, check_less_precise=True, check_names=False)
+        assert_series_equal(self.data['Tacoma_fit(gal/hr)'], fuel, rtol=0.1, check_names=False)
 
-    """
-    # Joy TODO: uncomment when this are fixed.
     def test_pdm_electric_power(self):
         energy_model = PDMElectric()
         power_value = energy_model.get_instantaneous_power
         power = self.data.apply(lambda x: power_value(x['a(m/s^2)'], x['v(m/s)'], x['rg(%)']), axis=1)
-        assert_series_equal(self.data['Prius_fit(KW)']*1000, power, check_less_precise=True, check_names=False)
-    """
+        assert_series_equal(self.data['Prius_fit(KW)']*1000, power, rtol=0.1, check_names=False)
 
 
 if __name__ == '__main__':
