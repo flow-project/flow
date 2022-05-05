fef789c036d0310093d538db937930ebc04135d0
grbagwe <grbagwe@mtu.edu>
2022-05-05 12:09:48 -0400

    pyglet renderer bug removed

--- flow/envs/base.py
+++ flow/envs/base.py
@@ -133,7 +133,7 @@ class Env(gym.Env, metaclass=ABCMeta):
         self.sim_params = deepcopy(sim_params)
         # check whether we should be rendering
         self.should_render = self.sim_params.render
-        self.sim_params.render = False
+        self.sim_params.render = self.sim_params.render
         time_stamp = ''.join(str(time.time()).split('.'))
         if os.environ.get("TEST_FLAG", 0):
             # 1.0 works with stress_test_start 10k times
@@ -434,7 +434,7 @@ class Env(gym.Env, metaclass=ABCMeta):
         # Now that we've passed the possibly fake init steps some rl libraries
         # do, we can feel free to actually render things
         if self.should_render:
-            self.sim_params.render = True
+            self.sim_params.render = self.should_render
             # got to restart the simulation to make it actually display anything
             self.restart_simulation(self.sim_params)
 
@@ -717,6 +717,7 @@ class Env(gym.Env, metaclass=ABCMeta):
         buffer_length : int
             length of the buffer
         """
+        print(self.sim_params.render, '720 sim params render')
         if self.sim_params.render in ['gray', 'dgray', 'rgb', 'drgb']:
             # render a frame
             self.pyglet_render()
